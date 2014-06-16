5310dd610fc8f65d99101a87aa70416c3f475b98
Jamezs Gladney <jamezs@angelofcode.com>
2014-06-16 02:30:22 -0400

    Update system brought back to main branch

--- update.js
+++ update.js
@@ -1,37 +1,30 @@
 /* jslint node: true */
 var fs = require('fs');
-var walk = require('fs-walk');
+var path = require('path');
+var startTime = new Date();
+console.log(startTime);
+function dirTree(filename) {
+    var data;
+    var stats = fs.lstatSync(filename),
+        info = {
+            path: filename,
+            name: path.basename(filename),
+            
+        };
 
-
-
-function update(location, file) {
-    var files = [];
-    walk.walkSync(location, function (basedir, filename, stat, next) {
-        files.push({
-            "filename": filename,
-            "modified": stat.mtime.getTime() || false
+    if (stats.isDirectory()) {
+        info.type = "folder";
+        info.subfolder = fs.readdirSync(filename).map(function (child) {
+            return dirTree(filename + '/' + child);
         });
+    } else {
+        // Assuming it's a file. In real life it could be a symlink or
+        // something else!
+        info.type = "file";
+        info.size = stats.size;
+    }
 
-
-    });
-    var output = JSON.stringify(files, null, 4);
-    fs.writeFile(file, output, function (error) {
-
-
-        console.log(file);
-    });
+    return info;
 }
-
-
-update(__dirname + "\\public\\ygopro\\", __dirname + '\\manifest\\ygopro.json');
-update(__dirname + "\\public\\ygopro\\script\\", __dirname + '\\manifest\\ygopro.script.json');
-update(__dirname + "\\public\\ygopro\\pics\\", __dirname + '\\manifest\\ygopro.pics.json');
-update(__dirname + "\\public\\ygopro\\fonts\\", __dirname + '\\manifest\\ygopro.fonts.json');
-update(__dirname + "\\public\\ygopro\\sound\\", __dirname + '\\manifest\\ygopro.sound.json');
-update(__dirname + "\\public\\ygopro\\texture\\", __dirname + '\\manifest\\ygopro.texture.json');
-update(__dirname + "\\public\\application\\js\\vendor", __dirname + '\\update\\public.application.js.ventter.json');
-//update(__dirname + "\\private\\tcg\\scripts", __dirname + '\\public\\tcg.json');
-//update(__dirname + "\\private\\ocg\\scripts", __dirname + '\\public\\ocg.json');
-console.log('done');
-       
-        
+fs.writeFile('manifest/ygopro.json', JSON.stringify(dirTree('ygopro'), null, 4),function(){});
+console.log((new Date()).getTime() - startTime.getTime(), 'ms'  );
