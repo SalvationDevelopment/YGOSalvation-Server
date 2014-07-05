ccf66ccda7e88d0cc4352c804c64145c5df683d8
Jamezs Gladney <jamezs@angelofcode.com>
2014-07-05 01:53:42 -0400

    A extremely bad test,... suffering will ensue.

--- test/test.js
+++ test/test.js
@@ -2,4 +2,14 @@
 console.log('Running Test');
 
 
-require('../manager.js');
+var parsePackets = require('../parsepacts.js');
+
+var net = require('net');
+var Primus = require('primus');
+try {
+    console.log('do this');
+}catch(e){}
+
+if (false){
+    console.log('never do this.');
+}
