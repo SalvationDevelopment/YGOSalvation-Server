3b976fca725c9b8ff219e2c598caa29c346f04f0
Jamezs Gladney <jamezs@angelofcode.com>
2014-07-06 02:16:18 -0400

    test fix

--- objectifier.js
+++ objectifier.js
@@ -31,10 +31,11 @@ var dataMap = {
 function structureDefinition(structure) {
     /* take the definition of the structure of the buffer/array you are decoding,
     then compare each value in it to see how big each 'data bucket' needs to be */
+    console.log(structure);
     var maxLength = 1;
     var names = [];
     for (var property in structure) {
-        if (hasOwnProperty(property)) {
+        if (structure.hasOwnProperty(property)) {
             names.push(property.toLowerCase());
             if (dataMap[structure[property].toLowerCase()] > maxLength) {
                 maxLength = dataMap[structure[property].toLowerCase()];
