60fdb14f3b351b0988f562524c206e8dc9a238a8
Jamezs Gladney <jamezs@angelofcode.com>
2014-07-12 06:39:46 -0400

    Server to Client Incoming task update

--- server/js/webconnectivity.js
+++ server/js/webconnectivity.js
@@ -10,8 +10,8 @@ var primus = new Primus(server, {
 });
 var Socket = primus.Socket;
 var parsePackets = require('../../parsepackets.js');
-var RecieveCTOS = require('../../recieveCTOS');
-var RecieveSTOC = require('../../recieveSTOC.js');
+//var recieveCTOS = require('../../recieveCTOS');
+var recieveSTOC = require('../../recieveSTOC.js');
 
 var proxy = net.createServer(function (socket) {
     var client = new Socket('http://localhost:5000');
@@ -21,7 +21,7 @@ var proxy = net.createServer(function (socket) {
         task = (function () {
             var output = [];
             for (var i = 0; task.length > i; i++) {
-                output.push(RecieveSTOC(task[i]), viewModel);
+                output.push(recieveSTOC(task[i]), viewModel);
             }
             return output;
         })();
@@ -45,4 +45,45 @@ var proxy = net.createServer(function (socket) {
 
     });
 });
+
+function processTask(task, socket) {
+    task = (function () {
+        var output = [];
+        for (var i = 0; task.length > i; i++) {
+            output.push(recieveSTOC(task[i], socket.username, socket.hostString));
+        }
+        return output;
+    })();
+    for (var i = 0; task.length > i; i++) {
+        if (task[i].CTOS_JOIN_GAME) {
+            socket.active = true;
+            socket.hostString = task[i].CTOS_JOIN_GAME;
+            //console.log(task);
+        }
+        if (task[i].STOC_REPLAY) {
+            //save replay file
+        }
+        if (task[i].STOC_TIME_LIMIT) {
+            console.log('Time Limit', task[i].STOC_TIME_LIMIT);
+        }
+        if (task[i].STOC_CHAT) {
+            console.log('Chat', task[i].STOC_CHAT);
+        }
+        if (task[i].STOC_HS_PLAYER_ENTER) {
+            console.log('Player Entered', task[i].STOC_HS_PLAYER_ENTER);
+        }
+        if (task[i].STOC_HS_PLAYER_CHANGE) {
+            console.log('Player Change', task[i].STOC_HS_PLAYER_CHANGE);
+        }
+        if (task[i].STOC_HS_WATCH_CHANGE) {
+            console.log('Change in the number of watchers', task[i].STOC_HS_WATCH_CHANGE);
+        }
+        if (task[i].STOC_TYPE_CHANGE) {
+            console.log('Chat', task[i].STOC_TYPE_CHANGE);
+        }
+        if (task[i].UNKONW) {
+            console.log('????', task[i].UNKNOWN);
+        }
+    }
+}
 proxy.listen(8911);
