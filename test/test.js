a2e041ab4f9b3454e01ba890665924153fb4964d
Jamezs Gladney <jamezs@angelofcode.com>
2014-07-20 06:03:02 -0400

    test fix, removed bad test

--- test/test.js
+++ test/test.js
@@ -102,3 +102,22 @@ describe('Test Network Connection Methods', function () {
         });
     });
 });
+/*
+var structureDefinition = require('../objectifier.js');
+
+var structureDefinition = require('../objectifier.js');
+var header = {
+    test: ['char', 10],
+    long: 'long'
+};
+var strut = structureDefinition(header);
+var out = strut.write({
+    test: 'a123456789',
+    long: "abcd    "
+});
+var validate = strut.read(out);
+console.log(validate)
+//console.log(validate.test.length, "a123456789".length, true);
+//console.log(validate.long.length, "abcd    ".length, true);
+console.log(validate.test, "a", true);
+console.log(validate.long, "abcd    ", true);*/
