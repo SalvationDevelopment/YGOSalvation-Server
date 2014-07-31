cf0a78bef45711b8e2a1d017092688e092087abd
Jamezs Gladney <panthrowzay@gmail.com>
2014-07-31 11:11:07 -0400

    fix

--- test/test.js
+++ test/test.js
@@ -10,7 +10,7 @@ console.log('Running test');
 var wsserver = net.createServer().listen(9003);
 var Primus = require('primus');
 var primus = new Primus(wsserver);
-var processIncomingTrasmission = require('./libs/processIncomingTrasmission.js');
+var processIncomingTrasmission = require('../server//libs/processIncomingTrasmission.js');
 var request = require('request');
 try {
     var server = require('../server/server.js');
