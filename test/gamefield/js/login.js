2bc6bf1d50b626df9b614d87f46b22d32459ac59
Jamezs Gladney <jamezs@angelofcode.com>
2014-09-11 19:31:48 -0400

    update, hand off.

--- test/gamefield/js/login.js
+++
@@ -1,5 +0,0 @@
-function login(){
-    u.getUnity().SendMessage("HubClient", "Login", "{'Username' : '" + ($('#username').val()) + "', 'Password' : '" + ($('#password').val()) + "', 'UID' : 'Unity', 'Version' : '998000'}");
-}
-
-
