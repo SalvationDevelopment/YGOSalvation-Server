7e6101822fc48f6e97197006c14c0ac4bdf613b1
Stefan Baiu <me.stefan.baiu@gmail.com>
2014-09-23 20:16:06 +0100

    implemented batch processing + db update

--- server/libs/carddb-update.js
+++ server/libs/carddb-update.js
@@ -2,15 +2,96 @@
 
 var http = require("http"),
     sqlite3 = require("sqlite3").verbose(),
-    path = "../../http/ygopro/cards.cdb",
-    database = null,
-    wikiaQueryString = "action=query&format=json&redirects&prop=revisions&rvprop=content",
-    cards = [],
-    stats = { success: 0, fail: 0 },
+    filepath = "../../http/ygopro/cards.cdb",
+    wikiaQuery = "/api.php?action=query&format=json&redirects&prop=revisions&rvprop=content&titles=",
+    events = require("events"),
+    phases = new events.EventEmitter(),
+    aliases = {},
+    statistics = [],
+    failQueue = [],
     log = console.log; // works (nodeJS) because the method was created using Function.prototype.bind
 
-function getUrlEncoding (match) {
-    switch (match) {
+function getDatabaseCards (callback) {
+    var database = new sqlite3.Database(filepath, sqlite3.OPEN_READONLY),
+        query = ["SELECT d.`id` AS id, d.`alias` AS alias, t.`name` AS name ",
+                 "FROM `datas` AS d INNER JOIN `texts` AS t ON d.`id` = t.`id` ",
+                 "WHERE d.`type` <> 16401;"].join(""),
+        cards = [],
+        queryError = null;
+    
+    // we get all the cards, except for tokens
+    database.each(query, function (error, row) {
+        var ids = null;
+        
+        if ( error ) {
+            queryError = error;
+            return;
+        }
+        
+        if ( row.alias ) {
+            ids = aliases[row.alias] || [];
+            ids.push(row.id);
+            aliases[row.alias] = ids;
+        } else {
+            cards.push(row);
+        }
+    });
+    
+    database.close();
+    
+    database.on("close", function () {
+        // throw error after db connection was closed
+        if ( queryError ) {
+            throw queryError;
+        }
+        
+        callback(cards);  
+    });
+}
+
+function updateDatabase (changes, callback)  {
+    var database = new sqlite3.Database(filepath, sqlite3.OPEN_READWRITE),
+        updateError = null;
+    
+    database.serialize(function () {
+        var query = "UPDATE `texts` SET `desc` = $desc WHERE `id` = $id",
+            statement = database.prepare(query);
+        
+        statement.on("error", function (error) {
+            updateError = error;
+        });
+        
+        Object.keys(changes).forEach(function (id) {
+            var desc = changes[id].lore;
+            
+            log("updating #", id);
+            statement.run({ $id: id, $desc:  desc });
+            
+            // wish I could use a join, but the sqlite syntax for UPDATE doesn't allow it
+            // http://sqlite.org/lang_update.html
+            (aliases[id] || []).forEach(function (alias) {
+                log("updating #", alias, " (alias for #", id, ")");
+                statement.run({ $id: alias, $desc: desc });
+            });
+        });
+        
+        statement.finalize();
+    });
+    
+    database.close();
+    
+    database.on("close", function () { 
+        // throw error after db connection was closed
+        if ( updateError ) {
+            throw updateError;
+        }
+        
+        callback();
+    });
+}
+
+function getUrlEncoding (character) {
+    switch (character) {
         case "'": return "%27";
         case ".": return "%2E";
         case "!": return "%21";
@@ -27,27 +108,10 @@ function encode (value) {
     return encodeURIComponent(value).replace(/[-_.!~*'()]/, getUrlEncoding);	
 }
 
-// adds a padding of 0's to the left if necessary
-function normalize (id) {
-    var stringId = id.toString(),
-        normalLength = 8,
-        actualLength = stringId.length,
-        padding = [];
-    
-    padding.length = normalLength - actualLength + 1;
-    return padding.join("0").concat(stringId);
-}
-
-function getWikiaPayload (id, name, callback) {
-    var titles = normalize(id), //.concat("|", name),
-        path = "/api.php?".concat(wikiaQueryString, "&titles=", encode(titles)),
+function getWikiaPayload (titles, callback) {
+    var titlesComponent = titles.join("|"),
+        path = wikiaQuery + encode(titlesComponent),
         options = { host: "yugioh.wikia.com", path: path };
-    
-    function triggerCallback (json) {
-        setTimeout(function () {
-            callback(json);
-        }, 0);
-    }
     
     function handleResponse (response) {
         var payload = [];
@@ -59,7 +123,9 @@ function getWikiaPayload (id, name, callback) {
         response.on("end", function () {
             var json = JSON.parse(payload.join(""));
 
-            triggerCallback(json);
+            // there's no need to call this in a timeout since
+            // the event is emitted within a process.nextTick callback
+            callback(json);
         });
     }
     
@@ -68,131 +134,237 @@ function getWikiaPayload (id, name, callback) {
     }
     
     http.get(options, handleResponse).on('error', handleRequestError);
+    log("request for ", titlesComponent ," was sent to wikia");
 }
-
-var failures = [];
 
 function getLoreReplacement (match, capture) {
     var indexOfPipe = capture.indexOf("|");
     
-    return indexOfPipe >= 0 ? capture.substr(indexOfPipe + 1) : capture;
+    return indexOfPipe >= 0 ? capture.substring(indexOfPipe + 1) : capture;
 }
 
-function parseWikiaPayload (payload) {
-    var pages = payload && payload.query && payload.query.pages || {}, 
-        key = "",
-        toBeParsed = "",
+function sanitize (data) {
+    var lore = data.lore;
+    
+    // e.g. [[Monster Card|monster]] -> monster
+    lore = lore.replace(/\[\[([\s\S]+?)\]\]/g, getLoreReplacement);
+    // e.g. <br /> -> (empty)
+    lore = lore.replace(/<[\s\S]+?>/g, "");
+    // ''Something goes here'' -> Something goes here
+    lore = lore.replace(/^''|''$/g, "");
+    
+    data.lore = lore;
+    
+    // add more clean-up code here, if needed
+    
+    return data;
+}
+
+function parseWikiaPage (page) {
+    var content = "",
+        body = null,
         matches = null,
         parts = null,
         parsed = null,
         i = 0,
         len = 0;
     
-    for ( key in pages ) {
-        if ( key !== "-1" && pages[key] ) {
-            toBeParsed = pages[key].revisions || [];
-            toBeParsed = toBeParsed[0] || {};
-            toBeParsed = toBeParsed["*"];
-            break;
-        }
-    }
+    parsed = {
+        title: page.title,
+        body: null
+    };
     
-    if ( toBeParsed ) {
-        matches = /\{\{CardTable2([\s\S]+)\}\}/.exec(toBeParsed);
+    content = page.revisions || [];
+    content = content[0] || {};
+    content = content["*"];
+    
+    if ( content ) {
+        matches =  /\{\{CardTable2([\s\S]+)\}\}/.exec(content);
         
         if ( matches ) {
             parts = matches[1].split(/\|(\w+)\s*=/);
-            parsed = {};
+
+            body = {};
             
             for (i = 1, len = parts.length - 1; i < len; i += 2) {
-                parsed[parts[i]] = parts[i + 1].trim();
+                if ( !body[ parts[i] ] ) {
+                    body[ parts[i] ] = parts[i + 1].trim();
+                }
+            }
+
+            // all cards should have a 'lore' section
+            if ( !body.lore ) { 
+                body = null;
             }
             
-            if ( !parsed.lore ) {
-                return null;
+            if ( body ) {
+                sanitize(body);
             }
             
-            // e.g. [[Monster Card|monster]] -> monster
-            parsed.lore = parsed.lore.replace(/\[\[([\s\S]+?)\]\]/g, getLoreReplacement);
-            // e.g. <br /> -> (empty)
-            parsed.lore = parsed.lore.replace(/<[\s\S]+?>/g, "");
-            // ''Something goes here'' -> Something goes here
-            parsed.lore = parsed.lore.replace(/^''|''$/g, "");
+            parsed.body = body;
         }
     }
     
     return parsed;
 }
 
-function update (card, callback) {
-    var id = card.alias || card.id,
-        name = card.name;
+function parseWikiaPayload (payload) {
+    var query = payload && payload.query || {},
+        pages = query.pages || {},
+        redirects = query.redirects || [],
+        links = {},
+        successCollection = [],
+        failCollection = [];
     
-    getWikiaPayload(id, name, function (json) {
-       var parsed = parseWikiaPayload(json);
-        
-        if ( parsed ) {
-            stats.success += 1;
-            log( parsed.lore );
-           //log(parsed);
-        } else { 
-            stats.fail += 1;
-            failures.push("#".concat(card.id, " ", card.name));
+    redirects.forEach(function (redirect) {
+        links[redirect.to] = redirect.from;
+    });
+    
+    function getQueryTitle (title) {
+        while ( links[title] ) {
+            title = links[title];
         }
+        return title;
+    }
+    
+    Object.keys(pages).forEach(function (key) {
+        var page = pages[key],
+            parsed = parseWikiaPage(page),
+            body = parsed.body;
         
-        callback();
+            parsed.queryTitle = getQueryTitle(parsed.title);
+        
+        (body ? successCollection : failCollection).push(parsed);
+    });
+    
+    return { 
+        successCollection: successCollection, 
+        failCollection: failCollection
+    };
+}
+
+function update (cards, queryComponentSelector, callback) {
+    var titles = [],
+        map = {};
+    
+    cards.forEach(function (card) {
+        var title = queryComponentSelector(card);
+        
+        map[title] = card;
+        titles.push(title);
+    });
+    
+    getWikiaPayload(titles, function (json) {
+       var results = parseWikiaPayload(json),
+           successCollection = results.successCollection,
+           failCollection = results.failCollection,
+           stats = statistics[statistics.length - 1],
+           changes = {};
+        
+        successCollection.forEach(function (parsed) {
+            var id = map[parsed.queryTitle].id;
+            changes[id] = parsed.body;
+        });
+        
+        failCollection.forEach(function (parsed) {
+           failQueue.push(map[parsed.queryTitle]); 
+        });
+        
+        stats.success += successCollection.length;
+        stats.fail += failCollection.length;
+        
+        log(stats);
+        log("failQueue: ", failQueue);
+         
+        updateDatabase(changes, callback);
     });
 }
 
-function processCards (index) {
-    if ( index < cards.length ) {
-        log("updating ", cards[index], " (", (index + 1), "/", cards.length, ")");
-        
-        update(cards[index], function () {
-            log("success: ", stats.success, ", fail: ", stats.fail);
-            log("failures: ", failures);
-            
-            processCards(index + 1);
+function processCards (iterator, queryComponentSelector, completionEventName) {
+    var cards = iterator.getNextBatch();
+    
+    if ( cards.length ) {
+        update(cards, queryComponentSelector, function () {
+            processCards(iterator, queryComponentSelector, completionEventName);
+        });
+    } else {
+        process.nextTick(function () {
+            phases.emit(completionEventName);
         });
     }
 }
 
-function handleDatabaseOpen () {
-    log("database was opened successfully");
-}
-
-function handleDatabaseClose () {
-    log("database was closed successfully");
-}
-
-database = new sqlite3.Database(path, sqlite3.OPEN_READWRITE);
-
-database.on("open", handleDatabaseOpen);
-database.on("close", handleDatabaseClose);
-
-function handleQueryRow (error, row) {
-    if ( error ) {
-        throw error;
+function createBatchIterator(list, start, length) {
+    var from = start;
+    
+    function getNextBatch () {
+        var end = from + length,
+            batch = list.slice(from, end);
+            
+        from = end;
+            
+        return batch;
     }
     
-    cards.push(row);
-}
-
-function handleQueryCompleted (error, count) {
-    if ( error ) {
-        throw error;
+    function reset () {
+        from = start;
     }
     
-    if ( count > 0 ) {
-        processCards(0);
-    }
+    return {
+        getNextBatch: getNextBatch,
+        reset: reset
+    };
 }
 
-// TODO: don't get cards with aliases, (when update the db) update card where id OR alias instead
+// adds a padding of 0's to the left if necessary
+function normalize (id) {
+    var stringId = id.toString(),
+        normalLength = 8,
+        actualLength = stringId.length,
+        padding = [];
+    
+    padding.length = normalLength - actualLength + 1;
+    return padding.join("0") + stringId;
+}
 
-var query = "SELECT `datas`.`id` AS id, `datas`.`alias` as alias, `texts`.`name` as name FROM `datas`, `texts` WHERE `datas`.`id` = `texts`.`id` AND `datas`.`type` <> 16401 AND `datas`.`alias` = 0";
+function queryUsingId (card) {
+    return normalize(card.id);
+}
 
-database.each(query, {}, handleQueryRow, handleQueryCompleted);
+function queryUsingName (card) {
+    return card.name;
+}
 
-// waits until all queries have completed
-database.close();
+function processDbCards (dbCards, callback) {
+    var dbCardsIterator = createBatchIterator(dbCards, 0, 5);
+    
+    log("db cards processing started");
+    statistics.push({ name: "initial iteration", success: 0, fail: 0 });
+
+    processCards(dbCardsIterator, queryUsingId, "dbCardsProcessingCompleted");
+        
+    phases.on("dbCardsProcessingCompleted", function () {
+        log("db cards processing ended");
+        callback();
+    });
+}
+
+function processFailedCards () {
+    var failedCards = failQueue.slice(0),
+        failedCardsIterator = createBatchIterator(failedCards, 0, 5);
+    
+    failQueue.length = 0;
+        
+    log("failed cards processing started");
+    statistics.push({ name: "failed cards iteration", success: 0, fail: 0 });
+        
+    processCards(failedCardsIterator, queryUsingName, "failedCardsProcessingCompleted");
+    
+    phases.on("failedCardsProcessingCompleted", function () {
+        log("failed cards processing ended");
+    });
+}
+
+getDatabaseCards(function (dbCards) {
+    processDbCards(dbCards, processFailedCards);
+});
