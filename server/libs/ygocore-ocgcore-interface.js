70d3f1c9ed51f642fe462d650aa9d5ed61d8f88a
Jamezs Gladney <jamezs@angelofcode.com>
2014-09-29 00:20:39 -0400

    exodius 1

--- server/libs/ygocore-ocgcore-interface.js
+++ server/libs/ygocore-ocgcore-interface.js
@@ -1,14 +1,18 @@
 /* jshint node:true */
 /* global module */
 /* jslint browser : true */
-var sqlite3 = require('sqlite3').verbose(),
+'use strict';
+
+var sqlite3 = require('sqlite3').verbose() /* access databse file */ ,
+    fs = require('fs') /* access file system */ ,
     ffi = require('ffi') /* allows dynamic linking of the ocgapi.dll, critical; */ ,
     ref = require('ref') /* allows use of C++ pointers for C++ JS interactions, critical */ ,
     struct = require('ref-struct') /* allows use of C++ structures for C++ JS interactions, critical */ ,
     queryfor = require('./sql-queries');
 
 
-function constructDatabase(targetDB) {
+function constructDatabase(targetDB, targetFolder) {
+    // create instance of card database in memory 2MB, prevents sychronous read request and server lag.
     var database,
         cards = {};
 
@@ -27,41 +31,74 @@ function constructDatabase(targetDB) {
     database.end();
 
     return function (request) {
+        //function used by the core to process DB
         var code = request.code;
-        
+
         return struct({
             code: code,
-            alias: cards[code].alias || code,
+            alias: cards[code].alias || 0,
             setcode: cards[code].setcode || 0,
             type: cards[code].type || 0,
-            level: cards[code].level ||1,
-            attribute: cards[code].attribute ||0,
-            race: cards[code].race ||0,
+            level: cards[code].level || 1,
+            attribute: cards[code].attribute || 0,
+            race: cards[code].race || 0,
             attack: cards[code].attack || 0,
-            defence: cards[code].defense ||0
+            defence: cards[code].defense || 0
         });
     };
 }
-var card_reader = constructDatabase('../ygocore/card.cdb');
 
-module.exports = function (scripts) {
-    function getScript(identificationNumber) {
-        /* pull the script out the cache and send it into the core as a buffer */
-        return new Buffer(scripts['c' + identificationNumber]);
+function constructScripts(targetFolder) {
+    //create instance of all scripts in memory 14MB, prevents sychronous read request and server lag.
+    var filelist = [];
+    var scripts = {};
+
+    function readFile(filename) {
+        // loop through a list of filename asynchronously and put the data into memory.
+        fs.readfile(targetFolder + '/' + filename, function (error, data) {
+            scripts[filename] = data;
+            filelist.shift();
+            if (filelist.length > 0) {
+                readFile(filelist[0]);
+            } else return;
+        });
     }
 
+    fs.readdir(targetFolder, function (error, list) {
+        // get list of script filenames in the folder.
+        if (error) {
+            throw console.log(error);
+        }
+        filelist = list;
+    });
 
+    return function (id) {
+        //function used by the core to process scripts
+        var filename = 'c' + id;
+        var output = new Buffer(scripts[filename]);
+        return output;
+    };
+}
+
+module.exports.configurations = [{
+ card_reader : constructDatabase('../ygocore/card.cdb'),
+ script_reader : constructScripts('../ygocore/scripts/')
+}];
+
+
+module.exports.core = function (settings) {
+    // create new instance of flourohydride/ygopro/ocgcore
+    
     this.ocgapi = ffi.Library(__dirname + '/ocgcore.dll', {
-        'set_script_reader': ['void', getScript],
-        'set_card_reader': ['void', card_reader],
+        'set_script_reader': ['void', settings.script_reader],
+        'set_card_reader': ['void', settings.card_reader],
         'set_message_handler': ['void', console.log],
         'create_duel': ['pointer', ['uint32']],
         'start_duel': ['void', ['pointer', 'int']],
         'end_duel': ['void', ['pointer']],
         'set_player_info': ['void', ['pointer', 'int32', 'int32', 'int32', 'int32']],
         'get_log_message': ['void', ['pointer', 'byte*']],
-        'get_message': ['int32', ['pointer' /*, get_message_pointer*/
-]],
+        'get_message': ['int32', ['pointer' /*, get_message_pointer*/]],
         'process': ['int32', ['pointer']],
         'new_card': ['void', ['pointer', 'uint32', 'uint8', 'uint8', 'uint8', 'uint8', 'uint8']],
         'new_tag_card': ['void', ['pointer', 'uint32', 'uint8', 'uint8']],
