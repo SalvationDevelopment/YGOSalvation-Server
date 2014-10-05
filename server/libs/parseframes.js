a0870b2b53fdd8cdf580cf618ce167e2ebdc3b8d
Jamezs Gladney <jamezs@angelofcode.com>
2014-10-05 06:15:39 -0400

    fixed

--- server/libs/parseframes.js
+++ server/libs/parseframes.js
@@ -1,4 +1,6 @@
 /* jshint node : true */
+var util         = require("util");
+var EventEmitter = require("events").EventEmitter;
 
 module.exports = function () {
     var memory = new Buffer([]);
@@ -6,18 +8,18 @@ module.exports = function () {
     this.input = function (buffer) {
         var x = true;
         var output = [];
-        console.log('before', memory.length, 'bytes in memory');
+        //console.log('before', memory.length, 'bytes in memory');
         memory = Buffer.concat([memory, buffer]);
-        console.log('concated', memory.length);
+        //console.log('concated', memory.length);
         while (x === true && memory.length > 2) {    
             
             var frame_length = memory[0] + memory[1];
-            console.log('read', frame_length, '(+2) of', memory.length, 'bytes');
+            //console.log('read', frame_length, '(+2) of', memory.length, 'bytes');
             if ((memory.length - 2) < frame_length) {
-                console.log('not enough');
+                //console.log('not enough');
                 x = false;
             } else {
-                var recordOfBuffer = memory.slice(0, frame_length).toJSON();
+                var recordOfBuffer = memory.slice(2, frame_length).toJSON();
 
                 output.push(recordOfBuffer.data);
                 if (memory.length === (frame_length + 2)) {
@@ -26,10 +28,10 @@ module.exports = function () {
                 } else {
                     memory = memory.slice((frame_length + 2));
                 }
-                console.log('after', memory.length);
+                //console.log('after', memory.length);
             }
         }
-        console.log('----',output);
+        //console.log('----',output);
         return output;
     };
     return this;
