aec036821e987fa9d9c071853feac788dbb8ea88
Jamezs Gladney <jamezs@angelofcode.com>
2014-10-05 03:40:15 -0400

    not bugged code

--- server/libs/parseframes.js
+++ server/libs/parseframes.js
@@ -6,21 +6,20 @@ module.exports = function () {
     this.input = function (buffer) {
         var x = true;
         var output = [];
-        var xx = 0;
-        while (x === true && xx < 10) {
-            xx++;
-            console.log('before', memory.length, 'bytes in memory');
-            memory = Buffer.concat([memory, buffer]);
-            console.log('concated', memory.length);
+        console.log('before', memory.length, 'bytes in memory');
+        memory = Buffer.concat([memory, buffer]);
+        console.log('concated', memory.length);
+        while (x === true && memory.length > 2) {    
+            
             var frame_length = memory[0] + memory[1];
             console.log('read', frame_length, 'of', memory.length, 'bytes');
             if ((memory.length - 2) < frame_length) {
                 console.log('not enough');
-                x = true;
+                x = false;
             } else {
                 var recordOfBuffer = memory.slice(2, frame_length).toJSON();
-                console.log(recordOfBuffer.data.length);
                 var frame = new Buffer(recordOfBuffer.data);
+                console.log(frame.length);
                 output.push(frame);
                 if (memory.length === (frame_length + 2)) {
                     memory = new Buffer([]);
@@ -32,6 +31,7 @@ module.exports = function () {
                 console.log('after', memory.length);
             }
         }
+        console.log('----',output);
         return output;
     };
     return this;
