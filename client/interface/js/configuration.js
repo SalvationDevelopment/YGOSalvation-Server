2a27f2c43d29cb6c577af3be3e59606d907018d7
Jamezs Gladney <jamezs@angelofcode.com>
2014-12-10 04:42:07 -0500

    Concat HTML pages, config fix

--- client/interface/js/configuration.js
+++ client/interface/js/configuration.js
@@ -2,7 +2,7 @@
 var developmentstage = {
     "production": "http://ygopro.us/launcher.html",
     "stage": "http://dev.ygopro.us/launcher.html",
-    "development": "http://127.0.0.1/launcher.html"
+    "development": "http://127.0.0.1:8080/launcher.html"
 };
 var mode = 'development';
 
