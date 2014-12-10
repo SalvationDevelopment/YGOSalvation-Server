3a86c2a16e91e5886e367e6869fbd262c47ec6bb
Jamezs Gladney <jamezs@angelofcode.com>
2014-12-10 03:55:20 -0500

    windows node-webkit update, UI udate

--- client/interface/js/configuration.js
+++ client/interface/js/configuration.js
@@ -1,5 +1,13 @@
+/* jslint node : true */
 var developmentstage = {
     "production": "http://ygopro.us/launcher.html",
     "stage": "http://dev.ygopro.us/launcher.html",
     "development": "http://127.0.0.1/launcher.html"
 };
+var mode = 'development';
+
+if (mode === 'development') {
+    try {
+        require('nw.gui').Window.get().showDevTools();
+    } catch (error) {}
+}
