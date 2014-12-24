99a1618e4126182564737132131bae779b59289a
Jamezs Gladney <jamezs@angelofcode.com>
2014-12-23 19:53:42 -0500

    Exports logic, linting

--- server/libs/gamelist.js
+++ server/libs/gamelist.js
@@ -1,134 +1,113 @@
-/*jshint node : true */
-var http = require('http');
-var Primus = require('primus');
-var Rooms = require('primus-rooms');
-// This object acts similar to a Redis server, could be replaced with on but its the gamelist state.
- var gamelist = {};
+/*jslint  node: true, plusplus: true, white: false*/
+// Gamelist object acts similar to a Redis server, could be replaced with on but its the gamelist state.
+(function () {
+    'use strict';
+    var primus,
+        gamelist = {},
+        http = require('http'),
+        Primus = require('primus'),
+        Rooms = require('primus-rooms'),
+        primusServer = http.createServer().listen(24555);
 
- // Things that only the initial starting p
-function messageListener(message){
-    var core_message_raw = message.core_message_raw;
-    var port = message.port;
-    var data = message.data;
-    return handleCoreMessage(core_message_raw, port, data);
-}
- function handleCoreMessage(core_message_raw, port, data) {
-     var core_message_txt = core_message_raw.toString();
-     //console.log(core_message_txt);
-     if (core_message_txt.indexOf("::::") < 0) {
-         return;
-     }
-     var core_message = core_message_txt.split('|');
-     //console.log(core_message);
-     core_message[0] = core_message[0].trim();
-     try {
-         switch (core_message[0]) {
-         case ('::::network-end'):
-             {
-                 //servercallback('kill', gamelist);
-             }
-             break;
-         case ('::::join-slot'):
-             {
-                 //socket.hostString = core_message[1];
-                 var join_slot = parseInt(core_message[2], 10);
-                 if (join_slot === -1) {
-                     return;
-                 }
-                 gamelist[core_message[1]].players[join_slot] = core_message[3];
-                 gamelist[core_message[1]].port = port;
+    function handleCoreMessage(core_message_raw, port, data) {
+        if (core_message_raw.toString().indexOf("::::") < 0) {
+            return;
+        }
 
-                 //servercallback('update', gamelist);
-             }
-             break;
-         case ('::::leave-slot'):
-             {
+        var chat,
+            join_slot,
+            leave_slot,
+            lock_slot,
+            core_message = core_message_raw.toString().split('|');
 
-                 var leave_slot = parseInt(core_message[2], 10);
-                 if (leave_slot === -1) {
-                     return;
-                 }
-                 gamelist[core_message[1]].players[leave_slot] = null;
-                 //servercallback('update', gamelist);
-             }
-             break;
-         case ('::::spectator'):
-             {
 
-                 gamelist[core_message[1]].spectators = parseInt(core_message[2], 10);
-                 //servercallback('update', gamelist);
+        try {
+            core_message[0] = core_message[0].trim();
+            switch (core_message[0]) {
 
-             }
-             break;
-         case ('::::lock-slot'):
-             {
+            case ('::::join-slot'):
+                join_slot = parseInt(core_message[2], 10);
+                if (join_slot === -1) {
+                    return;
+                }
+                gamelist[core_message[1]].players[join_slot] = core_message[3];
+                gamelist[core_message[1]].port = port;
+                break;
 
-                 var lock_slot = parseInt(core_message[2], 10);
-                 gamelist[core_message[1]].locked[lock_slot] = Boolean(core_message[2]);
-                 //servercallback('update', gamelist);
-             }
-             break;
-         case ('::::endduel'):
-             {
-                 //do ranking here
+            case ('::::leave-slot'):
+                leave_slot = parseInt(core_message[2], 10);
+                if (leave_slot === -1) {
+                    return;
+                }
+                gamelist[core_message[1]].players[leave_slot] = null;
+                break;
 
-                 delete gamelist[core_message[1]];
-                 //servercallback('update', gamelist);
-             }
-             break;
-         case ('::::startduel'): // rockpaperscissors
-             {
+            case ('::::spectator'):
+                gamelist[core_message[1]].spectators = parseInt(core_message[2], 10);
+                break;
 
-                 gamelist[core_message[1]].started = true;
-                 //servercallback('update', gamelist);
-             }
-             break;
-         case ('::::chat'):
-             {
-                 var chat ='';        
-             }
-             break;
-         }
-     } catch (error_message) {
-         console.log(error_message);
-     }
-     return gamelist;
- }
+            case ('::::lock-slot'):
+                lock_slot = parseInt(core_message[2], 10);
+                gamelist[core_message[1]].locked[lock_slot] = Boolean(core_message[2]);
+                break;
 
- var primus = new Primus(primusServer, {
-     parser: 'JSON'
- });
- primus.use('rooms', Rooms);
- primus.on('connection', function (socket) {
-     socket.on('data', function (data) {
-         data = data || {};
-         var action = data.action;
-         switch (action) {
-         case ('join'):
-             {
-                 socket.join('activegames', function () {
-                     socket.write(JSON.stringify(gamelist));
-                 });
-             }
-             break;
-         case ('leave'):
-             {
-                 socket.leave('activegames');
-             }
-             break;
-         default:
-             {
-                 console.log(data);
-             }
-         }
-     });
- });
- primus.on('disconnection', function (socket) {
-     
- });
+            case ('::::endduel'):
+                delete gamelist[core_message[1]];
+                break;
 
- primus.on('error', function (socket) {
-    
- });
+            case ('::::startduel'):
+                gamelist[core_message[1]].started = true;
+                break;
 
- var primusServer = http.createServer().listen(24555);
+            case ('::::chat'):
+                chat = core_message[1] + '|' + core_message[2];
+                break;
+
+            }
+            primus.room('activegames').write(JSON.stringify(gamelist));
+            return gamelist;
+        } catch (error_message) {
+            console.log(error_message);
+            return null;
+        }
+    }
+
+    module.exports = function messageListener(message) {
+        return handleCoreMessage(message.core_message_raw, message.port, message.data);
+    };
+
+    primus = new Primus(primusServer, {
+        parser: 'JSON'
+    });
+    primus.use('rooms', Rooms);
+    primus.on('connection', function (socket) {
+        socket.on('data', function (data) {
+            data = data || {};
+            var action = data.action;
+            switch (action) {
+            case ('join'):
+                socket.join('activegames', function () {
+                    socket.write(JSON.stringify(gamelist));
+                });
+                break;
+
+            case ('leave'):
+                socket.leave('activegames');
+                break;
+
+            default:
+                console.log(data);
+
+            }
+        });
+    });
+    primus.on('disconnection', function (socket) {
+        //nothing required
+    });
+
+    primus.on('error', function (socket) {
+        //nothing required
+    });
+
+
+}());
--- server/libs/ircbot.js
+++ server/libs/ircbot.js
@@ -12,19 +12,23 @@ var bot = new irc.Client(config.server, config.botName, {
     channels: config.channels
 });
 
-var output = {
-    ircSayPublic: function ircSayPublic(message) {
-        bot.say("#lobby", message);
-    },
-    ircSayPrivate: function ircSayPrivate(message) {
-        bot.say("#lobby", message);
-    }
-};
-function(){
-var updateinstance = spawn('git', ['pull']);
-                updateinstance.on('close', function preformupdate() {
-                    spawn('node', ['update.js'], {
-                        cwd: './http'
-                    });
-                });
+
+ircSayPublic: function ircSayPublic(message) {
+    bot.say("#lobby", message);
+}
+ircSayPrivate: function ircSayPrivate(message) {
+    bot.say("#server", message);
+}
+
+function () {
+    var updateinstance = spawn('git', ['pull']);
+    updateinstance.on('close', function preformupdate() {
+        spawn('node', ['update.js'], {
+            cwd: './http'
+        });
+    });
+}
+
+module.exports = {
+    notify: ircSayPrivate
 }
