ea1e36b2653b3066c8ec7d8ab84c9a836429bcd7
Jamezs Gladney <jamezs@angelofcode.com>
2014-12-23 19:02:10 -0500

    Missing package, Server.js Linted

--- server/server.js
+++ server/server.js
@@ -1,40 +1,5 @@
-/* jslint node : true */
-/* Salvation Server Main Process
-- This script starts the main server side program and maintains all subprocesses.
-*/
-(function () {;
-    console.log('YGOPro Salvation Server - Saving Yu-Gi-Oh!'); //Title
-    process.title = 'Salvation';
-
-    // load modules built into Node.js
-    var net = require('net');
-    var http = require('http');
-    var cluster = require('cluster');
-    var numCPUs = require('os').cpus().length;
-    var spawn = require('child_process').spawn;
-    var fs = require('fs');
-
-    /*
-Load modules pulled down from NPM, externals
-if you dont run `npm install` these requires will fail.
-Everything here, and more should be in the package.json one directory
-level above this one.
-*/
-    var gith = require('gith');
-
-    var static = require('node-static');
-    var WebSocketServer = require('ws').Server;
-
-    //load modules that are an internal part of the application
-    var ircManager = require('./libs/ircbot.js');
-    var gamelistManager = require('./libs/gamelist.js');
-    var processIncomingTrasmission = require('./libs/processIncomingTrasmission.js');
-
-
-
-    // error handling and cleanup function. Makes sure the server doesnt crash like the way DevPro does.
-
-    /*
+/*jslint  node: true, plusplus: true, white: false*/
+/*
 Start various sub-servers.
 --------------------------
 - Gamelist on port 24555
@@ -42,84 +7,91 @@ Start various sub-servers.
 - YGOPro Web listener on port 8913, browser version connects to this port,
   and is stripped and routed to the same place as the application.
 - HTTP server running static files out of port 80
-- Githooks listener on port 4901. Self updating system.
 */
 
-    function setupWorker() {
-        var worker = cluster.fork();
-        worker.on('message', function () {
-            var gamelist = gamelistManager();
-            worker.send({
-                messagetype: gamelist,
-                gamelist: gamelist
-            });
-        });
-    }
+//Title
+console.log('YGOPro Salvation Server - Saving Yu-Gi-Oh!');
+process.title = 'Salvation';
 
-    if (cluster.isMaster) {
-        for (var i = 0; i < numCPUs; i++) {
-            setupWorker();
-        }
-        cluster.on('exit', function (worker, code, signal) {
-            console.log('worker ' + worker.process.pid + ' died');
-            cluster.fork();
-        });
-        //process is suppose to do.
-        var githubServer = gith.create(4901);
+(function () {
+    /* Salvation Server Main Process
+- This script starts the main server side program and maintains all subprocesses.
+*/
+    'use strict';
+
+    //buit ins
+    var net = require('net'),
+        http = require('http'),
+        cluster = require('cluster'),
+        numCPUs = require('os').cpus().length,
 
 
-        //When http://gitub.com/salvationdevelopment contacts you, update the current git.
-        //We want to limit this to just the master cluster.
-        githubServer({
-            owner: 'SalvationDevelopment'
-        }).on('all', function (payload) {
-            var updateinstance = spawn('git', ['pull']);
-            updateinstance.on('close', function preformupdate() {
-                spawn('node', ['update.js'], {
-                    cwd: './http'
+        //from npm, run 'npm install', and keep project.json upto date.
+        staticserv = require('node-static'),
+        WebSocketServer = require('ws').Server,
+
+        //from the project itself.
+        ircManager = require('./libs/ircbot.js'),
+        gamelistManager = require('./libs/gamelist.js'),
+        processIncomingTrasmission = require('./libs/processIncomingTrasmission.js');
+
+
+    (function establishCluster() {
+        var ygoserver,
+            httpServer,
+            clusterIterator = 0;
+
+        function setupWorker() {
+            var worker = cluster.fork();
+            worker.on('message', function () {
+                var gamelist = gamelistManager();
+                worker.send({
+                    messagetype: gamelist,
+                    gamelist: gamelist
                 });
             });
-        });
+        }
 
-        //deal with gamelist updates
-
-
-    } else {
-        var ygoserver; //listen(8911);
-
-        /*
-    Static file server:
-    Via nginx /server/http/ is routed to http://ygopro.us/
-    */
-        var httpServer = new static.Server('./http'); //main http server is clustered.
-        http.createServer(function (request, response) {
-            request.addListener('end', function () {
-                httpServer.serve(request, response);
-            }).resume();
-        }).listen(80);
-
-
-        // When a user connects, create an instance and allow the to duel, clean up after.
-        var ygoserver = net.createServer(function (socket) {
-            socket.setNoDelay(true);
-            socket.active_ygocore = false;
-            socket.active = false;
-            socket.on('data', function (data) {
-                processIncomingTrasmission(data, socket, data);
-
+        if (cluster.isMaster) {
+            for (clusterIterator; clusterIterator < numCPUs; clusterIterator = clusterIterator++) {
+                setupWorker();
+            }
+            cluster.on('exit', function (worker, code, signal) {
+                console.log('worker ' + worker.process.pid + ' died/r/n' +
+                    code, signal);
+                cluster.fork();
             });
-            socket.on('close', function () {
-                //nothing needed.
-            });
-            socket.on('error', function () {
-                //nothing needed
-            });
-            socket.setTimeout(300000, function () {
-                socket.end();
-            });
-        });
-        ygoserver.listen(8911);
+            //exiting master process
+        } else {
+            //listen on 80, main http server is clustered.
+            httpServer = new staticserv.Server('./http');
+
+            http.createServer(function (request, response) {
+                request.addListener('end', function () {
+                    httpServer.serve(request, response);
+                }).resume();
+            }).listen(80);
 
 
-    }
-}());
+            // When a user connects, create an instance and allow the to duel, clean up after.
+            ygoserver = net.createServer(function (socket) {
+                socket.setNoDelay(true);
+                socket.active_ygocore = false;
+                socket.active = false;
+                socket.on('data', function (data) {
+                    processIncomingTrasmission(data, socket, data);
+                });
+                socket.on('close', function () {
+                    //nothing needed.
+                });
+                socket.on('error', function () {
+                    //nothing needed
+                });
+                socket.setTimeout(300000, function () {
+                    socket.end();
+                });
+            });
+            ygoserver.listen(8911);
+        } // end cluster split
+    }()); // end cluster setup
+}()); // end main
