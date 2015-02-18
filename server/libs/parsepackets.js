7b93c0b2987dba81b9c2f66e76ea352e3659f4b0
Jamezs Gladney <jamezs@angelofcode.com>
2015-02-18 17:31:09 -0500

    revert

--- server/libs/parsepackets.js
+++ server/libs/parsepackets.js
@@ -1,27 +1,25 @@
-/*jslint node: true */
+/* jshint node: true */
 var enums = require('./enums.js');
 module.exports = function parsePackets(command, message) {
-    'use strict';
-    var task = [],
-        buffer_read_position = 0,
-        read = message.length,
-        packet;
+    var task = [];
+    var buffer_read_position = 0;
     //The message is stripped and turned into a normal packet so we can understand it as:
     //{length, +length, type of message, the message itself }
     //the server may send more than one at a time so lets take it one at a time.
+    while (buffer_read_position < message.length) {
+        var read = message[buffer_read_position] + message[(buffer_read_position + 1)];
+        var packet = {
+            LENGTH: read,
+            message: message.slice((buffer_read_position + 3), (buffer_read_position + 1 + read)),
+            readposition: 0
+        };
+        packet[command] = enums[command][message[(buffer_read_position + 2)]] || message[(buffer_read_position + 2)];
 
-    packet = {
-        LENGTH: read,
-        message: message.slice((buffer_read_position + 3), (buffer_read_position + 1 + read)),
-        readposition: 0
-    };
-    packet[command] = enums[command][message[(buffer_read_position)]] || message[(buffer_read_position)];
 
+        task.push(packet);
 
-    task.push(packet);
-
-    buffer_read_position = buffer_read_position + 2 + read;
-
+        buffer_read_position = buffer_read_position + 2 + read;
+    }
     return task;
 };
 
