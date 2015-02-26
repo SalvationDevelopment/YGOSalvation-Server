2761a50f93d5b8094fc5c1d725a87e708a13cafb
Jamezs Gladney <jamezs@angelofcode.com>
2015-02-26 05:36:14 -0500

    more manual mode

--- server/libs/manualmode.js
+++ server/libs/manualmode.js
@@ -2,27 +2,67 @@
 
 var primus,
     manualgamelist = {},
+    manualgamelistSecret = {},
+    fs = require('fs'),
     http = require('http'),
     Primus = require('primus'),
     Rooms = require('primus-rooms'),
     primusServer = http.createServer().listen(24556), // +1 from gamelist
-    message_irc = require('./custom_error.js');
+    message_irc = require('./custom_error.js'),
+    banlist = fs.readFileSync('../http/ygopro/lflist.conf');
 
 
-function Room() {
+primus = new Primus(primusServer, {
+    parser: 'JSON'
+});
+
+function coinFlip() {
+    'use strict';
+    return Math.floor(Math.random() * 2);
+}
+
+function join(socket, id) {
+    'use strict';
+    if (manualgamelist[id].gamestate !== 'started') {
+        if (!manualgamelist[id].lobby.player1) {
+            manualgamelist[id].lobby.player1 = socket.identity;
+            socket.instance = 'player1';
+        } else if (!manualgamelist[id].lobby.player2) {
+            manualgamelist[id].lobby.player2 = socket.identity;
+            socket.instance = 'player2';
+        } else if (!manualgamelist[id].lobby.player3 && manualgamelist[id].mode === 'tag') {
+            manualgamelist[id].lobby.player3 = socket.identity;
+            socket.instance = 'player3';
+        } else if (!manualgamelist[id].lobby.player4 && manualgamelist[id].mode === 'tag') {
+            manualgamelist[id].lobby.player4 = socket.identity;
+            socket.instance = 'player4';
+        } else {
+            manualgamelist[id].lobby.spectators.push(socket.identity);
+            socket.instance = 'spectator';
+        }
+    } else {
+        manualgamelist[id].lobby.spectators.push(socket.identity);
+        socket.instance = 'spectator';
+    }
+}
+
+function leave(socket, id) {
+    'use strict';
+    manualgamelist[id].lobby[socket.instance] = false;
+    socket.instance = '';
+}
+
+function Room(id) {
     'use strict';
     return {
+        id: id,
         gamestate: 'off',
-        gametype: 'single',
+        mode: 'single',
         lobby: {
-            player1_username: '',
-            player2_username: '',
-            player3_username: '',
-            player4_username: '',
-            player1_loaded: false,
-            player2_loaded: false,
-            player3_loaded: false,
-            player4_loaded: false,
+            player1: false,
+            player2: false,
+            player3: false,
+            player4: false,
             spectators: 0
         },
         player1_rps_choice: undefined,
@@ -54,113 +94,8 @@ function Room() {
     };
 }
 
-function InstanceConstructor() {
-    return {
-        player1: false,
-        player2: false,
-        player3: false,
-        player4: false,
-        spectators: []
-    };
-}
 
-function NetworkInstanceConstructor() {
-    var game = new GameConstructor();
-    var instance = new InstanceConstructor();
-    var network = net.createServer(function (socket) {
-        /* socket is the concept of a single connection,
-        look at it as a person and we are doing 'work' on that person. */
-        socket.on('data', function (data) {
-            processCommunication(data, socket);
-        });
-        socket.on('close', function () {
-
-        });
-        socket.on('error', function () {
-
-        });
-    });
-
-
-    function assignSlot(socket) {
-        if (!instance.player1) {
-            instance.player1 = socket;
-            socket.instance = 'player1';
-        } else if (!game.player2) {
-            instance.player2 = socket;
-            socket.instance = 'player2';
-        } else if (!game.player3 && game.mode === 'tag') {
-            instance.player3 = socket;
-            socket.instance = 'player3';
-        } else if (!game.player4 && game.mode === 'tag') {
-            instance.player4 = socket;
-            socket.instance = 'player4';
-        } else {
-            var place = instance.spectators;
-            instance.spectators.push(socket);
-            socket.instance = 'spectator';
-        }
-    }
-
-    function publicPublication(data) {
-        for (var players in instance) {
-            if (players !== 'spectators' && instance[players] !== null && instance.hasOwnProperty(players)) {
-                players.write(JSON.stringify(data));
-            }
-        }
-        instance.forEach(function (spectator) {
-            spectator.write(JSON.stringify(data));
-        });
-    }
-
-    function coinFlip() {
-        return Math.floor(Math.random() * 2);
-    }
-
-    function processCommunication(data, socket) {
-        if (!socket.instance) {
-            assignSlot(socket);
-        }
-        if (data.task === 'username') {
-            socket.name = data.username;
-        }
-        if (data.task === 'chat') {
-            publicPublication(data);
-        }
-        if (data.task === 'leave') {
-            instance[socket.instance] = null;
-        }
-        if (data.task === 'kick') {
-            if (instance[socket.instance] === 'player1') {
-                instance[data.target] = null;
-            }
-        }
-        if (data.task === 'duel') {
-            assignSlot(socket);
-        }
-        if (data.task === 'start') {
-            game.gamestate = 'dieroll';
-            publicPublication({
-                game: game
-            });
-            var choices = (game.mode === 'single') ? ['player1', 'player2'] : ['player1', 'player3'];
-            game.choosingPlayer = choices[coinFlip()];
-            instance[game.choosingPlayer].write({
-                task: 'pickStart'
-            });
-        }
-        if (data.task === 'pickStart') {
-            if (socket.instance === game.choosingPlayer) {
-                game.activePlayer = data.choice;
-                publicPublication({
-                    game: game
-                });
-            }
-        }
-    }
-    return this;
-}
-function Room(id) {
+function RoomSecret(id) {
     'use strict';
     return {
         id: id,
@@ -176,9 +111,54 @@ function Room(id) {
 
     };
 }
-primus = new Primus(primusServer, {
-    parser: 'JSON'
-});
+
+
+/*function processCommunication(data, socket) {
+    if (!socket.instance) {
+        assignSlot(socket);
+    }
+    if (data.task === 'username') {
+        socket.name = data.username;
+    }
+    if (data.task === 'chat') {
+        publicPublication(data);
+    }
+    if (data.task === 'leave') {
+        instance[socket.instance] = null;
+    }
+    if (data.task === 'kick') {
+        if (instance[socket.instance] === 'player1') {
+            instance[data.target] = null;
+        }
+    }
+    if (data.task === 'duel') {
+        assignSlot(socket);
+    }
+    if (data.task === 'start') {
+        game.gamestate = 'dieroll';
+        publicPublication({
+            game: game
+        });
+        var choices = (game.mode === 'single') ? ['player1', 'player2'] : ['player1', 'player3'];
+        game.choosingPlayer = choices[coinFlip()];
+        instance[game.choosingPlayer].write({
+            task: 'pickStart'
+        });
+    }
+    if (data.task === 'pickStart') {
+        if (socket.instance === game.choosingPlayer) {
+            game.activePlayer = data.choice;
+            publicPublication({
+                game: game
+            });
+        }
+    }
+}
+*/
+function updateSubsGamelist(socket) {
+    'use strict';
+    socket.write(JSON.stringify(manualgamelist));
+}
 
 primus.use('rooms', Rooms);
 primus.on('connection', function (socket) {
@@ -186,28 +166,34 @@ primus.on('connection', function (socket) {
     socket.join('activegames', function () {
         socket.write(JSON.stringify(manualgamelist));
     });
-    
+
     socket.on('data', function (data) {
         data = data || {};
         var action = data.command;
+        //startLen = manualgamelist.length;
         switch (action) {
         case ('join'):
             if (!manualgamelist[data.room]) {
                 manualgamelist[data.room] = new Room(data.room);
+                manualgamelistSecret[data.room] = new RoomSecret(data.room);
+                primus.room('activegames').write(JSON.stringify(manualgamelist));
             }
+            manualgamelist[data.room].join(data.identity);
             socket.join(data.room, function () {
-                socket.write(JSON.stringify(manualgamelist[data.room]));
+                updateSubsGamelist(socket);
             });
             break;
 
         case ('leave'):
-            socket.leave('data.room');
+            socket.leave(data.room);
+            leave(data.room, socket);
             break;
 
         default:
             console.log(data);
 
         }
+
     });
 });
 primus.on('disconnection', function (socket) {
