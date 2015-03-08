6c73a776b91429969854cee3025f6c4458b52aef
Jamezs Gladney <jamezs@angelofcode.com>
2015-03-08 10:09:59 -0400

    duelrequest

--- server/libs/draftbot.js
+++ server/libs/draftbot.js
@@ -4,21 +4,53 @@ var bot,
     config = {
         channels: ["#server"],
         server: "ygopro.us",
-        botName: "Director"
+        botName: "DuelServ"
     };
+
+function randomString(len, charSet) {
+    'use strict';
+    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var randomPoz,
+        i = 0,
+        randomstring = '';
+    for (i; i < len; i++) {
+        randomPoz = Math.floor(Math.random() * charSet.length);
+        randomstring += charSet.substring(randomPoz, randomPoz + 1);
+    }
+    return randomstring;
+}
 
 bot = new irc.Client(config.server, config.botName, {
     channels: config.channels
 });
 
-function communicateToPrimus(message) {
+function communicateToPrimus(message, action) {
     'use strict';
     var output = {
         messagetype: 'draft',
-        message: message
+        message : message
     };
     process.send(output);
 }
+
+function duelrequest(challenger, challengedParty, roompass) {
+    'use strict';
+    communicateToPrimus({
+        clientEvent : 'duelrequest',
+        target : challengedParty,
+        from : challenger,
+        roompass : roompass
+    }, 'duelrequest');
+
+}
+bot.addListener("message", function (from, to, message) {
+    'use strict';
+    var command = message.split(' ');
+    if (command[0] !== '!duel' && command.length !== 2){
+        return;
+    }
+    duelrequest(from, )
+});
 
 module.exports = function incomingMsg(message) {
     'use strict';
