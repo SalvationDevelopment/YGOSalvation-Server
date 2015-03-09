848dbc2b5b47baa281456558c2314ca3fdc5bc5a
Jamezs Gladney <jamezs@angelofcode.com>
2015-03-08 21:12:49 -0400

    merge

--- server/libs/draftbot.js
+++ server/libs/draftbot.js
@@ -40,8 +40,15 @@ function duelrequest(challenger, challengedParty, roompass) {
 bot.addListener("message", function (from, to, message) {
     'use strict';
     var command = message.split(' '),
-        pass = randomString(5);
-    if (command[0] !== '!duel' || command.length !== 2) {
+        pass = randomString(5),
+        types = ['!tcg', '!ocg', '!duel', '!tag'],
+        codes = {
+            '!tcg' : '201OOO8000,0,5,1,U,',
+            '!ocg' : '201OOO8000,0,5,1,U,',
+            '!duel' : '201OOO8000,0,5,1,U,',
+            '!tag' : '201OOO8000,0,5,1,U,'
+        };
+    if (((types.indexOf(command[0]) !== -1) && command.length !== 2) || (command !== '!tag' && command.length !== 4)) {
         return;
     }
     if (command === '!tag' && command.length === 5) {
