36999ff3852d1504f522213f5bc6ed6a2417cab1
Jamezs Gladney <jamezs@angelofcode.com>
2015-03-11 22:25:11 -0400

    expansion

--- server/libs/recieveSTOC.js
+++ server/libs/recieveSTOC.js
@@ -1,9 +1,91 @@
-/* jshint node: true */
+/*jslint node: true*/
 var enums = require('./enums.js');
 
 module.exports = function recieveSTOC(packet) {
-    var task = Object.create(enums.STOCCheck);
+    'use strict';
+    var task = Object.create(enums.STOCCheck),
+        player,
+        clocation,
+        index,
+        data,
+        code,
+        pc,
+        pl,
+        ps,
+        pp,
+        cc,
+        cl,
+        cs,
+        cp,
+        reason,
+        ct;
+
     task[packet.STOC] = packet;
+    switch (packet.CTOS) {
+    case ("STOC_UNKNOWN"):
+        break;
+
+    case ("STOC_GAME_MSG"):
+        break;
+
+    case ("STOC_ERROR_MSG"):
+        break;
+
+    case ("STOC_SELECT_HAND"):
+        break;
+
+    case ("STOC_SELECT_TP"):
+        break;
+
+    case ("STOC_HAND_RESULT"):
+        break;
+
+    case ("STOC_TP_RESULT"):
+        break;
+
+    case ("STOC_CHANGE_SIDE"):
+        break;
+
+    case ("STOC_WAITING_SIDE"):
+        break;
+
+    case ("STOC_CREATE_GAME"):
+        break;
+
+    case ("STOC_JOIN_GAME"):
+        break;
+
+    case ("STOC_TYPE_CHANGE"):
+        break;
+
+    case ("STOC_LEAVE_GAME"):
+        break;
+
+    case ("STOC_DUEL_START"):
+        break;
+
+    case ("STOC_DUEL_END"):
+        break;
+
+    case ("STOC_REPLAY"):
+        break;
+
+    case ("STOC_TIME_LIMIT"):
+        break;
+
+    case ("STOC_CHAT"):
+        break;
+
+    case ("STOC_HS_PLAYER_ENTER"):
+        break;
+
+    case ("STOC_HS_PLAYER_CHANGE"):
+        break;
+
+    case ("STOC_HS_WATCH_CHANGE"):
+        break;
+
+    }
     task.reference = packet.message;
     return task;
 };
