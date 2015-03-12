62561c372f9b6876006347c06d19879793786ca3
Jamezs Gladney <panthrowzay@gmail.com>
2015-03-12 14:09:45 -0400

    Create card.js

---
+++ server/http/js/card.js
@@ -0,0 +1,148 @@
+function makeCard(buffer, start, controller) {
+    if (buffer.length < 4) {
+        return {
+            card: {
+                Code: 'nocard'
+            },
+            readposition: start
+        };
+    }
+    var flag = buffer.readUInt32LE(start);
+
+    if (!flag) {
+        return {
+            card: {
+                Code: 'cover',
+                Position: 'FaceDownAttack',
+            },
+            readposition: start + 9
+        };
+    }
+    var card = {
+        Code: 'cover',
+        Position: 'FaceDownAttack',
+    };
+
+    //console.log('flag:', flag);
+    var readposition = start + 4;
+
+    if (flag & enums.query.Code) {
+        card.Code = buffer.readUInt32LE(readposition);
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.Position) {
+        card.Controller = buffer[readposition + 0];
+        card.Position = enums.Positions[buffer[readposition + 3]];
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.Alias) {
+        card.Alias = buffer.readUInt32LE(readposition);
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.Type) {
+        card.Type = enums.cardTypes[buffer.readUInt32LE(readposition)];
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.Level) {
+        card.Level = buffer.readUInt32LE(readposition);
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.Rank) {
+        card.Rank = buffer.readUInt32LE(readposition);
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.Attribute) {
+        card.Attribute = enums.cardAttributes[buffer.readUInt32LE(readposition)];
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.Race) {
+        card.Race = enums.race[buffer.readUInt32LE(readposition)];
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.Attack) {
+        card.Attack = buffer.readUInt32LE(readposition);
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.Defense) {
+        card.Defense = buffer.readUInt32LE(readposition);
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.BaseAttack) {
+        card.Attribute = buffer.readUInt32LE(readposition);
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.BaseDefense) {
+        card.Attribute = buffer.readUInt32LE(readposition);
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.Reason) {
+        card.Attribute = buffer.readUInt16LE(readposition);
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.ReasonCard) {
+        card.Attribute = buffer.readUInt32LE(readposition);
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.EquipCard) {
+        card.EquipCard = {
+            c: buffer[readposition + 0],
+            l: buffer[readposition + 1],
+            s: buffer[readposition + 2]
+        };
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.TargetCard) {
+        card.TargetCard = [];
+        for (var i = 0; i < buffer.readUInt32LE(readposition); ++i) {
+            card.TargetCard.push({
+                c: buffer[readposition + 0],
+                l: buffer[readposition + 1],
+                s: buffer[readposition + 2]
+            });
+            readposition = readposition + 4;
+        }
+    }
+    if (flag & enums.query.OverlayCard) {
+        card.OverlayCard = [];
+        for (var ii = 0; ii < buffer.readUInt32LE(readposition); ++ii) {
+            card.OverlayCard.push(buffer.readUInt32LE(readposition));
+            readposition = readposition + 4;
+        }
+    }
+    if (flag & enums.query.Counters) {
+        card.Counters = [];
+        for (var iii = 0; iii < buffer.readUInt32LE(readposition); ++iii) {
+            card.Counters.push({
+                counterType: buffer.readUInt16LE(readposition),
+                amount: buffer.readUInt16LE(readposition + 2)
+            });
+            readposition = readposition + 4;
+        }
+    }
+    if (flag & enums.query.Owner) {
+        card.EquipCard = buffer.readUInt32LE(readposition);
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.IsDisabled) {
+        card.EquipCard = buffer.readUInt32LE(readposition);
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.IsPublic) {
+        card.EquipCard = buffer.readUInt32LE(readposition);
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.LScale) {
+        card.lscale = buffer.readUInt32LE(readposition);
+        readposition = readposition + 4;
+    }
+    if (flag & enums.query.RScale) {
+        card.rscale = buffer.readUInt32LE(readposition);
+        readposition = readposition + 4;
+    }
+    return {
+        card: card,
+        readposition: readposition
+    };
+
+
+}
