e64697c8b966c1cdd9a8ef3308ff0daf94081329
Jamezs Gladney <jamezs@angelofcode.com>
2015-03-12 20:44:35 -0400

    change to case statements

--- server/libs/recieveSTOC.js
+++ server/libs/recieveSTOC.js
@@ -18,11 +18,8 @@ module.exports = function recieveSTOC(packet) {
         command = enums.STOC.STOC_GAME_MSG[task.STOC_GAME_MSG.message[0]];
         task.STOC_GAME_MSG.command = command;
         game_message = task.STOC_GAME_MSG.message;
-        if (command === undefined) {
-            console.log('figure out STOC', task.STOC_GAME_MSG);
-        }
-        console.log(command);
-        if (command === 'MSG_START') {
+        switch (command) {
+        case ('MSG_START'):
             task.STOC_GAME_MSG.type = game_message[1];
             task.STOC_GAME_MSG.lifepoints1 = game_message.readUInt16LE(2);
             task.STOC_GAME_MSG.lifepoints2 = game_message.readUInt16LE(6);
@@ -30,64 +27,87 @@ module.exports = function recieveSTOC(packet) {
             task.STOC_GAME_MSG.player1extrasize = game_message.readUInt8(12);
             task.STOC_GAME_MSG.player2decksize = game_message.readUInt8(14);
             task.STOC_GAME_MSG.player2extrasize = game_message.readUInt8(16);
+            break;
 
-        } else if (command === 'MSG_HINT') {
+        case ('MSG_HINT'):
             console.log('MSG_HINT', game_message);
             task.STOC_GAME_MSG.hintplayer = game_message[1];
             task.STOC_GAME_MSG.hintcont = game_message[2];
             task.STOC_GAME_MSG.hintspeccount = game_message[3];
             task.STOC_GAME_MSG.hintforce = game_message[4];
-
-        } else if (command === 'MSG_NEW_TURN') {
             break;
-        } else if (command === 'MSG_WIN') {
-            break; //console.log('Win', game_message[1]);
-        } else if (command === 'MSG_NEW_PHASE') {
-            break; //
-        } else if (command === 'MSG_DRAW') {
+
+        case ('MSG_NEW_TURN'):
+            break;
+
+        case ('MSG_WIN'):
+            task.STOC_GAME_MSG.win = game_message[1];
+            break;
+
+        case ('MSG_NEW_PHASE'):
+            break;
+
+        case ('MSG_DRAW'):
             task.STOC_GAME_MSG.drawplayer = game_message[1];
             task.STOC_GAME_MSG.draw = game_message[2];
             task.STOC_GAME_MSG.cardslist = [];
             task.STOC_GAME_MSG.drawReadposition = 3;
-        } else if (command === 'MSG_SHUFFLE_DECK') {
-            task.STOC_GAME_MSG.shuffle = game_message[1];
-
-        } else if (command === 'MSG_SHUFFLE_HAND') {
-            task.STOC_GAME_MSG.layout = game_message[1];
-        } else if (command === 'MSG_CHAINING') {
-            break; //
-        } else if (command === 'MSG_CHAINED') {
-            task.STOC_GAME_MSG.ct = game_message[1];
-
-        } else if (command === 'MSG_CHAIN_SOLVING') {
-            task.STOC_GAME_MSG.ct = game_message[1];
-
-        } else if (command === 'MSG_CHAIN_SOLVED') {
             break;
-        } else if (command === 'MSG_CHAIN_END') {
+                
+        case ('MSG_SHUFFLE_DECK'):
+            task.STOC_GAME_MSG.shuffle = game_message[1];
+            break;
+
+        case ('MSG_SHUFFLE_HAND'):
+            task.STOC_GAME_MSG.layout = game_message[1];
+            break;
+                
+        case ('MSG_CHAINING'):
+            break; //
+        case ('MSG_CHAINED'):
+            task.STOC_GAME_MSG.ct = game_message[1];
+            break;
+                
+        case ('MSG_CHAIN_SOLVING'):
+            task.STOC_GAME_MSG.ct = game_message[1];
+            break;
+
+        case ('MSG_CHAIN_SOLVED'):
+            break;
+                
+        case ('MSG_CHAIN_END'):
             break; // remove any liggering chain parts
-        } else if (command === 'MSG_CHAIN_NEGATED' || command === 'MSG_CHAIN_DISABLED') {
+                
+        case ('MSG_CHAIN_NEGATED'):
             break; //graphical and trigger only for replay
-        } else if (command === 'MSG_CARD_SELECTED') {
-            break; // trigger
-        } else if (command === 'MSG_CARD_SELECTED') {
+                
+        case ('MSG_CHAIN_DISABLED'):
+            break; //graphical and trigger only for replay
+
+        case ('MSG_CARD_SELECTED'):
             /*  player = game_message[1];*/
             task.STOC_GAME_MSG.count = game_message[2];
+            break;
 
-        } else if (command === 'MSG_PAY_LPCOST') {
+        case ('MSG_PAY_LPCOST'):
             task.STOC_GAME_MSG.player = game_message[1];
             task.STOC_GAME_MSG.lpcost = game_message.readUInt16LE(2);
+            break;
 
-        } else if (command === 'MSG_DAMAGE') {
+        case ('MSG_DAMAGE'):
             task.STOC_GAME_MSG.player = game_message[1];
             task.STOC_GAME_MSG.damage = game_message.readUInt16LE(2);
-        } else if (command === 'MSG_SUMMONING ') {
+            break;
+                
+        case ('MSG_SUMMONING '):
             //ignoring
             break;
-        } else if (command === 'MSG_SELECT_IDLECMD') {
+                
+        case ('MSG_SELECT_IDLECMD'):
             task.STOC_GAME_MSG.idleplayer = game_message[1];
+            break;
 
-        } else if (command === 'MSG_MOVE') {
+        case ('MSG_MOVE'):
             task.STOC_GAME_MSG.code = game_message.readUInt16LE(1);
             task.STOC_GAME_MSG.pc = game_message[5]; // original controller
             task.STOC_GAME_MSG.pl = game_message[6]; // original cLocation
@@ -98,49 +118,70 @@ module.exports = function recieveSTOC(packet) {
             task.STOC_GAME_MSG.cs = game_message[11]; // current sequence (index)
             task.STOC_GAME_MSG.cp = game_message[12]; // current position
             task.STOC_GAME_MSG.reason = game_message.readUInt16LE[12]; //debug data??
+            break;
 
-        } else if (command === 'MSG_POS_CHANGE') {
+        case ('MSG_POS_CHANGE'):
             task.STOC_GAME_MSG.code = game_message.readUInt16LE(1);
             task.STOC_GAME_MSG.cc = game_message[5]; // current controller
             task.STOC_GAME_MSG.cl = game_message[6]; // current cLocation
             task.STOC_GAME_MSG.cs = game_message[7]; // current sequence (index)
             task.STOC_GAME_MSG.pp = game_message[8]; // padding??
             task.STOC_GAME_MSG.cp = game_message[9]; // current position
+            break;
 
-        } else if (command === 'MSG_SET') {
+        case ('MSG_SET'):
             // All the vars are commented out in the source.
             break;
-        } else if (command === 'MSG_SWAP') {
+                
+        case ('MSG_SWAP'):
             break;
-            // code vars are commented out in the source, assuming graphical only.
-        } else if (command === 'MSG_SUMMONING' || command === 'MSG_SPSUMMONING') {
+                
+            
+        case ('MSG_SUMMONING'):
             task.STOC_GAME_MSG.code = game_message.readUInt16LE(1);
-        } else if (command === 'MSG_SUMMONED' || command === 'MSG_SPSUMMONED' || command === 'MSG_FLIPSUMMONED') {
+            break;
+                
+        case ('MSG_SPSUMMONING'):
+            task.STOC_GAME_MSG.code = game_message.readUInt16LE(1);
+            break;
+                
+        case ('MSG_SUMMONED'):
             break; //graphical only
-        } else if (command === 'MSG_FLIPSUMMONING') {
+                
+        case ('MSG_SPSUMMONED'):
+            break; //graphical only
+                
+        case ('MSG_FLIPSUMMONED'):
+            break; //graphical only
+                
+        case ('MSG_FLIPSUMMONING'):
             // notice pp is missing, and everything is upshifted; not repeating code.
             task.STOC_GAME_MSG.code = game_message.readUInt16LE(1);
             task.STOC_GAME_MSG.cc = game_message[5]; // current controller
             task.STOC_GAME_MSG.cl = game_message[6]; // current cLocation
             task.STOC_GAME_MSG.cs = game_message[7]; // current sequence (index)
             task.STOC_GAME_MSG.cp = game_message[8]; // current position
-            
-        } else if (command === 'MSG_UPDATE_DATA') {
+            break;
+
+        case ('MSG_UPDATE_DATA'):
             task.STOC_GAME_MSG.player = game_message[1];
             task.STOC_GAME_MSG.fieldlocation = game_message[2];
             task.STOC_GAME_MSG.fieldmodel = enums.locations[task.STOC_GAME_MSG.fieldlocation];
+            break;
 
-        } else if (command === 'MSG_UPDATE_CARD') {
+        case ('MSG_UPDATE_CARD'):
             task.STOC_GAME_MSG.udplayer = game_message[1];
             task.STOC_GAME_MSG.udfieldlocation = game_message[2];
             task.STOC_GAME_MSG.udindex = game_message[3];
-
             task.STOC_GAME_MSG.udcard = makeCard(game_message, 8, task.STOC_GAME_MSG.udplayer).card;
-        } else {
+            break;
+        
+        default:
             console.log(command, game_message);
+            break;
         }
         break;
-
+            
     case ("STOC_ERROR_MSG"):
         break;
 
