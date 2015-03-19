98cad835592ca080778be34468fc3a1e26f97c94
Jamezs Gladney <panthrowzay@gmail.com>
2015-03-19 10:44:03 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,19 +1,12 @@
 /*jslint node: true, plusplus: true, unparam: false, nomen: true*/
 var assert = require("assert");
-var phantom = require('phantom');
 
-phantom.create(
-    function (ph) {
-        'use strict';
-        ph.createPage(function (page) {
-            page.open("../client/interface/index.html", function (status) {
-                //      console.log("opened google? ", status);
-                //      page.evaluate(function () { return document.title; }, function (result) {
-                //        console.log('Page title is ' + result);
-                ph.exit();
-            });
-        });
-    }
-);
+global.__base = __dirname + '/';
 
-require('../server/server.js');
+describe('Array', function(){
+  
+    it('Should start the server', function(){
+      require('./server.js')
+    })
+ 
+});
