0dbb3ea5d64ce51039889970b0b4bf662455c33a
Jamezs Gladney <panthrowzay@gmail.com>
2015-03-20 09:57:27 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 /*jslint node: true, plusplus: true, unparam: false, nomen: true*/
 var assert = require("assert");
 
-global.__base = __dirname + '/server/';
+global.__base = __dirname + '/../server/';
 
 describe('Array', function(){
   
