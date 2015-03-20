b23457d0def1d1c59565f1a2c13e1b17edf3b443
Jamezs Gladney <panthrowzay@gmail.com>
2015-03-20 10:18:30 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
 /*jslint node: true, plusplus: true, unparam: false, nomen: true*/
 var assert = require("assert");
 
-global.__base = __dirname + '/server/';
+//global.__base = __dirname + '/server/';
 
 describe('Array', function(){
   
     it('Should start the server', function(){
-      require('./server.js')
+      require('../server/server.js')
     })
  
 });
