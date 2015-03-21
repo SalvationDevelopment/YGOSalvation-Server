db0aa7959bf7e57ca2b0fd0b5987ff7673ebb8ce
Jamezs Gladney <jamezs@angelofcode.com>
2015-03-21 12:34:30 -0400

    testing changes

--- test/test.js
+++ test/test.js
@@ -1,12 +1,21 @@
 /*jslint node: true, plusplus: true, unparam: false, nomen: true*/
+/*mocha globals*/
+/*global describe, it*/
 var assert = require("assert");
 
 //global.__base = __dirname + '/server/';
 
-describe('Array', function(){
-  
-    it('Should start the server', function(){
-      require('../server/server.js')
-    })
- 
+describe('Array', function () {
+    'use strict';
+    it('Should start the server', function () {
+        require('../server/server.js');
+    });
+    it('Should start the AI', function () {
+        require('../server/server.js');
+        require('../server/ai.js');
+
+    });
 });
+
+//check all JSON files are JSON
+//do JSLint on everything
