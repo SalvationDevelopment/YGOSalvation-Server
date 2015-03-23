dfa9a35f9909d53b3c158bf1454e5582ac30b34c
Jamezs Gladney <panthrowzay@gmail.com>
2015-03-23 10:10:29 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -15,7 +15,72 @@ describe('System', function () {
         require('../server/ai.js');
 
     });
+    
 });
-
+describe('Boot Test', function () {
+    it('Should test  CDBUpdate', function () {
+        require('../server/libs/carddb-update.js');
+    });
+    it('Should test  custom_error.js', function () {
+        require('../server/libs/custom_error.js');
+    });
+     it('Should test  datetimestamp.js', function () {
+        require('../server/libs/datetimestamp.js');
+    });
+     it('Should test  draft.jsjs', function () {
+        require('../server/libs/draft.js');
+    });
+     it('Should test draftbot.js', function () {
+        require('../server/libs/draftbot.js');
+    });
+     it('Should test emergency.js', function () {
+        require('../server/libs/emergency.js');
+    });
+     it('Should test enums.js', function () {
+        require('../server/libs/enums.js');
+    });
+     it('Should test gamelist.js', function () {
+        require('../server/libs/gamelist.js');
+    });
+     it('Should test ircbot.js', function () {
+        require('../server/libs/ircbot.js');
+    });
+     it('Should test killcore.js', function () {
+        require('../server/libs/killcore.js');
+    });
+     it('Should test ldapclient.js', function () {
+        require('../server/libs/ldapclient.js');
+    });
+     it('Should test manualmode.js', function () {
+        require('../server/libs/manualmode.js');
+    });
+     it('Should test parseframes.js', function () {
+        require('../server/libs/parseframes.js');
+    });
+     it('Should test parsepackets.js', function () {
+        require('../server/libs/parsepackets.js');
+    });
+     it('Should test policyserver.js, function () {
+        require('../server/libs/policyserver.js');
+    });
+     it('Should test processIncomingTrasmission.js', function () {
+        require('../server/libs/processIncomingTrasmission.js');
+    });
+     it('Should test recieveCTOS.js', function () {
+        require('../server/libs/recieveCTOS.js');
+    });
+     it('Should test recieveSTOC.js', function () {
+        require('../server/libs/recieveSTOC.js');
+    });
+     it('Should test servercontrol.json', function () {
+        require('../server/libs/servercontrol.json');
+    });
+     it('Should test ldapserver.js', function () {
+        require('../server/libs/ldapserver.js');
+    });
+     it('Should test ygocore-network-interface.js', function () {
+        require('../server/libs/ygocore-network-interface.js');
+    });
+});
 //check all JSON files are JSON
 //do JSLint on everything
