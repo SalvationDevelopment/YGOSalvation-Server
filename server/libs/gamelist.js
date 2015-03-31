ddfc91516fa5d87f9c447cde241c8179103cde3e
Jamezs Gladney <jamezs@angelofcode.com>
2015-03-30 21:37:27 -0400

    AI Enhancement, error removal

--- server/libs/gamelist.js
+++ server/libs/gamelist.js
@@ -89,7 +89,7 @@ function handleCoreMessage(core_message_raw, port, pid) {
 
         case ('::::chat'):
             chat = core_message[1] + '|' + core_message[2];
-            message_irc(chat);
+            //message_irc(chat);
             break;
 
         }
