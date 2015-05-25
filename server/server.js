b2dc70acd633af247b81575e4b973d5af5daa1c1
Jamezs Gladney <jamezs@angelofcode.com>
2015-05-25 00:45:56 -0400

    Split slave out

--- server/server.js
+++ server/server.js
@@ -15,21 +15,13 @@ Run `npm install` in the directory above.
 
 /*jslint  node: true, plusplus: true*/
 
-var ygoserver, //port 8911 ygopro Server
-    ircManager, // magimagigal
-    numCPUs = 1, // atleast 1 slave and 1 master.
-    notification = '', // its a string, make memory.
+var notification = '', // its a string, make memory.
     gamelistManager, // primus and gamelist
     clusterIterator = 0, // its a number make memory,
     activegames = 0,
-    net = require('net'), //tcp connections
     cluster = require('cluster'), // multithreading
     colors = require('colors'), // oo pretty colors!
-    domain = require('domain'), // yay error handling
-
-    WebSocket = require('ws').Server,
-    Framemaker = require('./libs/parseframes.js'), //understand YGOPro API.
-    processIncomingTrasmission = require('./libs/processIncomingTrasmission.js'); // gamelist and start games
+    domain = require('domain'); // yay error handling
 
 
 function gamelistMessage(message) {
@@ -51,24 +43,23 @@ function gamelistMessage(message) {
     process.title = 'YGOPro Salvation Server [' + activegames + ']';
 }
 
-function initiateMaster() {
+function initiateMaster(numCPUs) {
     'use strict';
-
-
     console.log('YGOPro Salvation Server - Saving Yu-Gi-Oh!'.bold.yellow);
     console.log('    Starting Master');
     process.title = 'YGOPro Salvation Server [' + activegames + '] ' + new Date();
-    ircManager = require('./libs/ircbot.js');
     gamelistManager = require('./libs/gamelist.js');
+    //require('./libs/policyserver.js');
+    //require('./libs/ldapserver.js'); //LDAP endpoint; //Flash policy server for LightIRC;
 
     function setupWorker(x) {
         //'use strict';
         console.log(('        Starting Slave ' + x).grey);
         var worker = cluster.fork({
-            PORTRANGE: x
+            PORTRANGE: x,
+            SLAVE: true
         });
-        require('./libs/policyserver.js');
-        require('./libs/ldapserver.js'); //LDAP endpoint; //Flash policy server for LightIRC;
+
         worker.on('message', gamelistMessage);
     }
     for (clusterIterator; clusterIterator < numCPUs; clusterIterator++) {
@@ -76,8 +67,6 @@ function initiateMaster() {
     }
     cluster.on('exit', function (worker, code, signal) {
         notification = 'worker ' + clusterIterator + ' died ' + code + ' ' + signal;
-        ircManager.notify(notification);
-        console.log(notification);
         setupWorker(clusterIterator++);
         if (clusterIterator > 20) {
             clusterIterator = 0;
@@ -85,113 +74,25 @@ function initiateMaster() {
     });
 }
 
-function initiateSlave() {
-    'use strict';
-    // When a user connects, create an instance and allow the to duel, clean up after.
-    var parsePackets = require('./libs/parsepackets.js'),
-        ws;
-    function handleTCP(socket) {
-        var framer = new Framemaker();
-        socket.setNoDelay(true);
-        socket.active_ygocore = false;
-        socket.active = false;
-        socket.on('data', function listener(data) {
-            var frame,
-                task,
-                newframes = 0;
-            if (socket.active_ygocore) {
-                socket.active_ygocore.write(data);
-            }
-            frame = framer.input(data);
-            for (newframes; frame.length > newframes; newframes++) {
-                task = parsePackets('CTOS', new Buffer(frame[newframes]));
-                processIncomingTrasmission(data, socket, task);
-            }
-            frame = [];
 
-        });
-        socket.setTimeout(300000, function () {
-            socket.end(); //Security precaution
-        });
-        socket.on('error', function (error) {
-            console.log('::CLIENT', error);
-            if (socket.active_ygocore) {
-                try {
-                    socket.active_ygocore.end();
-                } catch (e) {
-                    console.log('::CLIENT ERROR Before connect', e);
-                }
-            }
-        });
-    }
-    ygoserver = net.createServer(handleTCP);
-    ygoserver.listen(8911);
-    
-    ws = new WebSocket({ port: 8080 });
-    ws.on('connection', function connection(socket) {
-        var framer = new Framemaker();
-        socket.active_ygocore = false;
-        socket.active = false;
-        socket.write = function (message) {
-            socket.send(message, {binary: true, mask: false});
-        };
-        socket.end = function () {
-        
-        };
-        socket.on('message', function incoming(data) {
-            var frame,
-                task,
-                newframes = 0;
-            console.log(data);
-            if (socket.active_ygocore) {
-                socket.active_ygocore.write(data);
-            }
-            frame = framer.input(data);
-            for (newframes; frame.length > newframes; newframes++) {
-                task = parsePackets('CTOS', new Buffer(frame[newframes]));
-                processIncomingTrasmission(data, socket, task);
-            }
-            frame = [];
-
-        });
-        socket.on('close', function close() {
-            //console.log('disconnected');
-        });
-    });
-    
-    return ygoserver;
-}
 
 (function main() {
     'use strict';
-    if (require('os').cpus().length > 1) {
-        numCPUs = require('os').cpus().length;
-        numCPUs = (numCPUs > 8) ? 8 : numCPUs;
-    }
-
-    if (cluster.isMaster) {
-        // if doing a build test on Travis running with istanbul coverage
-        if (process.env.running_under_istanbul) {
-            // use coverage for forked process
-            // disabled reporting and output for child process
-            // enable pid in child process coverage filename
-            cluster.setupMaster({
-                exec: './node_modules/.bin/istanbul',
-                args: [
-                    'cover', '--report', 'none', '--print', 'none', '--include-pid',
-                    process.argv[1], '--'].concat(process.argv.slice(2))
-            });
-        }
-        initiateMaster();
+    console.log(process.env.SLAVE);
+    if (process.env.SLAVE) {
+        require('./libs/slave.js');
     } else {
-        initiateSlave();
+        var numCPUs = 1; // atleast 1 slave and 1 master.
+        if (require('os').cpus().length > 1) {
+            numCPUs = require('os').cpus().length;
+            numCPUs = (numCPUs > 8) ? 8 : numCPUs;
+        }
+        initiateMaster(numCPUs);
     }
-
 }()); // end main
 
 module.exports = {
     initiateMaster: initiateMaster,
-    initiateSlave: initiateSlave,
     gamelistMessage: gamelistMessage
 };
 
@@ -199,6 +100,7 @@ module.exports = {
 /* This is bad code */
 process.on('uncaughtException', function (err) {
     'use strict';
-    console.log(err);
+    console.error((new Date()).toUTCString() + ' uncaughtException:', err.message);
+    console.error(err.stack);
+    process.exit(1);
 });
-
