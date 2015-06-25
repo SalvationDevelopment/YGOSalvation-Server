0b86b9105815adc9f977f8db4c567d5aa551abd9
Jamezs Gladney <jamezs@angelofcode.com>
2015-06-25 17:45:54 -0400

    translation enhancements, deck edit cleanup

--- server/http/js/Deckfilters.js
+++ server/http/js/Deckfilters.js
@@ -1,128 +1,136 @@
-
-function filterAttrType(obj, num, at){
-  val = (at==1) ? obj['Attribute'] : obj['Type'];
-  if (val === num){
-  	    return true;
-    } else {
-  		return false;
-	   }
-}
+/*jslint bitwise : true*/
+var cards = [];
+$.getJSON('http://ygopro.us/manifest/database.json', function (data) {
+    cards = data;
+});
 
 
-function filterSetcode(obj, sc){
-  val=obj['setcode'];
-  hexA= val.toString(16);
-  hexB= sc.toString(16);
-  if (val===sc  || hexA.substr(hexA.length-4) === hexB ||  hexA.substr(hexA.length-2) === hexB || (val>>8).toString(16) === hexB){
-	  return true;
-  } else {
-		return false;
-	}
- }
- 
- function filterLevel(obj, lv, op){
-   val=obj['level'].toString(16);
-   lv=parseInt(lv.toString(16));
-   if (op==0){
-	if (parseInt(val.substr(val.length-2)) <= lv){
-      return true;
-    } else {
-		return false;
-	  }
-	}
-	else if (op==1){
-	if (parseInt(val.substr(val.length-2)) === lv){
-      return true;
-    } else {
-		return false;
-	  }
-	}
-	else{
-	if (parseInt(val.substr(val.length-2)) >= lv){
-      return true;
-    } else {
-		return false;
-	  }
-	}
-}
-
-
- function filterScale(obj, sc, op){
-   val=obj['level'];
-   sc=parseInt(sc.toString(16));
-   if(op==0){
-     if (parseInt((val>>24).toString(16)) <= sc){
+function filterAttrType(obj, num, at) {
+    'use strict';
+    var val = (at === 1) ? obj.Attribute : obj.Type;
+    if (val === num) {
         return true;
-      } else {
-	   	  return false;
-	    }
-	}
-	else if(op==1){
-     if (parseInt((val>>24).toString(16)) === sc){
+    } else {
+        return false;
+    }
+}
+
+
+function filterSetcode(obj, sc) {
+    'use strict';
+    var val = obj.setcode,
+        hexA = val.toString(16),
+        hexB = sc.toString(16);
+    if (val === sc || hexA.substr(hexA.length - 4) === hexB || hexA.substr(hexA.length - 2) === hexB || (val >> 8).toString(16) === hexB) {
         return true;
-      } else {
-	   	  return false;
-	    }
-	}
-	else {
-     if (parseInt((val>>24).toString(16)) >= sc){
+    } else {
+        return false;
+    }
+}
+
+function filterLevel(obj, lv, op) {
+    'use strict';
+    var val = obj.level.toString(16);
+    lv = parseInt(lv.toString(16), 10);
+    if (op === 0) {
+        if (parseInt(val.substr(val.length - 2), 10) <= lv) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (op === 1) {
+        if (parseInt(val.substr(val.length - 2), 10) === lv) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        if (parseInt(val.substr(val.length - 2), 10) >= lv) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+function filterScale(obj, sc, op) {
+    'use strict';
+    var val = obj.level;
+    sc = parseInt(sc.toString(16));
+    if (op === 0) {
+        if (parseInt((val >> 24).toString(16)) <= sc) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (op === 1) {
+        if (parseInt((val >> 24).toString(16)) === sc) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        if (parseInt((val >> 24).toString(16)) >= sc) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+
+
+function filterType(obj, ty) {
+    'use strict';
+    var val = obj['type'];
+    if (val & ty > 0) {
         return true;
-      } else {
-	   	  return false;
-	    }
-	}
+    } else {
+        return false;
+    }
 }
 
-
-
-
- function filterType(obj, ty){
-   val=obj['type'];
-   if (val&ty >0){
-      return true;
+function filterAtkDef(obj, num, ad, op) {
+    'use strict';
+    var val = (ad === 1) ? obj.atk : obj.def;
+    if (op === 0) {
+        if (val >= num) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (op === 0) {
+        if (val === num) {
+            return true;
+        } else {
+            return false;
+        }
     } else {
-  		return false;
-	  }
+        if (val >= num) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
-function filterAtkDef(obj, num, ad, op){
-  val = (ad==1) ? obj['atk'] : obj['def'];
-  if(op==0){
-    if(val>=num){
-  	    return true;
+function filterNameDesc(obj, txt, nd) {
+    'use strict';
+    var val = (nd === 1) ? obj.name : obj['desc'];
+    if (val.includes(txt)) {
+        return true;
     } else {
-  		return false;
-	   }
-   }
-   else if(op==0){
-    if(val===num){
-  	    return true;
-    } else {
-  		return false;
-	   }
-   }
-   else{
-    if(val>=num){
-  	    return true;
-    } else {
-  		return false;
-	   }
-   }
-}
-
-function filterNameDesc(obj, txt, nd){
-  val = (nd==1) ? obj['name'] : obj['desc'];
-  if (val.includes(txt)){
-  	    return true;
-    } else {
-  		return false;
-	   }
+        return false;
+    }
 }
 
 // Attempt to use FilterNameDesc with filter
-function filterName (txt) {
-    output = cards.filter(function (item) {
-        filterNameDesc(item, txt, 1)
+function filterName(txt) {
+    'use strict';
+    var output = cards.filter(function (item) {
+        filterNameDesc(item, txt, 1);
     });
     return output;
 }
