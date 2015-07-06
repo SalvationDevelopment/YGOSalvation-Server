cb49aebd0218c9c129493d8b7b1a411653b7ce23
IrateGod <konamijudge@yahoo.com>
2015-07-06 22:41:53 +0200

    Fix to filter function

--- server/http/js/deck-edit.js
+++ server/http/js/deck-edit.js
@@ -185,7 +185,7 @@ function filterRace(result, race) {
 
 }
 
-function filterSetcode(setcode) {
+function filterSetcode(result, setcode) {
     'use strict';
     return result.filter(function(item) {
         return fSetCode(item, setcode);
