67f23cfc4620bf3a0c1e0f46374283e620b2a40e
Jamezs Gladney <panthrowzay@gmail.com>
2015-07-24 11:14:42 -0400

    Removing websocket for stability concerns.

--- server/libs/slave.js
+++ server/libs/slave.js
@@ -67,48 +67,6 @@ function initiateSlave() {
     }
     ygoserver = net.createServer(handleTCP);
     ygoserver.listen(8911);
-
-    ws = new WebSocket({
-        server: httpsServer
-    });
-    ws.on('connection', function connection(socket) {
-        console.log('!!! --- connection to Websocket')
-        var framer = new Framemaker();
-        socket.active_ygocore = false;
-        socket.active = false;
-        socket.write = function (message) {
-            socket.send(message, {
-                binary: true,
-                mask: false
-            });
-        };
-        socket.end = function () {
-
-        };
-        socket.on('message', function incoming(data) {
-            var frame,
-                task,
-                newframes = 0;
-
-            if (socket.active_ygocore) {
-                socket.active_ygocore.write(data);
-            }
-            frame = framer.input(data);
-            for (newframes; frame.length > newframes; newframes++) {
-                task = parsePackets('CTOS', new Buffer(frame[newframes]));
-                processIncomingTrasmission(data, socket, task);
-            }
-            frame = [];
-
-        });
-        socket.on('close', function close() {
-            console.log('WS, disconnected');
-        });
-        socket.on('error', function close(error) {
-            console.log(error);
-        });
-    });
-
     return ygoserver;
 }
 
