200dc7f703eb26dc809407e752082243c54ae3e0
IrateGod <konamijudge@yahoo.com>
2015-07-25 18:43:12 +0200

    Changes to ydkToList

--- server/http/js/parseYDK.js
+++ server/http/js/parseYDK.js
@@ -37,6 +37,7 @@ function ydkToList(deckObject, options) {
         formatString = options.formatString || "%ix",
         cards = options.cards,
         decks = ["Main", "Extra", "Side"],
+		cardFound = false,
         temp;
     decks.forEach(function(deck) {
         output += deck + " Deck (" + deckObject[deck.toLowerCase() + "Length"] + "):\r\n";
@@ -46,42 +47,28 @@ function ydkToList(deckObject, options) {
                 cards.forEach(function(card) {
                     if (parseInt(temp, 10) === card.id) {
                         output += card.name;
+						cardFound = true;
                     }
                 });
+				if (!cardFound) {
+					output += temp + " (card not yet registered in database)";
+				}
                 output += "\r\n";
             } else {
                 while (deckObject[deck.toLowerCase()][temp]--) {
                     cards.forEach(function(card) {
                         if (parseInt(temp, 10) === card.id) {
                             output += card.name + "\r\n";
+							cardFound = true;
                         }
                     });
+					if (!cardFound) {
+						output += temp + " (card not yet registered in database)\r\n";
+					}
                 }
             }
+			cardFound = false;
         }
     });
     return output;
-}
-
-function getDecklists(callback) {
-	try {
-		var fs = fs || require("fs");
-	} catch(e) {
-		throw e;
-	}
-	fs.readdir('./ygopro/deck', function(err, files) {
-		var ret = "";
-		files.forEach(function(file) {
-			if(!/\.ydk$/.test(file)) {
-				return;
-			}
-			file = file.replace(/\.ydk$/, '');
-			ret += '<option value="' + file + '">' + file + '</option>';
-		});
-		callback(ret);
-	});
-}
-
-function fillSelect(str) {
-	$('#ydkListSelect').html(str);
 }
