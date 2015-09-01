58f27e0aed01a76ba60cedabc81146f8e264c10c
IrateGod <konamijudge@yahoo.com>
2015-09-01 20:23:43 +0200

    Completely change manualMode.js

--- server/libs/manualMode.js
+++ server/libs/manualMode.js
@@ -1,178 +1,226 @@
 // manualMode.js
- 
- process.on('uncaughtException', function (error) {
-    console.log('Error not caught: ', error);
-});
- 
-var wss_config = {
-        host: 'ygopro.us',
-        port: 55542,
-        verifyClient: function (info) {
-            return (info.origin.indexOf(ACCEPT_ORIGIN) > -1 && info.req.headers.host.indexOf(ACCEPT_ORIGIN) > -1);
-        }
-    },
-    activeDuels = {},
-    registry = {},
-    ws = require("ws"),
-    WebSocketServer = new ws.Server(wss_config);
-    
-// Define Constants
 
-var ACCEPT_ORIGIN = "ygopro.us",
+var Primus = require('primus'),
+    Rooms = require('primus-rooms'),
+    server = require('http').createServer().listen(55542),
+    registry = {},
+    activeDuels = {},
     ROLE_SPECTATOR = 0,
     ROLE_HOST = 1,
     ROLE_PLAYER_TWO = 2,
     ROLE_PLAYER_THREE = 3,
     ROLE_PLAYER_FOUR = 4,
+    QUERY_DUEL_COMMAND = "duelCommand",
     QUERY_GET_OPTIONS = "getOptions",
     QUERY_GET_STATE = "getState",
-    QUERY_START_DUEL = "startDuel";
-    
-WebSocketServer.on('connection', function (WebSocket) {
-    WebSocket.on('message', function (message) {
-        console.log("Active duels: ", activeDuels);
-        console.log("Registry: ", registry);
-        WebSocket.send(generateServerResponse(message, WebSocket));
+    QUERY_START_DUEL = "startDuel",
+    primus = new Primus(server, {
+        transformer: 'websockets',
+        timeout: 60000,
+        cors: ["http://forum.ygopro.us", "http://ygopro.us"]
+    });
+
+primus.use('rooms', Rooms);
+
+primus.on('connection', function (spark) {
+    spark.on('data', function (data) {
+        handlePrimusEvent(data, spark);
+    });
+    spark.on('end', function () {
+        handleClientDisconnect(spark);
     });
 });
 
-WebSocketServer.on('error', function (error) {
-    console.log('Server encountered an error: ', error);
-});
-
-function generateServerResponse(message, WebSocket) {
-    /**
-     *
-     * Message Structure is as follows:
-     *
-     * uniqueID,messageType,duelID|userName[,duelOptions|query|command]
-     *
-     * Possible messageTypes:
-     *
-     * - registerUID
-     * - hostDuel
-     * - joinDuel
-     * - spectateDuel
-     * - duelQuery
-     * - duelCommand
-     *
-     **/
-    var splitMessage = message.split(','),
-        uniqueID = splitMessage[0],
-        messageType = splitMessage[1],
-        identifier = splitMessage[2],
-        params = splitMessage[3],
-        response = "serverResponse,";
-    if (!uniqueID || !messageType || !identifier) {
-        WebSocket.close();
+function handlePrimusEvent(data, spark) {
+    var data = data || {},
+        action = data.action,
+        uid = data.uid,
+        username = data.username,
+        duelOptions = data.duelOptions,
+        duelID = duelOptions.duelID,
+        duelQuery = duelOptions.duelQuery,
+        duelCommand = duelOptions.duelCommand,
+        id = spark.id;
+    if (!action || !uid) {
+        writeResponse(spark, [403, 'invalidRequest']);
+        return;
     }
-    switch (messageType) {
+    switch (action) {
         case "registerUID": {
-            if (registry.hasOwnProperty(uniqueID)) {
-                WebSocket.close();
-                break;
+            if (registry.hasOwnProperty(id)) {
+                writeResponse(spark, [403, 'invalidRequest']);
+                return;
             }
-            registry[uniqueID] = identifier;
-            console.log(response = response + 'registeredUID');
-            break;
+            registry[id] = {
+                uid: uid,
+                username: username
+            };
+            writeResponse(spark, [200, 'registeredUID']);
+            return;
         }
         case "hostDuel": {
-            activeDuels[identifier] = {
-                options: params
-            };
-            activeDuels[identifier][uniqueID] = {
-                ROLE: ROLE_HOST
-            };
-            console.log(response = response + ',hostedDuel,' + identifier);
-            break;
-        }
-        case "joinDuel": {
-            if (activeDuels.hasOwnProperty(identifier)) {
-                switch (Object.keys(activeDuels[identifier]).length) {
-                    case 2: {
-                        activeDuels[identifier][uniqueID] = {
-                            ROLE: ROLE_PLAYER_TWO
-                        };
-                        break;
-                    }
-                    case 3: {
-                        activeDuels[identifier][uniqueID] = {
-                            ROLE: ROLE_PLAYER_THREE
-                        };
-                        break;
-                    }
-                    case 4: {
-                        activeDuels[identifier][uniqueID] = {
-                            ROLE: ROLE_PLAYER_FOUR
-                        };
-                        break;
-                    }
-                    default: {
-                        break;
-                    }
-                }
-                console.log(response = response + "joinedDuel," + identifier);
-                break;
-            } else {
-                console.log(response = response + "invalidDuelID," + identifier);
-                break;
+            if (!duelID) {
+                writeResponse(spark, [403, 'invalidRequest']);
+                return;
             }
-        }
-        case "spectateDuel": {
-            if (activeDuels.hasOwnProperty(identifier)) {
-                activeDuels[identifier][uniqueID] = {
-                    ROLE: ROLE_SPECTATOR
-                };
-                console.log(response = response + 'spectatingDuel,' + identifier);
-                break;
+            if (!activeDuels.hasOwnProperty(duelID)) {
+                spark.join(duelID, function () {
+                    activeDuels[duelID] = {
+                        options: duelOptions
+                    };
+                    activeDuels[duelID][uid] = {
+                        ROLE: ROLE_HOST
+                    };
+                    writeResponse(spark, [200, 'hostedDuel', duelID]);
+                });
+                return;
             } else {
-                console.log(response = response + "invalidSpectatorDuelID," + identifier);
-                break;
-            }
-        }
-        case "duelQuery": {
-            // possible queries: "kick:userName", "getState", "getOptions", "startDuel"
-            if (activeDuels.hasOwnProperty(identifier)) {
-                if (params.indexOf("kick:") === 0) {
-                    params = params.substr(5);
-                    if (activeDuels[identifier].hasOwnProperty(params)) {
-                        delete activeDuels[identifier][params];
-                        console.log(response = response + 'kickedUser,' + params);
-                        break;
-                    }
-                }
-                if (params === QUERY_GET_STATE) {
-                    console.log(response = response + ((activeDuels[identifier].state && JSON.stringify(activeDuels[identifier].state)) || "emptyState"));
-                    break;
-                }
-                if (params === QUERY_GET_OPTIONS) {
-                    console.log(response = response + JSON.stringify(activeDuels[identifier].options));
-                    break;
-                }
-                if (params === QUERY_START_DUEL && activeDuels[identifier][uniqueID].ROLE === ROLE_HOST) {
-                    activeDuels[identifier].state = {}; // replace with: new GameState();
-                    console.log(response = response + "startedDuel" + identifier);
-                    break;
-                }
-            } else {
-                console.log(response = response + "invalidDuelQueryID," + identifier);
-                break;
-            }
-        }
-        case "duelCommand": {
-            if (activeDuels.hasOwnProperty(identifier) && activeDuels[identifier].state && activeDuels[identifier][uniqueID].ROLE > ROLE_SPECTATOR) {
-                // TODO: evaluate the command and change state
-                console.log(response = response + "commandEvaluated," + identifier + "," + JSON.stringify(activeDuels[identifier].state));
-                break;
-            } else {
-                WebSocket.close();
+                writeResponse(spark, [403, 'invalidRequest']);
                 return;
             }
         }
+        case "joinDuel": {
+            if (!duelID || !activeDuels.hasOwnProperty(duelID)) {
+                writeResponse(spark, [403, 'invalidRequest']);
+                return;
+            }
+            switch (Object.keys(activeDuels[duelID]).length) {
+                case 2: {
+                    spark.join(duelID, function () {
+                        activeDuels[duelID][uid] = {
+                            ROLE: ROLE_PLAYER_TWO
+                        };
+                        writeResponse(spark, [200, 'joinedDuel', duelID]);
+                    });
+                    return;
+                }
+                case 3: {
+                    spark.join(duelID, function () {
+                        activeDuels[duelID][uid] = {
+                            ROLE: ROLE_PLAYER_THREE
+                        };
+                        writeResponse(spark, [200, 'joinedDuel', duelID]);
+                    });
+                    return;
+                }
+                case 4: {
+                    spark.join(duelID, function () {
+                        activeDuels[duelID][uid] = {
+                            ROLE: ROLE_PLAYER_FOUR
+                        };
+                        writeResponse(spark, [200, 'joinedDuel', duelID]);
+                    }
+                    return;
+                }
+                default: {
+                    writeResponse(spark, [412, 'duelRoomFull', duelID]);
+                    return;
+                }
+            }
+        }
+        case "spectateDuel": {
+            if (!duelID || !activeDuels.hasOwnProperty(duelID)) {
+                writeResponse(spark, [403, 'invalidRequest']);
+                return;
+            }
+            spark.join(duelID, function () {
+                activeDuels[duelID][uid] = {
+                    ROLE: ROLE_SPECTATOR
+                };
+                writeResponse(spark, [200, 'spectatingDuel', duelID]);
+            });
+            return;
+        }
+        case "duelQuery": {
+            if (!duelID || !activeDuels.hasOwnProperty(duelID)) {
+                writeResponse(spark, [403, 'invalidRequest']);
+                return;
+            }
+            if (duelQuery.indexOf("kick ") === 0 && activeDuels[duelID][uid].ROLE === ROLE_HOST) {
+                duelQuery = duelQuery.split(' ')[1];
+                primus.room(duelID).write({
+                    event: 'kick',
+                    data: duelQuery
+                });
+                for(var user in registry) {
+                    if (registry[user].username === duelQuery) {
+                        delete activeDuels[duelID][registry[user].uid];
+                        break;
+                    }
+                }
+                writeResponse(spark, [200, 'kickedUser', duelQuery]);
+                return;
+            }
+            switch (duelQuery) {
+                case QUERY_DUEL_COMMAND: {
+                    processDuelCommand(duelCommand, duelID, spark);
+                    return;
+                }
+                case QUERY_GET_OPTIONS: {
+                    if (!duelID || !activeDuels.hasOwnProperty(duelID)) {
+                        writeResponse(spark, [403, 'invalidRequest']);
+                    }
+                    writeResponse(spark, [200, 'getOptionsResponse', activeDuels[duelID].options]);
+                    return;
+                }
+                case QUERY_GET_STATE: {
+                    if (!duelID || !activeDuels.hasOwnProperty(duelID) || !activeDuels.hasOwnProperty("state")) {
+                        writeResponse(spark, [403, 'invalidRequest']);
+                        return;
+                    }
+                    writeResponse(spark, [200, 'getStateResponse', activeDuels[duelID].state);
+                    return;
+                }
+                case QUERY_START_DUEL: {
+                    if (!duelID || !activeDuels.hasOwnProperty(duelID) || activeDuels[duelID][uid].ROLE !== ROLE_HOST) {
+                        writeResponse(spark, [403, 'invalidRequest']);
+                        return;
+                    }
+                    primus.room(duelID).write({
+                        event: 'duelStart',
+                        data: activeDuels[duelID]
+                    });
+                    activeDuels[duelID].state = GameState(Object.keys(activeDuels[duelID]).length - 1);
+                    activeDuels[duelID].duelStarted = true;
+                    writeResponse(spark, [200, 'duelStarted', duelID]);
+                    return;
+                }
+                default: {
+                    writeResponse(spark, [403, 'invalidRequest']);
+                    return;
+                }
+            }
+        }
         default: {
-            WebSocket.close();
+            writeResponse(spark, [403, 'invalidRequest']);
             return;
         }
     }
-    return response;
+}
+
+function handleClientDisconnect(spark) {
+    var id = spark.id,
+        duel;
+    if (registry.hasOwnProperty(id)) {
+        for(duel in activeDuels) {
+            if (activeDuels.hasOwnProperty(duel) && activeDuels[duel].hasOwnProperty(registry[id].uid)) {
+                primus.room(duel).write({
+                    event: 'playerDisconnect',
+                    data: registry[id].username
+                });
+                delete activeDuels[duel];
+                delete registry[id];
+                return;
+            }
+        }
+    }
+}
+
+function writeResponse(spark, dataArray) {
+    // dataArray format: [responseCode, event, data]
+    spark.write({
+        responseCode: dataArray[0],
+        event: dataArray[1],
+        data: dataArray[2] || {}
+    });
 }
