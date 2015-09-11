b343abf56102352032f18d8ca9222d622e66d1eb
IrateGod <konamijudge@yahoo.com>
2015-09-11 22:02:14 +0200

    Update http-gamelist.js

--- server/http/js/http-gamelist.js
+++ server/http/js/http-gamelist.js
@@ -404,6 +404,7 @@ function preformfilter(translated, players, rooms, started, pid, watchers) {
         pass = '',
         spectators = (watchers) ? ' +' + watchers : '';
     game = (translated.poolFormat !== 'Newgioh') ? game : 'newgioh';
+    game = (translated.poolFormat !== 'ARG') ? game : 'arg';
     game = (translated.poolFormat !== 'Unlimited') ? game : 'bad';
     game = (translated.poolFormat !== 'Traditional') ? game : 'bad';
     game = (translated.poolFormat !== 'Mega-Banned') ? game : 'bad';
