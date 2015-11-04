f6e71694a4d9ef6a38c0d160c407afb59d502995
Jamezs Gladney <jamezs@angelofcode.com>
2015-11-03 22:46:23 -0500

    fix

--- server/libs/ai.js
+++ server/libs/ai.js
@@ -20,7 +20,8 @@ var Primus = require('primus'), //Primus, our Sepiroth-Qliphopth Creator God. We
     Socket = require('primus').createSocket(),
     client = new Socket('http://ygopro.us:24555'), //Connect the God to the tree;
     childProcess = require('child_process'),
-    startDirectory = __dirname;
+    startDirectory = __dirname,
+    path = process.cwd() + '/../ai';
 
 
 
@@ -32,11 +33,15 @@ function gamelistUpdate(data) {
         console.log('[AI]:Event: Client Event');
         if (data.clientEvent === 'duelrequest' && data.target === 'SnarkyChild') {
             console.log('[AI]:Event: Duel Request for SnarkyChild');
+            console.log(path.resolve(__dirname + '/..ai/'));
             windbot = childProcess.spawn('windbot.exe', ['SnarkyChild', 'Hours', '127.0.0.1', '8911', data.roompass], {
-                cwd: startDirectory + '/../ai'
+                cwd: path
             });
             windbot.on('error', function (error) {
                 console.log('[AI]:Error:', error);
+            });
+            windbot.on('exit', function (extra) {
+                console.log('[AI]:Exit:', extra);
             });
         }
         return;
