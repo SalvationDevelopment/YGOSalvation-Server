b3c2240ac24440ea929e9b8adae97cc1a1df20ef
Jamezs Gladney <jamezs@angelofcode.com>
2015-11-19 01:54:15 -0500

    memoryleak fix

--- server/libs/forum-validator.js
+++ server/libs/forum-validator.js
@@ -27,13 +27,17 @@ function forumValidate(data, callback) {
         }, function (error, response, body) {
             if (!error && response.statusCode === 200) {
                 try {
-                    info = JSON.parse(body.trim());
+                    forumdata = JSON.parse(body.trim());
                 } catch (msgError) {
                     console.log('Error during validation', {}, body, msgError);
                     callback('Error during validation', info, body, msgError);
                     return;
                 }
-                validationCache[data.username] = info;
+                info.success = forumdata.success;
+                info.data = {};
+                info.data.g_access_cp = forumdata.data.g_access_cp;
+                info.displayname = forumdata.displayname
+                validationCache[data.username] = forumdata;
 
                 callback(null, info, body);
                 return;
@@ -45,34 +49,34 @@ function forumValidate(data, callback) {
 }
 
 module.exports = forumValidate;
-
-var crypto = require('crypto');
-
-
-function md5(input) {
-    return crypto.createHash('md5').update(input).digest("hex");
-}
-
-function encode(pass, salt) {
-    return md5(md5(process.env.SALT) + md5(pass));
-}
-
-function initDB(query) {
-
-    var connection = mysql.createConnection({
-        host: 'localhost',
-        user: process.env.MYSQLUSER,
-        password: process.env.MYSQLPASSWORD,
-        database: process.env.MYSQLFORUMDB
-    });
-
-    connection.connect();
-    connection.query(query, function (err, rows, fields) {
-        if (err) {
-            throw err;
-        }
-
-        console.log('The solution is: ', rows[0].solution);
-        connection.end();
-    });
-}
+//
+//var crypto = require('crypto');
+//
+//
+//function md5(input) {
+//    return crypto.createHash('md5').update(input).digest("hex");
+//}
+//
+//function encode(pass, salt) {
+//    return md5(md5(process.env.SALT) + md5(pass));
+//}
+//
+//function initDB(query) {
+//
+//    var connection = mysql.createConnection({
+//        host: 'localhost',
+//        user: process.env.MYSQLUSER,
+//        password: process.env.MYSQLPASSWORD,
+//        database: process.env.MYSQLFORUMDB
+//    });
+//
+//    connection.connect();
+//    connection.query(query, function (err, rows, fields) {
+//        if (err) {
+//            throw err;
+//        }
+//
+//        console.log('The solution is: ', rows[0].solution);
+//        connection.end();
+//    });
+//}
