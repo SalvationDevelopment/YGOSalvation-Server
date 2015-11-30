02ba90152c165257df22e500ce04b5c715fdb89d
Jamezs Gladney <jamezs@angelofcode.com>
2015-11-29 23:13:53 -0500

    debug

--- server/libs/gamelist.js
+++ server/libs/gamelist.js
@@ -65,6 +65,7 @@ function handleCoreMessage(core_message_raw, port, pid, game) {
 
     core_message = core_message_raw.toString().split('|');
     core_message[0] = core_message[0].trim();
+    console.log(core_message);
     if (core_message[1] === undefined) {
         return gamelist;
     }
