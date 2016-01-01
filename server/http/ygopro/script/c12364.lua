326f0e19f8e7ab34d22408579655551aafb26985
Zayelion <panthrowzay@gmail.com>
2015-12-31 21:34:10 -0500

    mass remove

--- server/http/ygopro/script/c12364.lua
+++
@@ -1,8 +0,0 @@
---Machine Angel Ritual
-function c12364.initial_effect(c)
-	aux.AddRitualProcGreater(c,c12364.ritual_filter)
-end
-function c12364.ritual_filter(c)
-	local code=c:GetCode()
-	return code==12363 or code==12365 or code==12366
-end
--- server/http/ygopro/script/c33044783.lua
+++
@@ -1,4 +0,0 @@
---Giant Weasel Explosion
-function c33044783.initial_effect(c)
-
-end
