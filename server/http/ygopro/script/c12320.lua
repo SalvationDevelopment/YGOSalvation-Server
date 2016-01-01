318841cb90418b5c5edb81418b08a0e8dc8e7248
Zayelion <panthrowzay@gmail.com>
2015-12-31 21:34:10 -0500

    mass remove

--- server/http/ygopro/script/c12320.lua
+++
@@ -1,4 +0,0 @@
---Dark Dragon Ritual
-function c12320.initial_effect(c)
-	aux.AddRitualProcGreater(c,aux.FilterBoolFunction(Card.IsCode,12319))
-end
--- server/http/ygopro/script/c41426870.lua
+++
@@ -1,4 +0,0 @@
---BLACK ILLUSION RITUAL used for GOAT FORMAT AND NEWGIOH
-function c41426870.initial_effect(c)
-	aux.AddRitualProcGreater(c,aux.FilterBoolFunction(Card.IsCode,64631467))
-end
