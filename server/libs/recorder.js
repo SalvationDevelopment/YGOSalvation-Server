1c122f52ca4fab396386201e4ae37a1bf031ab24
Zayelion <panthrowzay@gmail.com>
2016-01-17 08:44:06 -0500

    draft of cloud replays

---
+++ server/libs/recorder.js
@@ -0,0 +1,121 @@
+/*jslint node:true, plusplus:true*/
+'use strict';
+
+
+var net = require('net'),
+    fs = require('fs'),
+    recieveCTOS = require('./recieveCTOS'),
+    Framemaker = require('./parseframes.js');
+
+function ctos_PlayerInfo(message) {
+    var ctos = new Buffer([0x10]),
+        name = Array.apply(null, new Array(40)).map(Number.prototype.valueOf, 0),
+        username = new Buffer(message, 'utf16le'),
+        usernamef = new Buffer(name),
+        x = username.copy(usernamef),
+        len = usernamef.length + 1,
+        proto = new Buffer(2);
+    proto.writeUInt16LE(len, 0);
+    proto = Buffer.concat([proto, ctos, usernamef]);
+    //console.log(proto);
+    return proto;
+}
+
+function ctos_JoinGame(roompass) {
+    var ctos = new Buffer([0x12]),
+        name = Array.apply(null, new Array(60)).map(Number.prototype.valueOf, 0),
+        version = new Buffer([0x38, 0x13]),
+        gameid = new Buffer([75, 144, 0, 0, 0, 0]),
+        pass = new Buffer(roompass, 'utf16le'),
+        rpass = new Buffer(name),
+        x = pass.copy(rpass),
+        len = ctos.length + version.length + gameid.length + 60,
+        proto = new Buffer(2);
+    //unknownDataStructure = new Buffer([0,0,0,0,254,255,255,255,230,110,238,118]);
+    proto.writeUInt16LE(len, 0);
+
+    proto = Buffer.concat([proto, ctos, version, gameid, rpass]);
+    //console.log(proto);
+    //console.log(rpass.toString('utf16le'));
+    return proto;
+
+}
+
+function record(port, roompass) {
+    var toSend = Buffer.concat([ctos_PlayerInfo('Recorder'), ctos_JoinGame(roompass)]),
+        duel = new Buffer(),
+        socket = net.createConnection(port, '127.0.0.1');
+
+    console.log('Socket created.');
+    socket.on('data', function (data) {
+        duel = Buffer.concat(duel, data);
+    }).on('connect', function () {
+        socket.write(toSend);
+    }).on('end', function () {
+        fs.write(roompass, duel, function () {
+
+        });
+    });
+}
+
+function processTask(task, socket) {
+
+    var i = 0,
+        l = 0,
+        output = [];
+    for (i; task.length > i; i++) {
+        output.push(recieveCTOS(task[i], socket.username, socket.hostString));
+    }
+
+    for (l; output.length > l; l++) {
+        if (output[l].CTOS_JOIN_GAME) {
+            socket.active = true;
+            socket.hostString = output[l].CTOS_JOIN_GAME;
+        }
+        if (output[l].CTOS_PLAYER_INFO) {
+            socket.username = output[l].CTOS_PLAYER_INFO;
+        }
+    }
+}
+
+
+function processIncomingTrasmission(data, socket, task) {
+    processTask(task, socket);
+    if (socket.hostString) {
+        if (fs.exist(socket.hostString)) {
+            fs.readFile(socket.hostString, function callback(errorMSG, data) {
+                if (errorMSG) {
+                    return;
+                } else {
+                    socket.write(data);
+                    socket.close();
+                }
+
+            });
+        }
+    }
+    return data;
+}
+net.createServer(function (socket) {
+    var parsePackets = require('./parsepackets.js'),
+        framer = new Framemaker();
+
+    socket.setNoDelay(true);
+    socket.active_ygocore = false;
+    socket.active = false;
+    socket.on('data', function listener(data) {
+        var frame,
+            task,
+            newframes = 0;
+        if (socket.active_ygocore) {
+            socket.active_ygocore.write(data);
+        }
+        frame = framer.input(data);
+        for (newframes; frame.length > newframes; newframes++) {
+            task = parsePackets('CTOS', new Buffer(frame[newframes]));
+            processIncomingTrasmission(data, socket, task);
+        }
+        frame = [];
+
+    });
+}).listen(1198, '127.0.0.1');
