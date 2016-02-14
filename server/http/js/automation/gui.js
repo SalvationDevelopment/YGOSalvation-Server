6732b8d778023d0b7bdbcd5e22b599a3eeb074d1
Zayelion <panthrowzay@gmail.com>
2016-02-13 23:02:11 -0500

    Manual Mode
    - Onclick Events

--- server/http/js/automation/gui.js
+++ server/http/js/automation/gui.js
@@ -17,6 +17,10 @@ function cardCollections(player) {
 
 (function wireUpUI() {
     'use strict';
+    // these are wiring functions or rather controllers for the UI.
+    // they are bounded to a higher level than the objects they 
+    // interact with because the objects get dynamically created.
+
     $('body').on('click', '.okButton', function fireOKCallbackTrue() {
         window[gui.OKCallback](true);
         $('#alertUI').css('display', 'none');
@@ -25,6 +29,9 @@ function cardCollections(player) {
     $('body').on('click', '.cancelButton', function fireOKCallbackFalse() {
         window[gui.OKCallback](false);
         $('#alertUI').css('display', 'none');
+    });
+    $('div').on('hover', '.card', function displayCardOptions(cardElement) {
+        // user is hovering over a card, display the information about it.
     });
 
     $('body').on('click', '.card', function displayCardOptions(cardElement) {
@@ -38,19 +45,164 @@ function cardCollections(player) {
 
     $('body').on('click', '.setViaAutomation', function setViaAutomation(id) {
         //remove the prompt
-        //reply via socket you want to set card id.
+        //reply via the automation-ws you want to set card id.
     });
     $('body').on('click', '.activateViaAutomation', function activateViaAutomation(id) {
         //remove the prompt
-        //reply via socket you want to activate card id.
+        //reply via the automation-ws you want to activate card id.
     });
     $('body').on('click', '.summonViaAutomation', function summonViaAutomation(id) {
         //remove the prompt
-        //reply via socket you want to summon card id.
+        //reply via the automation-ws you want to summon card id.
     });
     $('body').on('click', '.specialSummonViaAutomation', function specialSummonViaAutomation(id) {
         //remove the prompt
-        //reply via socket you want to special summon card id.
+        //reply via the automation-ws you want to special summon card id.
+    });
+    $('body').on('click', '.manualReveal', function manualReveal(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualToBottom', function manualToBottom(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualToTop', function manualToTop(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualToSetTrapSpell', function manualToSetTrapSpell(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualBanish', function manualBanish(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualBanishFaceDown', function manualBanishFaceDown(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.moveToHand', function moveToHand(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualToST', function manualToST(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualSpecialSummonDef', function manualSpecialSummonDef(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualSpecialSummonAtt', function manualSpecialSummonAtt(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualSetMonster', function manualSetMonster(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualNormalSummon', function manualNormalSummon(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualActivateFieldSpell', function manualActivateFieldSpell(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualViewExtra', function manualViewExtra(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualToExtaFaceDown', function manualToExtaFaceDown(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualRevealExta', function manualRevealExta(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualToExtraFaceUp', function manualToExtraFaceUp(id) {
+        //remove the prompt
+        // make sure its a token
+
+    });
+    $('body').on('click', '.moveChangeControl', function moveChangeControl(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualOverlay', function manualOverlay(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualDetach', function manualOverlay(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualFlipDown', function manualFlipDown(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualFlipUp', function manualFlipUp(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualToAtk', function manualToAtk(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualToDef', function manualToDef(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualRemove', function manualRemove(id) {
+        //remove the prompt
+        // make sure its a token
+
+    });
+    $('body').on('click', '.manualToPZoneR', function manualToPZoneR(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualToPZoneL', function manualToPZoneL(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualToGrave', function manualToGrave(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualToOpponentsHand', function manualToOpponentsHand(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualToOpponentsGrave', function manualToOpponentsGrave(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualToOpponentsExtra', function manualToOpponentsExtra(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualToDeck', function manualToDeck(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualToOpponentsDeck', function manualToOpponentsDeck(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualMill', function manualMill(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualShuffle', function manualShuffle(id) {
+        //remove the prompt
+
+    });
+    $('body').on('click', '.manualDraw', function manualDraw(id) {
+        //remove the prompt
     });
 }());
 
