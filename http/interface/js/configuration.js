0d7925720614190af0689f447fbc7ad7fccffb2b
Zayelion <panthrowzay@gmail.com>
2016-03-30 03:01:17 -0400

    server side interface

---
+++ http/interface/js/configuration.js
@@ -0,0 +1,12 @@
+/*jslint node : true */
+var developmentstage = {
+    "production": "http://ygopro.us/launcher.html",
+    "stage": "http://ygopro.us/launcher.html",
+    "development": "http://127.0.0.1/launcher.html"
+};
+var sitelocationdir = {
+    "production": "http://ygopro.us",
+    "stage": "http://ygopro.us",
+    "development": "http://127.0.0.1/"
+};
+var mode = "stage";
