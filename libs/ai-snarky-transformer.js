c1773ec2971a47fa34929fae10ac39c6dc1235e0
Zayelion <panthrowzay@gmail.com>
2016-05-06 00:12:00 -0400

    AI Snarky edits

--- libs/ai-snarky-transformer.js
+++ libs/ai-snarky-transformer.js
@@ -228,43 +228,6 @@ function updateMassCards(player, clocation, buffer) {
     return output;
 }
 
-/* READ THE FOLLOWING : https://github.com/SalvationDevelopment/YGOPro-Salvation-Server/issues/274 */
-function Framemaker() {
-    "use strict";
-    var memory = new Buffer([]);
-
-    this.input = function (buffer) {
-        var x = true,
-            output = [],
-            recordOfBuffer,
-            frame_length;
-        //console.log('before', memory.length, 'bytes in memory');
-        memory = Buffer.concat([memory, buffer]);
-        //console.log('concated', memory.length);
-        while (x === true && memory.length > 2) {
-            frame_length = memory.readUInt16LE(0);
-            //console.log('read', frame_length, '(+2) of', memory.length, 'bytes');
-            if ((memory.length - 2) < frame_length) {
-                //console.log('not enough');
-                x = false;
-            } else {
-                recordOfBuffer = memory.slice(2).toJSON();
-                output.push(recordOfBuffer);
-                if (memory.length === (frame_length + 2)) {
-                    memory = new Buffer([]);
-                    x = false;
-                } else {
-                    memory = memory.slice((frame_length + 2));
-                }
-                //console.log('after', memory.length);
-            }
-        }
-        //console.log('----',output);
-        return output;
-    };
-    return this;
-}
-
 
 function BufferStreamReader(packet) {
     'use strict';
@@ -1456,38 +1419,8 @@ function recieveSTOC(packet) {
 }
 
 
-function CommandParser() {
-    'use strict';
-    var output = {};
-    output = new EventEmitter();
-    output.input = function (input) {
-        output.emit(input.command, input);
-    };
-    return output;
-}
 
-function parsePackets(command, message) {
-    "use strict";
 
-    var task = [],
-        packet = {
-            message: message.slice(1),
-            readposition: 0
-        };
-    packet[command] = enums[command][message[0]];
-    task.push(packet);
-    return task;
-}
-
-function processTask(task, socket) {
-    'use strict';
-    var i = 0,
-        l = 0,
-        output = [],
-        RESPONSE = false;
-    for (i; task.length > i; i++) {
-        output.push(recieveSTOC(task[i]));
-    }
-
-    return output;
+modules.exports = {
+    recieveSTOC: recieveSTOC
 }
