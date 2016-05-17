4f7e944c16a361fe1bf8e3499948f5c94a9cf1d0
Zayelion <panthrowzay@gmail.com>
2016-05-16 22:50:00 -0400

    get setcodes

--- http/js/updatesystem.js
+++ http/js/updatesystem.js
@@ -510,14 +510,7 @@ function processServerRequest(data) {
 
 
 
-/*Boot command*/
-setTimeout(function () {
-    'use strict';
-    deleteFolderRecursive('./ygopro/expansions', true);
-    fs.watch('./ygopro/deck', function (occurance) {
-        populatealllist();
-    });
-}, 2500);
+
 
 screenMessage.html('Update System Loaded');
 
@@ -671,3 +664,97 @@ function getCustoms(target) {
     }
 
 }
+
+function ConfigParser(content, options) {
+    "use strict";
+    if (!(this instanceof ConfigParser)) {
+        return new ConfigParser(content, options);
+    }
+    var commentDelims = [
+            "#",
+            ";",
+            "@",
+            "//"
+        ],
+        blockRegexp = /^\s?\[\s?(.*?)\s?\]\s?$/,
+        keyValueDelim = "=",
+        newLineDelim = "\r\n",
+        joinKeyValue = false,
+        joinKeySlice = 0,
+        configObject = {},
+        currentBlock,
+        currentLine;
+    if (typeof options === "object") {
+        commentDelims = options.commentDelims || commentDelims;
+        blockRegexp = options.blockRegexp || blockRegexp;
+        keyValueDelim = options.keyValueDelim || keyValueDelim;
+        newLineDelim = options.newLineDelim || newLineDelim;
+        joinKeyValue = options.joinKeyValue || joinKeyValue;
+        joinKeySlice = options.joinKeySlice || joinKeySlice;
+    }
+    content = content.split(newLineDelim);
+    content.forEach(function (line) {
+        var isComment = false;
+        commentDelims.forEach(function (delim) {
+            if (line.indexOf(delim) === 0) {
+                isComment = true;
+            } else {
+                return;
+            }
+        });
+        if (isComment) {
+            return;
+        }
+        if (blockRegexp.test(line)) {
+            currentBlock = line.replace(blockRegexp, '$1');
+            configObject[currentBlock] = {};
+            return;
+        }
+        currentLine = line.split(keyValueDelim);
+        if (currentBlock === undefined) {
+            configObject[currentLine[joinKeySlice]] = joinKeyValue ? currentLine.slice(joinKeySlice + 1).join(keyValueDelim) : currentLine[1];
+        } else {
+            configObject[currentBlock][currentLine[joinKeySlice]] = joinKeyValue ? currentLine.slice(joinKeySlice + 1).join(keyValueDelim) : currentLine[1];
+        }
+    });
+    return configObject;
+}
+
+function updateSetcodes() {
+    'use strict';
+    fs.readfile('./ygopro/strings.conf', function (error, data) {
+        if (error) {
+            return;
+        }
+        var setcodes = new ConfigParser(data, {
+                keyValueDelim: " ",
+                commentDelims: [],
+                blockRegexp: new RegExp("^\\s?#(.*?)\\s?$/"),
+                joinKeyValue: true,
+                joinKeySlice: 1
+            }).setcodes,
+            setcode,
+            monsterCardSelect = $('.monsterCardSelect'),
+            monsterTypeSelect = $('.monsterTypeSelect'),
+            spellSelect = $('.spellSelect'),
+            trapSelect = $('.trapSelect'),
+            raceSelect = $('.raceSelect'),
+            attributeSelect = $('.attributeSelect');
+
+        for (setcode in setcodes) {
+            if (setcodes.hasOwnProperty(setcode)) {
+                frames[0].$('.setcodeSelect').append('<option value="' + parseInt(setcode, 16) + '">' + setcodes[setcode] + '</option>');
+            }
+        }
+    });
+}
+
+/*Boot command*/
+setTimeout(function () {
+    'use strict';
+    updateSetcodes();
+    deleteFolderRecursive('./ygopro/expansions', true);
+    fs.watch('./ygopro/deck', function (occurance) {
+        populatealllist();
+    });
+}, 2500);
