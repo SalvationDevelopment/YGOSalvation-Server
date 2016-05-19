87412f6156f710965ce925ef889812b1dd6b1ce5
Zayelion <panthrowzay@gmail.com>
2016-05-19 01:49:38 -0400

    fix

--- http/js/index-controls.js
+++ http/js/index-controls.js
@@ -454,9 +454,9 @@ function makedatasSQL() {
     });
     category = '"' + category + '"';
     console.log(category);
-    level = '0x' + leftpad($('#sqlscalel').val(), 2, 0) + leftpad($('#sqlscaler').val(), 2, 0) + leftpad($('#sqllevel').val(), 4, 0);
+    level = '0x' + leftpad($('#sqlscalel').attr(), 2, 0) + leftpad($('#sqlscaler').val(), 2, 0) + leftpad($('#sqllevel').val(), 4, 0);
     level = '"' + parseInt(level, 16) + '"';
-    setcode = '0x' + leftpad($('#sqlsc4').val(), 3, 0) + leftpad($('#sqlsc3').val(), 3, 0) + leftpad($('#sqlsc2').val(), 3, 0) + leftpad($('#sqlsc1').val(), 3, 0);
+    setcode = '0x' + leftpad($('#sqlsc4').attr('data-calc'), 3, 0) + leftpad($('#sqlsc3').attr('data-calc'), 3, 0) + leftpad($('#sqlsc2').attr('data-calc'), 3, 0) + leftpad($('#sqlsc1').attr('data-calc'), 3, 0);
     setcode = '"' + parseInt(setcode, 16) + '"';
     texts = [id, ot, alias, setcode, type, atk, def, level, race, attribute, category].join(',');
     return 'INSERT OR REPLACE INTO "datas" VALUES (' + texts + ');';
