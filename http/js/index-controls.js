2d54e7d046fcc1077468d7058e9682d44fdd2884
Zayelion <panthrowzay@gmail.com>
2016-05-19 02:40:30 -0400

    revert

--- http/js/index-controls.js
+++ http/js/index-controls.js
@@ -403,6 +403,8 @@ function maketextsSQL() {
         str12 = '"' + $('#sqlstr12').val() + '"',
         str13 = '"' + $('#sqlstr13').val() + '"',
         str14 = '"' + $('#sqlstr14').val() + '"',
+        str15 = '"' + $('#sqlstr15').val() + '"',
+        str16 = '"' + $('#sqlstr16').val() + '"',
         datas = [id, name, description, str1, str2, str3, str4, str5, str5, str6, str7, str8, str9, str10, str11, str11, str12, str13, str14, str15, str16].join(',');
 
     return 'INSERT OR REPLACE INTO "texts" VALUES (' + datas + ');';
