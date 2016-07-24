0dc0a3497f8b56a5d50e54e21902dde6bedc812a
StormWulf <auronzeroo@aim.com>
2016-07-24 01:29:10 -0400

    Revert "... Let's pray"
    This reverts commit b9c2851a8567d6713675d6ce803a19219f8a233f.

--- http/js/updatesystem.js
+++ http/js/updatesystem.js
@@ -788,14 +788,13 @@ function updateSetcodes() {
                 joinKeySlice: 1
             }),
             setcode,
-            strings = '<datalist id="setcodes" value="None" data-value="0"><option value="None" data-value="0"></option>';
+            strings = '<option value="None" data-value="0"></option>';
         console.log(setcodes);
         for (setcode in setcodes) {
             if (setcodes.hasOwnProperty(setcode) && setcode[0] === '0' && setcode[1] === 'x' && setcode !== '0x0') {
                 strings = strings + '<option data-value="' + parseInt(setcode, 16) + '" value="' + setcodes[setcode] + '"> </option>';
             }
         }
-		strings+= '</datalist>'
         frames[0].$('.setcodeSelect').html(strings);
     });
 }
