178e32023d747bef055715521da35fd776c3eb56
Chibi Chan <chibi_theone@yahoo.com>
2016-09-29 21:19:09 -0500

    added randomString function
    there!

--- libs/manual2.js
+++ libs/manual2.js
@@ -6,3 +6,11 @@ wss.on('connection', function(ws) {
     });
     ws.send('something');
 });
+
+function randomString(len){
+    var text = "";
+    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for( var i=0; i < len; i++ )
+        text += chars.charAt(Math.floor(Math.random() * chars.length));
+    return text;
+}
