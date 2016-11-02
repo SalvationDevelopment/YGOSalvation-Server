60b9ae5f67857bf46ffa06eb35a38ebf2d27cda2
IrateGod <konamijudge@yahoo.com>
2016-11-02 09:48:36 +0100

    Add validate-deck.js

---
+++ http/js/validate-deck.js
@@ -0,0 +1,103 @@
+function validateDeck (deck, banlist) {
+  var validate = {
+    error: false,
+    msg: ''
+  };
+  // make sure we can work with the input
+  if (!deck.main || !deck.side || !deck.extra || typeof deck.main.forEach !== 'function' || typeof deck.side.forEach !== 'function' || typeof deck.extra.forEach !== 'function') {
+    validate.error = true;
+    validate.msg = 'Invalid deck object';
+    return validate;
+  }
+  // check deck lengths
+  if (deck.main.length < 40) {
+    validate.error = true;
+    validate.msg = 'Main Deck size below 40';
+    return validate;
+  }
+  if (deck.main.length > 60) {
+    validate.error = true;
+    validate.msg = 'Main Deck size above 60';
+    return validate;
+  }
+  if (deck.side.length > 15) {
+    validate.error = true;
+    validate.msg = 'Side Deck size above 15';
+    return validate;
+  }
+  if (deck.extra.length > 15) {
+    validate.error = true;
+    validate.msg = 'Extra Deck size above 15';
+    return validate;
+  }
+  // remap decks
+  var main = {};
+  var side = {};
+  var extra = {};
+  deck.main.forEach(function (card) {
+    if (!main[card]) {
+      main[card] = 1;
+    } else {
+      main[card]++;
+    }
+  });
+  deck.side.forEach(function (card) {
+    if (!side[card]) {
+      side[card] = 1;
+    } else {
+      side[card]++;
+    }
+  });
+  deck.extra.forEach(function (card) {
+    if (!extra[card]) {
+      extra[card] = 1;
+    } else {
+      extra[card]++;
+    }
+  });
+  // check amount of cards
+  for (var card in main) {
+    if (main[card] > 3 || side[card] && main[card] + side[card] > 3) {
+      validate.error = true;
+      validate.message = card;
+      return validate;
+    }
+  }
+  for (var card in side) {
+    if (side[card] > 3 || main[card] && main[card] + side[card] > 3) {
+      validate.error = true;
+      validate.message = card;
+      return validate;
+    }
+  }
+  for (var card in extra) {
+    if (extra[card] > 3 || side[card] && extra[card] + side[card] > 3) {
+      validate.error = true;
+      validate.message = card;
+      return validate;
+    }
+  }
+  // check banlist, assume banlist is an object generated from ConfigParser()
+  for (var card in banlist) {
+    var cardAmount = 0;
+    if (main[card]) {
+      cardAmount += main[card];
+    }
+    if (side[card]) {
+      cardAmount += side[card];
+    }
+    if (extra[card]) {
+      cardAmount += extra[card];
+    }
+    if (cardAmount > banlist[card]) {
+      validate.error = true;
+      validate.message = card;
+      return validate;
+    }
+  }
+  return validate;
+}
+
+if (module && typeof module.exports !== 'undefined') {
+  module.exports = validateDeck;
+}
