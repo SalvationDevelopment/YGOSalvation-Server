e0e51332ebc4163bcecb90de578f40fb2e108867
Zayelion <panthrowzay@gmail.com>
2016-11-05 15:58:10 -0400

    murder

--- libs/bansystem.js
+++ libs/bansystem.js
@@ -3,5 +3,6 @@ require('fs').watch(__filename, process.exit);
 
 
 module.exports = {
-    // 'Nagate': "Really bad SJW authoritrism/racism, swearing billegerently during a duel. Pissing AccessDenied off."
+    // 'Nagate': "Really bad SJW authoritrism/racism, swearing billegerently during a duel. Pissing AccessDenied off.",
+    'lookatme ': "Aggrivating staff and users repeatedly."
 };
