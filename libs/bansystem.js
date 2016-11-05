26bf6fbc9f67ef2750ff6bacfa8713bc009d6a59
Jeff Falberg <auronzeroo@aim.com>
2016-11-05 16:34:33 -0400

    Case-sensitivity is a bitch

--- libs/bansystem.js
+++ libs/bansystem.js
@@ -4,5 +4,5 @@ require('fs').watch(__filename, process.exit);
 
 module.exports = {
     // 'Nagate': "Really bad SJW authoritrism/racism, swearing billegerently during a duel. Pissing AccessDenied off.",
-    'Madeyouloook': "Aggrivating staff and users repeatedly."
+    'madeyouloook': "Aggrivating staff and users repeatedly."
 };
