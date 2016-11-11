d056ad282b89b432d34c559929187e79ddee43e8
StormWulf <auronzeroo@aim.com>
2016-11-10 21:29:51 -0500

    Die die die

--- libs/bansystem.js
+++ libs/bansystem.js
@@ -6,6 +6,8 @@ module.exports = {
     // 'Nagate': "Really bad SJW authoritrism/racism, swearing billegerently during a duel. Pissing AccessDenied off.",
     'madeyouloook': "Aggravating staff and users repeatedly, intentionally crashed games.",
     'Disconnect': "madeyouloook's multiple account",
-	'iDCniggas': "Intentionally crashing games"
+	'iDCniggas': "Intentionally crashing games",
+	'idcniggas': "This file needs to be case-insensitive",
+	'disconnect': "This file needs to be case-insensitive"
 };
 
