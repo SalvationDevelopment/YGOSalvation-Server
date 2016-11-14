fd7608f36a95bba4d94ac90c0fad5530edf4d8c4
Zayelion <panthrowzay@gmail.com>
2016-11-13 20:53:35 -0500

    rearrange

--- http/js/manual/http-manual.js
+++ http/js/manual/http-manual.js
@@ -584,17 +584,18 @@ function automaticZonePicker(realPlayer, zone) {
     if ($('#automationduelfield .p' + player + '.' + zone + '.i1').length < 1) {
         result = 1;
     }
-    if ($('#automationduelfield .p' + player + '.' + zone + '.i3').length < 1) {
-        result = 3;
-    }
-    if ($('#automationduelfield .p' + player + '.' + zone + '.i4').length < 1) {
-        result = 4;
-    }
+
     if ($('#automationduelfield .p' + player + '.' + zone + '.i0').length < 1) {
         result = 0;
     }
     if ($('#automationduelfield .p' + player + '.' + zone + '.i2').length < 1) {
         result = 2;
+    }
+    if ($('#automationduelfield .p' + player + '.' + zone + '.i3').length < 1) {
+        result = 3;
+    }
+    if ($('#automationduelfield .p' + player + '.' + zone + '.i4').length < 1) {
+        result = 4;
     }
 
 
