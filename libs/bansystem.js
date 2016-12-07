4fb6573872bbb183aecf73dbe8886f56ca88ed6e
Zayelion <panthrowzay@gmail.com>
2016-12-07 00:47:07 -0500

    Colbart Axis Ban

--- libs/bansystem.js
+++ libs/bansystem.js
@@ -6,10 +6,10 @@ module.exports = {
     // 'Nagate': "Really bad SJW authoritrism/racism, swearing billegerently during a duel. Pissing AccessDenied off.",
     'madeyouloook': "Aggravating staff and users repeatedly, intentionally crashed games.",
     'Disconnect': "madeyouloook's multiple account",
-	'iDCniggas': "Intentionally crashing games",
-	'idcniggas': "This file needs to be case-insensitive",
-	'disconnect': "This file needs to be case-insensitive",
-	'boosted turtle': "Intentionally crashing games",
-	'itstimetoduel': "Intentionally crashing games"
+    'iDCniggas': "Intentionally crashing games",
+    'idcniggas': "Killing Games",
+    'disconnect': "Killing Games",
+    'boosted turtle': "Intentionally crashing games",
+    'itstimetoduel': "Intentionally crashing games",
+    'Colbalt Axis': 'Rudeness to staff'
 };
-
