1a6be413acc0bf96001aa6fff4b4cb44928f41c8
Zayelion <panthrowzay@gmail.com>
2016-12-11 19:24:26 -0500

    Deck Editor

--- http/js/manual/deck-editor.js
+++ http/js/manual/deck-editor.js
@@ -1,1 +1,94 @@
-var usersDecks = [];
+/*global currentMousePos, getCardObject, reorientmenu, cardIs, $*/
+
+
+
+var deckEditor = (function () {
+    'use strict';
+    var usersDecks = [],
+        activeIndex = 0;
+
+    function makeBlankDeck(name, username, date) {
+        return {
+            main: [],
+            extra: [],
+            side: [],
+            name: [],
+            creator: username,
+            creationDate: date
+        };
+    }
+
+    function makeNewDeck(name) {
+        usersDecks.push(makeBlankDeck(name, localStorage.nickname, new Date()));
+        return usersDecks[usersDecks.length - 1];
+    }
+
+    function updateDeckSelect() {
+        var text = '';
+        usersDecks.forEach(function (deck, index) {
+            text += '<option data-index=' + index + '>' + deck.name + '</option>';
+        });
+        return text;
+    }
+
+    function makeCard(cards, zone) {
+        var html = '';
+        cards.forEach(function (card, index) {
+            var hardcard = JSON.stringify(card),
+                src = 'ygopro/pics/' + card + '.jpg';
+            html += '<img class="deckeditcard" src="http://ygopro.us/' + src + '" data-"' + card + '" onclick = "deckeditonclick(' + index + ', \'' + zone + '\')" / > ';
+        });
+
+        $('.deckeditzone .' + zone).html(html);
+        //$('#subreveal').width(cards.length * 197);
+    }
+
+    function deckeditonclick(index, zone) {
+
+
+        $('#manualcontrols button').css({
+            'display': 'none'
+        });
+
+        $('#manualcontrols').css({
+            'top': currentMousePos.y,
+            'left': currentMousePos.x,
+            'display': 'block'
+        });
+        var sideReference = {
+                id: usersDecks[activeIndex][zone][index],
+                zone: zone,
+                index: index
+            },
+            dbEntry = getCardObject(parseInt(sideReference.id, 10));
+        if (sideReference.zone === 'main') {
+            $('.s-toside').css({
+                'display': 'block'
+            });
+
+        }
+        if (sideReference.zone === 'extra') {
+            $('.s-toside').css({
+                'display': 'block'
+            });
+        }
+        if (sideReference.zone === 'side') {
+            if (cardIs('xyz', dbEntry) || cardIs('fusion', dbEntry) || cardIs('synchro', dbEntry)) {
+                $('.s-toextra').css({
+                    'display': 'block'
+                });
+            } else {
+                $('.s-tomain').css({
+                    'display': 'block'
+                });
+            }
+        }
+        reorientmenu();
+        return;
+    }
+
+
+    return {
+        updateDeckSelect: updateDeckSelect
+    };
+}());
