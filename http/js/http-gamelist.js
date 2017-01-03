b201a894a7825a7811ca2d985584583570440ba2
Zayelion <panthrowzay@gmail.com>
2017-01-03 05:35:37 -0500

    remove tag

--- http/js/http-gamelist.js
+++ http/js/http-gamelist.js
@@ -961,8 +961,14 @@ $('#publicchat').keypress(function (e) {
 function userlistonclick(person) {
     'use strict';
     personOfIntrest = person;
+    var friends = deckEditor.getFriends();
     if (admin === "1") {
         $('.a-admin').css('display', 'block');
+    }
+    if (friends.indexOf(person) > -1) {
+        $('.a-remove').css('display', 'block');
+    } else {
+        $('.a-add').css('display', 'block');
     }
     $('.a-user').css('display', 'block');
     reorientmenu();
