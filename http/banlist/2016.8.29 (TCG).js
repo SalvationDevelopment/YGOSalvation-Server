9e9103c2b7638f5b4bfb48f1979e4c403197bef1
Jeff Falberg <auronzeroo@aim.com>
2017-02-23 10:50:06 -0500

    Change to js file and include comments

---
+++ http/banlist/2016.8.29 (TCG).js
@@ -0,0 +1,10 @@
+module.exports =
+{
+    bannedCards : {
+    },
+     bannedTypes : [],
+     exceptions : []
+     startDate : '2016-08-29',
+     endDate : '???',
+     name : '2016.8.29 (TCG)',
+}
