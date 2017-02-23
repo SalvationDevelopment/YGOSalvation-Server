c99cd21b4334e5be3ed602fa1f0cca89ca5febc3
Amine Belaid <AmineBelaid2909@gmail.com>
2017-02-23 01:51:28 -0500

    Testing Linked Zones

--- http/js/manual/http-manual.js
+++ http/js/manual/http-manual.js
@@ -2678,10 +2678,85 @@ function guicardonclick() {
 }
 var internalDB = [];
 
+function getLinkedZone(player, index, link) {
+    'use strict';
+    switch (link) {
+    case 0://Top-Left
+        if (index === 2) {
+            return {player: player, index: 5};
+        } else if (index === 4) {
+            return {player: player, index: 6};
+        } else if (index === 5) {
+            return {player: 1 - player, index: 4};
+        } else if (index === 6) {
+            return {player: 1 - player, index: 2};
+        }
+        break;
+    case 1://Top
+        if (index === 1) {
+            return {player: player, index: 5};
+        } else if (index === 3) {
+            return {player: player, index: 6};
+        } else if (index === 5) {
+            return {player: 1 - player, index: 3};
+        } else if (index === 6) {
+            return {player: 1 - player, index: 1};
+        }
+        break;
+    case 2://Top-Right
+        if (index === 0) {
+            return {player: player, index: 5};
+        } else if (index === 2) {
+            return {player: player, index: 6};
+        } else if (index === 5) {
+            return {player: 1 - player, index: 2};
+        } else if (index === 6) {
+            return {player: 1 - player, index: 0};
+        }
+        break;
+    case 3://Left
+        if (index > 0 && index < 5) {
+            return {player: player, index: index - 1};
+        }
+        break;
+    case 4://Right
+        if (index >= 0 && index < 4) {
+            return {player: player, index: index + 1};
+        }
+        break;
+    case 5://Bottom-Left
+        if (index === 5) {
+            return {player: player, index: 0};
+        } else if (index === 6) {
+            return {player: player, index: 2};
+        }
+        break;
+    case 6://Bottom
+        if (index === 5) {
+            return {player: player, index: 1};
+        } else if (index === 6) {
+            return {player: player, index: 3};
+        }
+        break;
+    case 7://Bottom-Right
+        if (index === 5) {
+            return {player: player, index: 2};
+        } else if (index === 6) {
+            return {player: player, index: 4};
+        }
+        break;
+    }
+    return null;
+}
 
 function processCardHover(event) {
     'use strict';
     var uid = event.currentTarget.id,
+        idIndex = window.manualDuel.uidLookup(record),
+        stackunit = window.manualDuel.stack[idIndex],
+        uindex = stackunit.index,
+        uplayer = stackunit.player,
+        ulinks = stackunit.links,
         html = '';
 
     record = parseInt($('#' + uid).attr('data-uid'), 10);
@@ -2694,7 +2769,14 @@ function processCardHover(event) {
     $('.imgContainer').attr('src', $('#' + event.currentTarget.id).attr('src'));
     $('.cardDescription').html(html);
 
-
+    for (var link in ulinks) {
+        var linkedZone = getLinkedZone(uplayer, uindex, ulinks);
+        if (linkedZone !== null) {
+            var linkPlayer = linkedZone.player,
+                linkIndex = linkedZone.index;
+            $('.cardselectionzone.p' + linkPlayer + 'SPELLZONE.i' + linkIndex).addClass('attackglow card');
+        }
+    }
 }
 
 $(document).ready(function () {
