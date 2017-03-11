f48761ec6118f083a76798a5a6e54a3a9a518c67
Jeff Falberg <auronzeroo@aim.com>
2017-03-11 16:05:11 -0500

    Fixed stuff accidentally removed

--- libs/manual.js
+++ libs/manual.js
@@ -314,14 +314,16 @@ module.exports = function (wss) {
                 break;
             }
             if (socket.slot !== undefined) {
-
+				var banlist = require('../http/banlist/' + games[activeduel].banlist + '.js');
+				message.validate = validateDeck(message.deck, banlist, database);
+				console.log(banlist);
                 try {
-                    var banlist = require('../http/banlist/2016.8.29 (TCG Advanced).js');
                     message.validate = validateDeck(message.deck, banlist, database);
                     if (message.validate) {
                         if (message.validate.error) {
                             console.log(message.validate.error);
                             socket.send(JSON.stringify({
+								errorType: 'validation',
                                 action: 'error',
                                 error: message.validate.error,
                                 msg: message.validate.msg
