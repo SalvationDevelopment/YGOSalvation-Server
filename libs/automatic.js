e0b3b12cf2cf2e398607e7c9dd279b44ffb8e4bd
Zayelion <panthrowzay@gmail.com>
2017-03-19 22:10:32 -0400

    Order of Operations

--- libs/automatic.js
+++ libs/automatic.js
@@ -4,17 +4,56 @@
 
 // review the following https://github.com/Fluorohydride/ygopro-core/blob/master/processor.cpp
 
+var DRAW_PHASE = 0,
+    STANDBY_PHASE = 1;
+
+/*
+    Action Object
+    {
+        command  : {Function},
+        callback : {Function},
+        params   : {Array}
+    }
+    Can be understood as:
+    `command(...params, callback);`
+    
+    When sending a question to the user, make sure to set a duel.responseEngine.once listener with a
+    unique id to catch the response. Do not use `.on`.
+*/
 
 /**
- * Process a queue of actions.
+ * Process a queue of actions (as defined by above Action Object).
  * @param {Array} actionQueue Array of action objects. Each contains a command and parameters.
  */
-function processActionQueue(actionQueue) {
-    var currentAction;
-    while (actionQueue.length !== 0) {
-        currentAction = actionQueue.shift();
-        currentAction.command.apply(currentAction.parameters);
-    }
+function processActionQueue(actionQueue, callback) {
+    waterfall(actionQueue.map(function (action) {
+        return function (lastActionResult, nextCallback) {
+            var params = action.parameters || [],
+                actionCallback = function (error, result) {
+                    if (error) {
+                        nextCallback(error);
+                    } else {
+                        if (typeof action.callback === 'function') {
+                            action.callback(error, result);
+                        }
+                        nextCallback(null, true);
+                    }
+                };
+            params.push(actionCallback);
+            action.command.apply(params);
+        };
+    }), callback);
+}
+
+
+function askYesNo(duel, player, id, callback) {
+    var question = {
+        action: 'askYesNo',
+        player: player,
+        id: id
+    };
+    duel.question(question);
+    duel.responseEngine.once(id, callback);
 }
 
 /**
@@ -40,11 +79,18 @@ function doDrawPhase(duel, drawPhaseActionQueue) {
         player = state.turnOfPlayer,
         turnCount = state.turnCount;
 
-    processActionQueue(drawPhaseActionQueue);
+    processActionQueue(drawPhaseActionQueue, function () {
+        if (turnCount && !state.skipDrawPhase) {
+            duel.drawCard(player, 1, duel.playerName[player]);
+        }
+        duel.nextPhase(STANDBY_PHASE);
+        processActionQueue.push({
+            command: doStandbyPhase,
+            params: [duel, standbyPhaseActionQueue]
+        });
+    });
 
-    if (turnCount && !state.skipDrawPhase) {
-        duel.drawCard(player, 1, duel.playerName[player]);
-    }
+
     return;
 }
 
