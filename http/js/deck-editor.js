d143980185dae9e2d9f8e25eb4c0986d2d1cfac0
Zayelion <panthrowzay@gmail.com>
2017-03-23 05:09:50 -0400

    No more strings.conf

--- http/js/deck-editor.js
+++ http/js/deck-editor.js
@@ -133,62 +133,6 @@ var databaseSystem = (function () {
         return banlist[activeBanlist].bannedCards;
     }
 
-
-
-    function configParser(content, options) {
-        var commentDelims = [
-            "#",
-            ";",
-            "@",
-            "//"
-        ],
-            blockRegexp = /^\s?\[\s?(.*?)\s?\]\s?$/,
-            keyValueDelim = "=",
-            newLineDelim = "\r\n",
-            joinKeyValue = false,
-            joinKeySlice = 0,
-            configObject = {},
-            currentBlock,
-            currentLine;
-        if (typeof options === "object") {
-            commentDelims = options.commentDelims || commentDelims;
-            blockRegexp = options.blockRegexp || blockRegexp;
-            keyValueDelim = options.keyValueDelim || keyValueDelim;
-            newLineDelim = options.newLineDelim || newLineDelim;
-            joinKeyValue = options.joinKeyValue || joinKeyValue;
-            joinKeySlice = options.joinKeySlice || joinKeySlice;
-        }
-        content = content.split(newLineDelim);
-        content.forEach(function (line) {
-            var isComment = false;
-            commentDelims.forEach(function (delim) {
-                if (line.indexOf(delim) === 0) {
-                    isComment = true;
-                } else {
-                    return;
-                }
-            });
-            if (isComment) {
-                return;
-            }
-            if (blockRegexp.test(line)) {
-                currentBlock = line.replace(blockRegexp, '$1');
-                configObject[currentBlock] = {};
-                return;
-            }
-            currentLine = line.split(keyValueDelim);
-            if (currentBlock === undefined) {
-                configObject[currentLine[joinKeySlice]] = joinKeyValue ? currentLine.slice(joinKeySlice + 1).join(keyValueDelim) : currentLine[1];
-            } else {
-                configObject[currentBlock][currentLine[joinKeySlice]] = joinKeyValue ? currentLine.slice(joinKeySlice + 1).join(keyValueDelim) : currentLine[1];
-            }
-        });
-        return configObject;
-    }
-
-    function updateSetcodes() {
-
-    }
     /**
      * Filters duplicate, and unprinted cards out
      * @param   {Array[Object]} list of cards.
@@ -310,16 +254,11 @@ var databaseSystem = (function () {
         activeBanlist = $('.banlistSelect option:selected').val();
     });
 
-    $.get('./ygopro/strings.conf', 'utf-8', function (data) {
-        setcodes = configParser(data, {
-            keyValueDelim: " ",
-            commentDelims: [],
-            blockRegexp: new RegExp("^\\s?#(.*?)\\s?$/"),
-            joinKeyValue: true,
-            joinKeySlice: 1
-        });
+    $.getJSON('./setcodes.json', 'utf-8', function (data) {
+        setcodes = data;
         var setcode,
             strings = '<option value="0" data-calc="0">None</option>';
+        console.log(JSON.stringify(setcodes));
         for (setcode in setcodes) {
             if (setcodes.hasOwnProperty(setcode) && setcode[0] === '0' && setcode[1] === 'x' && setcode !== '0x0') {
                 strings = strings + '<option data-calc="' + setcode.slice(2) + '" value="' + parseInt(setcode, 16) + '">' + setcodes[setcode] + '</option>';
