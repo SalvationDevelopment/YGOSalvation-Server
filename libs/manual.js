b0ea2582a52e70d6d574acb7aab8936c67b87e9d
Jeff Falberg <auronzeroo@aim.com>
2017-04-06 14:34:27 -0400

    Revert "file loading fix"
    This reverts commit 911f18ed5ddb1c506701d8e02a850409ea025ca1.

--- libs/manual.js
+++ libs/manual.js
@@ -6,37 +6,14 @@ var validateDeck = require('./validate-Deck'),
     http = require('http'),
     https = require('https'),
     path = require('path'),
+    database = require('../http/manifest/manifest_0-en-OCGTCG.json'),
+    banlist = require('../http/manifest/banlist.json'),
     fs = require('fs'),
-    jsonfile = require('jsonfile'),
-    database = [],
-    banlist = {},
-    dbfilename = '../http/manifest/manifest_0-en-OCGTCG.json',
-    banlistfilename = '../http/manifest/banlist.json';
+    banLists = {};
 
-
-
-function generateDBs() {
-
-    jsonfile.readFile(banlistfilename, function (err, obj) {
-        banlist = obj;
-    });
-    jsonfile.readFile(dbfilename, function (err, obj) {
-        banlist = obj;
-    });
-}
-fs.watchFile(dbfilename, function () {
-    jsonfile.readFile(dbfilename, function (err, obj) {
-        database = obj;
-    });
-});
-
-fs.watchFile(banlistfilename, function () {
-    jsonfile.readFile(dbfilename, function (err, obj) {
-        banlist = obj;
-    });
-});
-
-generateDBs();
+/**
+ * Update the banlist
+ */
 
 module.exports = function (wss) {
 
