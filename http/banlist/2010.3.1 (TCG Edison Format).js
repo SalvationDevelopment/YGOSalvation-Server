6bd059c96608df66718bc7e3f2d57e2a5f3077dc
Jeff Falberg <auronzeroo@aim.com>
2017-04-26 23:21:59 -0400

    2010.3.1 (TCG Edison Format)

---
+++ http/banlist/2010.3.1 (TCG Edison Format).js
@@ -0,0 +1,151 @@
+module.exports = {
+	bannedCards: {
+		72989439: 0, //Black Luster Soldier - Envoy of the Beginning
+		69243953: 0, //Butterfly Dagger - Elma
+		57953380: 0, //Card of Safe Return
+		4031928: 0, //Change of Heart
+		82301904: 0, //Chaos Emperor Dragon - Envoy of the End
+		17375316: 0, //Confiscation
+		57728570: 0, //Crush Card Virus
+		57728571: 0, //Crush Card Virus
+		34124316: 0, //Cyber Jar
+		69015963: 0, //Cyber-Stein
+		53129443: 0, //Dark Hole
+		40737112: 0, //Dark Magician of Chaos
+		32646477: 0, //Dark Strike Fighter
+		44763025: 0, //Delinquent Duo
+		56570271: 0, //Destiny HERO - Disk Commander
+		23557835: 0, //Dimension Fusion
+		17484499: 0, //Exchange of the Spirit
+		78706415: 0, //Fiber Jar
+		79571449: 0, //Graceful Charity
+		18144506: 0, //Harpie's Feather Duster
+		18144507: 0, //Harpie's Feather Duster
+		61740673: 0, //Imperial Order
+		28566710: 0, //Last Turn
+		85602018: 0, //Last Will
+		34206604: 0, //Magical Scientist
+		31560081: 0, //Magician of Faith
+		21593977: 0, //Makyura the Destructor
+		46411259: 0, //Metamorphosis
+		41482598: 0, //Mirage of Nightmare
+		83764718: 0, //Monster Reborn
+		83764719: 0, //Monster Reborn
+		74191942: 0, //Painful Choice
+		55144522: 0, //Pot of Greed
+		70828912: 0, //Premature Burial
+		12580477: 0, //Raigeki
+		83555666: 0, //Ring of Destruction
+		83555667: 0, //Ring of Destruction
+		8131171: 0, //Sinister Serpent
+		45986603: 0, //Snatch Steal
+		42829885: 0, //The Forceful Sentry
+		63519819: 0, //Thousand-Eyes Restrict
+		35316708: 0, //Time Seal
+		33184167: 0, //Tribe-Infecting Virus
+		34853266: 0, //Tsukuyomi
+		44910027: 0, //Victory Dragon
+		78010363: 0, //Witch of the Black Forest
+		3078576: 0, //Yata-Garasu
+		46052429: 1, //Advanced Ritual Art
+		1475311: 1, //Allure of Darkness
+		73580471: 1, //Black Rose Dragon
+		2009101: 1, //Blackwing - Gale the Whirlwind
+		87910978: 1, //Brain Control
+		50321796: 1, //Brionac, Dragon of the Ice Barrier
+		48976825: 1, //Burial from a Different Dimension
+		97077563: 1, //Call of the Haunted
+		72892473: 1, //Card Destruction
+		85087012: 1, //Card Trooper
+		36468556: 1, //Ceasefire
+		9596126: 1, //Chaos Sorcerer
+		94886282: 1, //Charge of the Light Brigade
+		60682203: 1, //Cold Wave
+		65192027: 1, //Dark Armed Dragon
+		45809008: 1, //Destiny Draw
+		40044918: 1, //Elemental HERO Stratos
+		67723438: 1, //Emergency Teleport
+		33396948: 1, //Exodia the Forbidden One
+		81439173: 1, //Foolish Burial
+		77565204: 1, //Future Fusion
+		42703248: 1, //Giant Trunade
+		41470137: 1, //Gladiator Beast Bestiari
+		44330098: 1, //Gorz the Emissary of Darkness
+		7391448: 1, //Goyo Guardian
+		85742772: 1, //Gravity Bind
+		19613556: 1, //Heavy Storm
+		7902349: 1, //Left Arm of the Forbidden One
+		44519536: 1, //Left Leg of the Forbidden One
+		3136426: 1, //Level Limit - Area B
+		23171610: 1, //Limiter Removal
+		95503687: 1, //Lumina, Lightsworn Summoner
+		62279055: 1, //Magic Cylinder
+		32723153: 1, //Magical Explosion
+		31305911: 1, //Marshmallon
+		22046459: 1, //Megamorph
+		92826944: 1, //Mezuki
+		37520316: 1, //Mind Control
+		15800838: 1, //Mind Crush
+		96782886: 1, //Mind Master
+		44095762: 1, //Mirror Force
+		43040603: 1, //Monster Gate
+		33508719: 1, //Morphing Jar
+		79106360: 1, //Morphing Jar
+		5318639: 1, //Mystical Space Typhoon
+		4906301: 1, //Necro Gardna
+		28297833: 1, //Necroface
+		80344569: 1, //Neo-Spacian Grand Mole
+		16226786: 1, //Night Assailant
+		29843091: 1, //Ojama Trio
+		2295440: 1, //One for One
+		3659803: 1, //Overload Fusion
+		33420078: 1, //Plaguespreader Zombie
+		58577036: 1, //Reasoning
+		32807846: 1, //Reinforcement of the Army
+		14878871: 1, //Rescue Cat
+		27174286: 1, //Return from the Different Dimension
+		70903634: 1, //Right Arm of the Forbidden One
+		8124921: 1, //Right Leg of the Forbidden One
+		26202165: 1, //Sangan
+		73915051: 1, //Scapegoat
+		84290642: 1, //Snipe Hunter
+		41420027: 1, //Solemn Judgment
+		23205979: 1, //Spirit Reaper
+		423585: 1, //Summoner Monk
+		72302403: 1, //Swords of Revealing Light
+		46652477: 1, //The Transmigration Prophecy
+		53582587: 1, //Torrential Tribute
+		98777036: 1, //Tragoedia
+		64697231: 1, //Trap Dustshoot
+		17078030: 1, //Wall of Revealing Light
+		91351370: 2, //Black Whirlwind
+		29401950: 2, //Bottomless Trap Hole
+		91623717: 2, //Chain Strike
+		70095154: 2, //Cyber Dragon
+		70095155: 2, //Cyber Dragon
+		15341821: 2, //Dandylion
+		72426662: 2, //Demise, King of Armageddon
+		9411399: 2, //Destiny HERO - Malicious
+		63665875: 2, //Goblin Zombie
+		75500286: 2, //Gold Sarcophagus
+		37742478: 2, //Honest
+		57774843: 2, //Judgment Dragon
+		48686504: 2, //Lonefire Blossom
+		98494543: 2, //Magical Stone Excavation
+		51452091: 2, //Royal Decree
+		93016201: 2, //Royal Oppression
+		82732705: 2, //Skill Drain
+		12538374: 2, //Treeborn Frog
+		80604091: 2, //Ultimate Offering
+		80604092: 2, //Ultimate Offering
+		56747793: 2, //United We Stand
+	},
+    bannedTypes: [],
+    exceptions: [],
+    startDate: new Date('2010-03-01'),
+    endDate: new Date('2010-04-30'),
+    name: '2010.3.1 (TCG Edison Format)',
+    masterRule: 1,
+    region: 'tcg'
+
+}
