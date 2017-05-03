c837cd55f1462a317c1a087f2fdbd2556bcb3301
Jeff Falberg <auronzeroo@aim.com>
2017-05-03 16:44:22 -0400

    TeleDAD banlist

---
+++ http/banlist/2008.9.1 (TCG TeleDAD).js
@@ -0,0 +1,125 @@
+module.exports = {
+    bannedCards: {
+		72989439: 0, //Black Luster Soldier - Envoy of the Beginning
+		69243953: 0, //Butterfly Dagger - Elma
+		97077563: 0, //Call of the Haunted
+		4031928: 0, //Change of Heart
+		82301904: 0, //Chaos Emperor Dragon - Envoy of the End
+		9596126: 0, //Chaos Sorcerer
+		17375316: 0, //Confiscation
+		34124316: 0, //Cyber Jar
+		69015963: 0, //Cyber-Stein
+		53129443: 0, //Dark Hole
+		40737112: 0, //Dark Magician of Chaos
+		44763025: 0, //Delinquent Duo
+		56570271: 0, //Destiny HERO - Disk Commander
+		23557835: 0, //Dimension Fusion
+		17484499: 0, //Exchange of the Spirit
+		78706415: 0, //Fiber Jar
+		79571449: 0, //Graceful Charity
+		18144506: 0, //Harpie's Feather Duster
+		18144507: 0, //Harpie's Feather Duster
+		61740673: 0, //Imperial Order
+		28566710: 0, //Last Turn
+		85602018: 0, //Last Will
+		34206604: 0, //Magical Scientist
+		31560081: 0, //Magician of Faith
+		21593977: 0, //Makyura the Destructor
+		46411259: 0, //Metamorphosis
+		41482598: 0, //Mirage of Nightmare
+		74191942: 0, //Painful Choice
+		55144522: 0, //Pot of Greed
+		70828912: 0, //Premature Burial
+		12580477: 0, //Raigeki
+		83555666: 0, //Ring of Destruction
+		83555667: 0, //Ring of Destruction
+		8131171: 0, //Sinister Serpent
+		3280747: 0, //Sixth Sense
+		45986603: 0, //Snatch Steal
+		29762407: 0, //Temple of the Kings
+		63519819: 0, //Thousand-Eyes Restrict
+		35316708: 0, //Time Seal
+		33184167: 0, //Tribe-Infecting Virus
+		34853266: 0, //Tsukuyomi
+		44910027: 0, //Victory Dragon
+		78010363: 0, //Witch of the Black Forest
+		46052429: 1, //Advanced Ritual Art
+		87910978: 1, //Brain Control
+		71413901: 1, //Breaker the Magical Warrior
+		72892473: 1, //Card Destruction
+		85087012: 1, //Card Trooper
+		36468556: 1, //Ceasefire
+		57728570: 1, //Crush Card Virus
+		57728571: 1, //Crush Card Virus
+		70095154: 1, //Cyber Dragon
+		70095155: 1, //Cyber Dragon
+		7572887: 1, //D.D. Warrior Lady
+		15341821: 1, //Dandylion
+		40044918: 1, //Elemental HERO Stratos
+		33396948: 1, //Exodia the Forbidden One
+		66788016: 1, //Fissure
+		77565204: 1, //Future Fusion
+		42703248: 1, //Giant Trunade
+		75500286: 1, //Gold Sarcophagus
+		44330098: 1, //Gorz the Emissary of Darkness
+		85742772: 1, //Gravity Bind
+		46668237: 1, //Green Baboon, Defender of the Forest
+		19613556: 1, //Heavy Storm
+		7902349: 1, //Left Arm of the Forbidden One
+		44519536: 1, //Left Leg of the Forbidden One
+		3136426: 1, //Level Limit - Area B
+		23171610: 1, //Limiter Removal
+		62279055: 1, //Magic Cylinder
+		31305911: 1, //Marshmallon
+		22046459: 1, //Megamorph
+		15800838: 1, //Mind Crush
+		44095762: 1, //Mirror Force
+		43040603: 1, //Monster Gate
+		83764718: 1, //Monster Reborn
+		83764719: 1, //Monster Reborn
+		33508719: 1, //Morphing Jar
+		79106360: 1, //Morphing Jar
+		5318639: 1, //Mystical Space Typhoon
+		80344569: 1, //Neo-Spacian Grand Mole
+		16226786: 1, //Night Assailant
+		29843091: 1, //Ojama Trio
+		3659803: 1, //Overload Fusion
+		73125233: 1, //Raiza the Storm Monarch
+		58577036: 1, //Reasoning
+		27174286: 1, //Return from the Different Dimension
+		70903634: 1, //Right Arm of the Forbidden One
+		8124921: 1, //Right Leg of the Forbidden One
+		26202165: 1, //Sangan
+		73915051: 1, //Scapegoat
+		97169186: 1, //Smashing Ground
+		84290642: 1, //Snipe Hunter
+		72302403: 1, //Swords of Revealing Light
+		46652477: 1, //The Transmigration Prophecy
+		53582587: 1, //Torrential Tribute
+		64697231: 1, //Trap Dustshoot
+		12538374: 1, //Treeborn Frog
+		80604091: 1, //Ultimate Offering
+		80604092: 1, //Ultimate Offering
+		17078030: 1, //Wall of Revealing Light
+		14087893: 2, //Book of Moon
+		57953380: 2, //Card of Safe Return
+		91623717: 2, //Chain Strike
+		65192027: 2, //Dark Armed Dragon
+		81439173: 2, //Foolish Burial
+		57774843: 2, //Judgment Dragon
+		98494543: 2, //Magical Stone Excavation
+		77121851: 2, //Manticore of Darkness
+		28933734: 2, //Mask of Darkness
+		28297833: 2, //Necroface
+		71044499: 2, //Nobleman of Crossout
+		30312361: 2, //Phantom of Chaos
+    },
+    bannedTypes: [],
+    exceptions: [],
+    startDate: new Date('2008-09-01'),
+    endDate: new Date('2009-02-27'),
+    name: '2008.9.1 (TCG TeleDAD)',
+    masterRule: 1,
+    region: 'tcg'
+
+}
