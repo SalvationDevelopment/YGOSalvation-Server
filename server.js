87a3207fe2bd5e510dfffd73a7405f9279326945
Zayelion <panthrowzay@gmail.com>
2017-07-21 00:36:51 -0400

    VSCode Debugger

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
-/*
-Start various sub-servers.
---------------------------
-- Game List on port 24555
-- YGOPro listener on port 8911, YGOPro applications connect to this port
-- YGOPro Web listener on port 8913, browser version connects to this port, 
-  and is stripped and routed to the same place as the application. *disabled
-- HTTP server running static files out of port 80
-- IRC bot connects to #lobby, is named DuelServ
-- Update Server is running on port 12000
-- Ports 8911, 8913, *80, 12000 need to be free for the server to run.
-
-*/
-
 /*jslint  node: true, plusplus: true*/
 'use strict';
+require('dotenv').config();
 var fs = require('fs'),
     os = require('os'),
     path = require('path'),
@@ -25,7 +12,9 @@ var fs = require('fs'),
         SSL: process.env.SSL,
         ProductionFORUM: 'forum.ygopro.us',
         ProductionSITE: 'ygopro.us'
-    };
+    },
+    gamelistChildParams = (process.env.DEBUGGER === 'true') ? ['--debug-brk=5859'] : [],
+    dbChildParams = (process.env.DEBUGGER === 'true') ? ['--debug-brk=5860'] : [];
 
 var dependencies = require('./package.json').dependencies,
     modules,
@@ -39,23 +28,11 @@ var colors = require('colors'), // oo pretty colors!
     net = require('net'); // ping!;;
 
 
-
-
-
-
-//var ygopro_core = processManager.exec('./premake4 vs2012', {
-//    cwd: path.resolve('../../ygopro-core')
-//}, function () {
-//    makeYGOProCore();
-//});
-
-
-
-
 function deckstorageBoot() {
     console.log('    DeckStorage Online');
     processManager.fork('./deckstorage.js', [], {
-        cwd: 'libs'
+        cwd: 'libs',
+        execArgv: dbChildParams
     }).on('exit', deckstorageBoot);
 }
 
@@ -63,7 +40,8 @@ function deckstorageBoot() {
 function bootGameList() {
     console.log('    Primus Server Game Manager @ port 80/443'.bold.yellow);
     processManager.fork('./gamelist.js', [], {
-        cwd: 'libs'
+        cwd: 'libs',
+        execArgv: gamelistChildParams
     }).on('exit', bootGameList);
 }
 
@@ -73,33 +51,22 @@ function bootGameList() {
 function main() {
     var mainStack = domain.create();
 
-    mainStack.on('error', function (err) {
+    mainStack.on('error', function(err) {
         console.error((new Date()).toUTCString(), ' mainStackException:', err.message);
     });
-    mainStack.run(function () {
+
+    mainStack.run(function() {
 
         //segfaultHandler.registerHandler("crash.log"); // With no argument, SegfaultHandler will generate a generic log file name
         process.title = 'YGOPro Salvation Server ' + new Date();
-        console.log('YGOPro Salvation Server - Saving Yu-Gi-Oh!'.bold.yellow);
-
-
+        console.log('YGO Salvation Server - Saving Yu-Gi-Oh!'.bold.yellow);
         bootGameList();
-
-        
-        setTimeout(function () {
+        setTimeout(function() {
             deckstorageBoot();
         }, 500);
 
 
 
     });
-    delete process.send; // in case we're a child process
 }
-
-
-
-
-
-// segfaultHandler = require('segfault-handler'); //http://imgs.xkcd.com/comics/compiler_complaint.png
-//https://www.npmjs.com/package/segfault-handler
 main();
