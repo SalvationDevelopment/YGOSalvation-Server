cd99c1a43bf8f92c1db6b35088e178cd5bcd7a64
Jeff Falberg <auronzeroo@aim.com>
2017-07-21 21:54:26 -0400

    2017 World Championship banlist

---
+++ http/banlist/2017 World Championship.js
@@ -0,0 +1,275 @@
+module.exports = {
+	bannedCards: {
+        27279764: 0, //Apoqliphort Towers
+        30603688: 0, //Apprentice Illusion Magician
+        53804307: 0, //Blaster, Dragon Ruler of Infernos
+        82301904: 0, //Chaos Emperor Dragon - Envoy of the End
+        64753157: 0, //Charming Resort Staff
+        34124316: 0, //Cyber Jar
+        69015963: 0, //Cyber-Stein
+        56570271: 0, //Destiny HERO - Disk Commander
+        8903700: 0, //Djinn Releaser of Rituals
+        40044918: 0, //Elemental HERO Stratos
+        78706415: 0, //Fiber Jar
+        93369354: 0, //Fishborg Blaster
+        67120578: 0, //Gagaga Head
+        2530830: 0, //Galaxy-Eyes Cipher Blade Dragon
+        17502671: 0, //Infernity General
+        57421866: 0, //Level Eater
+        34206604: 0, //Magical Scientist
+        43930492: 0, //Magical Something
+        21593977: 0, //Makyura the Destructor
+        96782886: 0, //Mind Master
+        33508719: 0, //Morphing Jar
+        79106360: 0, //Morphing Jar #2
+        68819554: 0, //Performage Damage Juggler
+        90411554: 0, //Redox, Dragon Ruler of Boulders
+        90307777: 0, //Shurit, Strategist of the Nekroz
+        94096018: 0, //SPYGAL Misty
+        4474060: 0, //SPYRAL GEAR - Drone
+        37433748: 0, //SPYRAL GEAR - Last Resort
+        91258852: 0, //SPYRAL Master Plan
+        78080961: 0, //SPYRAL Quik-Fix
+        35699: 0, //SPYRAL Sleeper
+        41091257: 0, //SPYRAL Super Agent
+        20584712: 0, //SPYRAL Tough
+        20663556: 0, //Substitoad
+        95218695: 0, //Subterror Behemoth Dragossuary
+        1151281: 0, //Subterror Behemoth Phospheroglacier
+        47556396: 0, //Subterror Behemoth Speleogeist
+        78202553: 0, //Subterror Behemoth Stalagmo
+        65976795: 0, //Subterror Behemoth Stygokraken
+        92970404: 0, //Subterror Behemoth Ultramafus
+        42713844: 0, //Subterror Behemoth Umastryx
+        21607304: 0, //Subterror Behemoth Voltelluric
+        74762582: 0, //Subterror Fiendess
+        39581190: 0, //Subterror Nemesis Archer
+        68823957: 0, //Subterror Nemesis Defender
+        16719140: 0, //Subterror Nemesis Warrior
+        89399912: 0, //Tempest, Dragon Ruler of Storms
+        88071625: 0, //The Tyrant Neptune
+        26400609: 0, //Tidal, Dragon Ruler of Waterfalls
+        33184167: 0, //Tribe-Infecting Virus
+        72144675: 0, //Troposphere
+        44910027: 0, //Victory Dragon
+        16923472: 0, //Wind-Up Hunter
+        78010363: 0, //Witch of the Black Forest
+        3078576: 0, //Yata-Garasu
+        31178212: 0, //Majespecter Unicorn - Kirin
+        21250202: 0, //Odd-Eyes Persona Dragon
+        93149655: 0, //Odd-Eyes Phantom Dragon
+        7563579: 0, //Performage Plushfire
+        17330916: 0, //Performapal Monkeyboard
+        20366274: 0, //El Shaddoll Construct
+        17412721: 0, //Elder Entity Norden
+        96334243: 0, //Sea Monster of Theseus
+        37910722: 0, //Ascension Sky Dragon
+        8763963: 0, //Beelzeus of the Diabolic Dragons
+        30757396: 0, //Blood Mefist
+        89474727: 0, //Stardust Chronicle Spark Dragon
+        66698383: 0, //Vermillion Dragon Mech
+        46772449: 0, //Evilswarm Exciton Knight
+        34086406: 0, //Lavalval Chain
+        54719828: 0, //Number 16: Shock Master
+        75253697: 0, //Number 72: Shogi Rook
+        23187256: 0, //Number 93: Utopia Kaiser
+        18326736: 0, //Tellarknight Ptolemaeus
+        6983839: 0, //Tornado Dragon
+        81122844: 0, //Wind-Up Carrier Zenmaity
+        85115440: 0, //Zoodiac Broadbull
+        48905153: 0, //Zoodiac Drident
+        96704974: 0, //Boogie Trap
+        69243953: 0, //Butterfly Dagger - Elma
+        72892473: 0, //Card Destruction
+        57953380: 0, //Card of Safe Return
+        4031928: 0, //Change of Heart
+        67616300: 0, //Chicken Game
+        60682203: 0, //Cold Wave
+        17375316: 0, //Confiscation
+        44763025: 0, //Delinquent Duo
+        23557835: 0, //Dimension Fusion
+        53046408: 0, //Emergency Provisions
+        97926515: 0, //Emerging Emergency Rescute Rescue
+        22829942: 0, //Fusion Recycling Plant
+        27970830: 0, //Gateway of the Six
+        42703248: 0, //Giant Trunade
+        79571449: 0, //Graceful Charity
+        18144506: 0, //Harpie's Feather Duster
+        35059553: 0, //Kaiser Colosseum
+        85602018: 0, //Last Will
+        34906152: 0, //Mass Driver
+        46411259: 0, //Metamorphosis
+        41482598: 0, //Mirage of Nightmare
+        83764718: 0, //Monster Reborn
+        80161395: 0, //Mystik Wok
+        79324191: 0, //Ninjitsu Art Notebook
+        74191942: 0, //Painful Choice
+        67169062: 0, //Pot of Avarice
+        55144522: 0, //Pot of Greed
+        70828912: 0, //Premature Burial
+        12580477: 0, //Raigeki
+        45986603: 0, //Snatch Steal
+        46448938: 0, //Spellbook of Judgment
+        30979619: 0, //SPYRAL GEAR - Big Red
+        73828446: 0, //SPYRAL GEAR - Fully Armed
+        27642961: 0, //SPYRAL MISSION - Assault
+        54631665: 0, //SPYRAL Resort
+        68001309: 0, //Subterror Cave Clash
+        48130397: 0, //Super Polymerization
+        27770341: 0, //Super Rejuvenation
+        42829885: 0, //The Forceful Sentry
+        5697558: 0, //The Hidden City
+        14731897: 0, //Unity
+        31222701: 0, //Wavering Eyes
+        81332143: 0, //Yu-Jo Friendship
+        46060017: 0, //Zoodiac Barrage
+        55948544: 0, //Judgment of the Pharaoh
+        28566710: 0, //Last Turn
+        17178486: 0, //Life Equalizer
+        42378577: 0, //Pendulum Switch
+        34002992: 0, //Rainbow Life
+        27174286: 0, //Return from the Different Dimension
+        93016201: 0, //Royal Oppression
+        57585212: 0, //Self-Destruct Button
+        3280747: 0, //Sixth Sense
+        41420027: 0, //Solemn Judgment
+        92092092: 0, //Space Dragster
+        53989821: 0, //SPYRAL GEAR - Utility Wire
+        39373426: 0, //SPYRAL MISSION - Recapture
+        9212051: 0, //SPYRAL MISSION - Rescue
+        28369508: 0, //Subterror Behemoth Burrowing
+        74640994: 0, //Subterror Final Battle
+        35316708: 0, //Time Seal
+        64697231: 0, //Trap Dustshoot
+        73988674: 0, //Tri-and-Guess
+        80604091: 0, //Ultimate Offering
+        5851097: 0, //Vanity's Emptiness
+        7902349: 1, //Left Arm of the Forbidden One
+        44519536: 1, //Left Leg of the Forbidden One
+        70903634: 1, //Right Arm of the Forbidden One
+        8124921: 1, //Right Leg of the Forbidden One
+        85103922: 1, //Artifact Moralltach
+        72989439: 1, //Black Luster Soldier - Envoy of the Beginning
+        9929398: 1, //Blackwing - Gofu the Vague Shadow
+        57143342: 1, //Cir, Malebranche of the Burning Abyss
+        15341821: 1, //Dandylion
+        65192027: 1, //Dark Armed Dragon
+        78868119: 1, //Deep Sea Diva
+        96570609: 1, //Ehther the Heavenly Monarch
+        50720316: 1, //Elemental HERO Shadow Mist
+        33396948: 1, //Exodia the Forbidden One
+        55623480: 1, //Fairy Tail - Snow
+        64034255: 1, //Genex Ally Birdman
+        20758643: 1, //Graff, Malebranche of the Burning Abyss
+        99177923: 1, //Infernity Archfiend
+        68184115: 1, //Inzektor Dragonfly
+        69207766: 1, //Inzektor Hornet
+        55885348: 1, //Kozmo Dark Destroyer
+        21377582: 1, //Master Peace, the True Dracoslaying King
+        41386308: 1, //Mathematician
+        23434538: 1, //Maxx "C"
+        28297833: 1, //Necroface
+        80344569: 1, //Neo-Spacian Grand Mole
+        16226786: 1, //Night Assailant
+        88264978: 1, //Red-Eyes Darkness Metal Dragon
+        14878871: 1, //Rescue Cat
+        85138716: 1, //Rescue Rabbit
+        81275020: 1, //Speedroid Terrortop
+        423585: 1, //Summoner Monk
+        10802915: 1, //Tour Guide From the Underworld
+        78872731: 1, //Zoodiac Ratpier
+        65518099: 1, //Qliphort Scout
+        92746535: 1, //Luster Pendulum, the Dracoslayer
+        40318957: 1, //Performapal Skullcrobat Joker
+        45222299: 1, //Evigishki Gustkraken
+        11877465: 1, //Evigishki Mind Augus
+        26674724: 1, //Nekroz of Brionac
+        89463537: 1, //Nekroz of Unicore
+        6602300: 1, //Blaze Fenix, the Burning Bombardment Bird
+        48063985: 1, //Ritual Beast Ulti-Cannahawk
+        50321796: 1, //Brionac, Dragon of the Ice Barrier
+        70583986: 1, //Dewloren, Tiger King of the Ice Barrier
+        18239909: 1, //Ignister Prominence, the Blasting Dracoslayer
+        74586817: 1, //PSY-Framelord Omega
+        90953320: 1, //T.G. Hyper Librarian
+        52687916: 1, //Trishula, Dragon of the Ice Barrier
+        27552504: 1, //Beatrice, Lady of the Eternal
+        83531441: 1, //Dante, Traveler of the Burning Abyss
+        90809975: 1, //Toadally Awesome
+        14087893: 1, //Book of Moon
+        87910978: 1, //Brain Control
+        48976825: 1, //Burial from a Different Dimension
+        81674782: 1, //Dimensional Fissure
+        15854426: 1, //Divine Wind of Mist Valley
+        14733538: 1, //Draco Face-Off
+        13035077: 1, //Dragonic Diagram
+        6417578: 1, //El Shaddoll Fusion
+        67723438: 1, //Emergency Teleport
+        95308449: 1, //Final Countdown
+        77565204: 1, //Future Fusion
+        66957584: 1, //Infernity Launcher
+        99330325: 1, //Interrupted Kaiju Slumber
+        23171610: 1, //Limiter Removal
+        93600443: 1, //Mask Change II
+        37520316: 1, //Mind Control
+        43040603: 1, //Monster Gate
+        97211663: 1, //Nekroz Cycle
+        33782437: 1, //One Day of Peace
+        2295440: 1, //One for One
+        22842126: 1, //Pantheism of the Monarchs
+        53208660: 1, //Pendulum Call
+        96729612: 1, //Preparation of Rites
+        23701465: 1, //Primal Seed
+        58577036: 1, //Reasoning
+        32807846: 1, //Reinforcement of the Army
+        74845897: 1, //Rekindling
+        72405967: 1, //Royal Tribute
+        17639150: 1, //Saqlifice
+        54447022: 1, //Soul Charge
+        45305419: 1, //Symbol of Heritage
+        11110587: 1, //That Grass Looks Greener
+        79844764: 1, //The Monarchs Stormforth
+        70368879: 1, //Upstart Goblin
+        29401950: 1, //Bottomless Trap Hole
+        36468556: 1, //Ceasefire
+        94192409: 1, //Compulsory Evacuation Device
+        54974237: 1, //Eradicator Epidemic Virus
+        61740673: 1, //Imperial Order
+        9059700: 1, //Infernity Barrier
+        30241314: 1, //Macro Cosmos
+        32723153: 1, //Magical Explosion
+        83555666: 1, //Ring of Destruction
+        82732705: 1, //Skill Drain
+        84749824: 1, //Solemn Warning
+        73599290: 1, //Soul Drain
+        53582587: 1, //Torrential Tribute
+        17078030: 1, //Wall of Revealing Light
+        74311226: 2, //Atlantean Dragoons
+        85087012: 2, //Card Trooper
+        14943837: 2, //Debris Dragon
+        37742478: 2, //Honest
+        10028593: 2, //Reborn Tengu
+        84764038: 2, //Scarm, Malebranche of the Burning Abyss
+        71564252: 2, //Thunder King Rai-Oh
+        59297550: 2, //Wind-Up Magician
+        47075569: 2, //Performapal Pendulum Sorcerer
+        59750328: 2, //Card of Demise
+        91623717: 2, //Chain Strike
+        94886282: 2, //Charge of the Light Brigade
+        53129443: 2, //Dark Hole
+        84171830: 2, //Domain of the True Monarchs
+        62265044: 2, //Dragon Ravine
+        57103969: 2, //Fire Formation - Tenki
+        73628505: 2, //Terraforming
+        66399653: 2, //Union Hangar
+        29843091: 2, //Ojama Trio
+        40605147: 2, //Solemn Strike
+	},
+	bannedTypes: [],
+	exceptions: [],
+	startDate: new Date('2017-07-20'),
+	endDate: new Date('2017-07-22'),
+	name: '2017 World Championship',
+	masterRule: 4,
+	region: 'tcg',
+}
