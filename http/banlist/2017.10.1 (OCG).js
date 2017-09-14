8180d8f7a9676fd42cb9f928eba410dc2386d0eb
Jeff Falberg <auronzeroo@aim.com>
2017-09-13 23:48:35 -0400

    2017.10.1 (OCG) banlist

---
+++ http/banlist/2017.10.1 (OCG).js
@@ -0,0 +1,164 @@
+module.exports = {
+    bannedCards: {
+        53804307: 0, //Blaster, Dragon Ruler of Infernos
+        69243953: 0, //Butterfly Dagger - Elma
+        57953380: 0, //Card of Safe Return
+        4031928: 0, //Change of Heart
+        60682203: 0, //Cold Wave
+        17375316: 0, //Confiscation
+        34124316: 0, //Cyber Jar
+        13035077: 0, //Dragonic Diagram
+        44763025: 0, //Delinquent Duo
+        56570271: 0, //Destiny HERO - Disk Commander
+        23557835: 0, //Dimension Fusion
+        20366274: 0, //El Shaddoll Construct
+        17412721: 0, //Elder Entity Norden
+        78706415: 0, //Fiber Jar
+        93369354: 0, //Fishborg Blaster
+        42703248: 0, //Giant Trunade
+        79571449: 0, //Graceful Charity
+        19613556: 0, //Heavy Storm
+        28566710: 0, //Last Turn
+        85602018: 0, //Last Will
+        34086406: 0, //Lavalval Chain
+        57421866: 0, //Level Eater
+        17178486: 0, //Life Equalizer
+        34206604: 0, //Magical Scientist
+        31178212: 0, //Majespecter Unicorn - Kirin
+        21593977: 0, //Makyura the Destructor
+        34906152: 0, //Mass Driver
+        21377582: 0, //Master Peace, the True Dracoslaying King
+        46411259: 0, //Metamorphosis
+        96782886: 0, //Mind Master
+        41482598: 0, //Mirage of Nightmare
+        54719828: 0, //Number 16: Shock Master
+        74191942: 0, //Painful Choice
+        68819554: 0, //Performage Damage Juggler
+        7563579: 0, //Performage Plushfire
+        17330916: 0, //Performapal Monkeyboard
+        55144522: 0, //Pot of Greed
+        70828912: 0, //Premature Burial
+        12580477: 0, //Raigeki
+        90411554: 0, //Redox, Dragon Ruler of Boulders
+        27174286: 0, //Return from the Different Dimension
+        93016201: 0, //Royal Oppression
+        3280747: 0, //Sixth Sense
+        45986603: 0, //Snatch Steal
+        46448938: 0, //Spellbook of Judgment
+        20663556: 0, //Substitoad
+        18326736: 0, //Tellarknight Ptolemaeus
+        89399912: 0, //Tempest, Dragon Ruler of Storms
+        42829885: 0, //The Forceful Sentry
+        88071625: 0, //The Tyrant Neptune
+        26400609: 0, //Tidal, Dragon Ruler of Waterfalls
+        35316708: 0, //Time Seal
+        64697231: 0, //Trap Dustshoot
+        80604091: 0, //Ultimate Offering
+        80604092: 0, //Ultimate Offering
+        44910027: 0, //Victory Dragon
+        16923472: 0, //Wind-Up Hunter
+        3078576: 0, //Yata-Garasu
+        46060017: 0, //Zoodiac Barrage
+        85115440: 0, //Zoodiac Broadbull
+        48905153: 0, //Zoodiac Drident
+        1561110: 1, //ABC-Dragon Buster
+        86120751: 1, //Aleister, the Invoker
+        36042004: 1, //Babycerasaurus
+        9929398: 1, //Blackwing - Gofu the Vague Shadow
+        6602300: 1, //Blaze Fenix, the Burning Bombardment Bird
+        72892473: 1, //Card Destruction
+        36468556: 1, //Ceasefire
+        69015963: 1, //Cyber-Stein
+        83531441: 1, //Dante, Traveler of the Burning Abyss
+        65192027: 1, //Dark Armed Dragon
+        78868119: 1, //Deep Sea Diva
+        65536818: 1, //Denglong, First of the Yang Zing
+        70583986: 1, //Dewloren, Tiger King of the Ice Barrier
+        58984738: 1, //Dinomight Knight, the True Dracofighter
+        15854426: 1, //Divine Wind of Mist Valley
+        14733538: 1, //Draco Face-Off
+        50720316: 1, //Elemental HERO Shadow Mist
+        67723438: 1, //Emergency Teleport
+        33396948: 1, //Exodia the Forbidden One
+        55623480: 1, //Fairy Tail - Snow
+        81439173: 1, //Foolish Burial
+        64034255: 1, //Genex Ally Birdman
+        18144506: 1, //Harpie's Feather Duster
+        18144507: 1, //Harpie's Feather Duster
+        61740673: 1, //Imperial Order
+        66957584: 1, //Infernity Launcher
+        99330325: 1, //Interrupted Kaiju Slumber		
+        7902349: 1, //Left Arm of the Forbidden One
+        44519536: 1, //Left Leg of the Forbidden One
+        23171610: 1, //Limiter Removal
+        92746535: 1, //Luster Pendulum, the Dracoslayer
+        32723153: 1, //Magical Explosion
+        93600443: 1, //Mask Change II
+        83764718: 1, //Monster Reborn
+        83764719: 1, //Monster Reborn
+        33508719: 1, //Morphing Jar
+        28297833: 1, //Necroface
+        97211663: 1, //Nekroz Cycle
+        26674724: 1, //Nekroz of Brionac
+        89463537: 1, //Nekroz of Unicore
+        16226786: 1, //Night Assailant
+        33782437: 1, //One Day of Peace
+        2295440: 1, //One for One
+        74586817: 1, //PSY-Framelord Omega
+        22842126: 1, //Pantheism of the Monarchs
+        53208660: 1, //Pendulum Call
+        40318957: 1, //Performapal Skullcrobat Joker
+        23701465: 1, //Primal Seed
+        58577036: 1, //Reasoning
+        88264978: 1, //Red-Eyes Darkness Metal Dragon
+        32807846: 1, //Reinforcement of the Army
+        70903634: 1, //Right Arm of the Forbidden One
+        8124921: 1, //Right Leg of the Forbidden One
+        83555666: 1, //Ring of Destruction
+        83555667: 1, //Ring of Destruction
+        90307777: 1, //Shurit, Strategist of the Nekroz
+        41420027: 1, //Solemn Judgment
+        84749824: 1, //Solemn Warning
+        89739383: 1, //Spellbook of Secrets
+        54447022: 1, //Soul Charge
+        44335251: 1, //Souleating Oviraptor
+        48130397: 1, //Super Polymerization
+        45305419: 1, //Symbol of Heritage
+        90953320: 1, //T.G. Hyper Librarian
+        73628505: 1, //Terraforming
+        90809975: 1, //Toadally Awesome
+        10802915: 1, //Tour Guide From the Underworld
+        33184167: 1, //Tribe-Infecting Virus
+        21076084: 1, //Trickstar Reincarnation
+        52687916: 1, //Trishula, Dragon of the Ice Barrier
+        49430782: 1, //True Draco Heritage
+        35125879: 1, //True King's Return
+        5851097: 1, //Vanity's Emptiness
+        78872731: 1, //Zoodiac Ratpier
+        81275020: 1, //Speedroid Terrortop
+        74311226: 2, //Atlantean Dragoons
+        59750328: 2, //Card of Demise
+        91623717: 2, //Chain Strike
+        15341821: 2, //Dandylion
+        53129443: 2, //Dark Hole
+        40044918: 2, //Elemental HERO Stratos
+        45222299: 2, //Evigishki Gustkraken
+        47325505: 2, //Fossil Dig
+        77565204: 2, //Future Fusion
+        23434538: 2, //Maxx "C"
+        43040603: 2, //Monster Gate
+        67169062: 2, //Pot of Avarice
+        10028593: 2, //Reborn Tengu
+        40605147: 2, //Solemn Strike
+        423585: 2, //Summoner Monk
+        66399653: 2, //Union Hangar
+        43722862: 2, //Windwitch - Ice Bell
+    },
+    bannedTypes: [],
+    exceptions: [],
+    startDate: new Date('2017-10-01'),
+    endDate: new Date('2017-12-31'),
+    name: '2017.10.1 (OCG)',
+    masterRule: 4,
+    region: 'ocg'
+};
