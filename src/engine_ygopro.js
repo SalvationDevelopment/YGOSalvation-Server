08a26f16091168b2da50b8bc6b0afe4f3c137502
Zayelion <panthrowzay@gmail.com>
2017-10-07 13:43:59 -0400

    Rename, clarify, and clean code

--- src/engine_ygopro.js
+++ src/engine_ygopro.js
@@ -1,15 +1,33 @@
+/**
+ * @typedef Packet
+ * @type {Object}
+ * @property {Buffer} message 
+ * @property {Number} readposition
+ * @property {String} STOC 
+ */
+
+/**
+ * @typedef DataStream
+ * @type {Object}
+ * @property {Function} input
+ */
+
 var child_process = require('child_process'),
     EventEmitter = require('events'),
     net = require('net'),
     enums = require('./enums.js'),
     translateYGOProAPI = require('./receiver.js'),
-    manual = require('./engine_manual.js');
+    manualControlEngine = require('./engine_manual.js');
 
-
-function makeUICallback(socket) {
-    return function gameResponse(view, stack, callback) {
+/**
+ * Create a single players view of the game that is reflected down to the UI.
+ * @param {Object} webSockectConnection A players connection to the server.
+ * @returns {Object} A game instance with manual controls.
+ */
+function GameBoard(webSockectConnection) {
+    return manualControlEngine(function gameResponse(view, stack, callback) {
         try {
-            socket.write((view.p0));
+            webSockectConnection.write((view.p0));
         } catch (error) {
             console.log('failed messaging socket', error);
         } finally {
@@ -17,9 +35,14 @@ function makeUICallback(socket) {
                 return callback(stack);
             }
         }
-    };
+    });
 }
 
+/**
+ * Disect a message header from YGOPro.
+ * @param {Buffer} message YGOPro Protocol Message.
+ * @returns {Packet[]} Disected message in an array.
+ */
 function parsePackets(message) {
     'use strict';
 
@@ -33,29 +56,32 @@ function parsePackets(message) {
     return task;
 }
 
-
-function Framemaker() {
+/**
+ * Takes streamed broken up incoming data, stores it in a buffer, and as completed, returns complete messages.
+ * @returns {DataStream} data stream with input method.
+ */
+function DataStream() {
     'use strict';
     var memory = new Buffer([]);
 
     this.input = function(buffer) {
-        var x = true,
+        var incomplete = true,
             output = [],
             recordOfBuffer,
-            frame_length;
+            frameLength;
         memory = Buffer.concat([memory, buffer]);
-        while (x === true && memory.length > 2) {
-            frame_length = memory[0] + memory[1];
-            if ((memory.length - 2) < frame_length) {
-                x = false;
+        while (incomplete === true && memory.length > 2) {
+            frameLength = memory[0] + memory[1];
+            if ((memory.length - 2) < frameLength) {
+                incomplete = false;
             } else {
                 recordOfBuffer = memory.slice(2).toJSON();
                 output.push(recordOfBuffer);
-                if (memory.length === (frame_length + 2)) {
+                if (memory.length === (frameLength + 2)) {
                     memory = new Buffer([]);
-                    x = false;
+                    incomplete = false;
                 } else {
-                    memory = memory.slice((frame_length + 2));
+                    memory = memory.slice((frameLength + 2));
                 }
             }
         }
@@ -64,56 +90,69 @@ function Framemaker() {
     return this;
 }
 
-module.exports = function(instance, sockets) {
+/**
+ * Proxy a web socket connection to a TCP connection, which connects to YGOSharp.
+ * @param {Number} port Port Number requested instance of YGOSharp is running on
+ * @param {Object} webSockectConnection Players connection to the server.
+ * @param {Function} callback Function to run once player is connected.
+ * @returns {Object} TCP Client Connection Instance
+ */
+function connectToYGOSharp(port, webSockectConnection, callback) {
+    var dataStream = new DataStream(),
+        gameStateUpdater = new EventEmitter(),
+        gameBoard = new GameBoard(webSockectConnection),
+        tcpConnection;
+
+    gameStateUpdater.update = function(gameAction) {
+        gameStateUpdater.emit(gameAction.command, gameAction);
+    };
+
+    function cutConnections() {
+        if (tcpConnection) {
+            tcpConnection.end();
+        }
+        if (webSockectConnection) {
+            webSockectConnection.end();
+        }
+    }
+
+    tcpConnection = net.connect(port, '127.0.0.1', function() {
+        tcpConnection.on('data', function(data) {
+            dataStream.input(data)
+                .map(parsePackets)
+                .map(translateYGOProAPI)
+                .map(gameStateUpdater.update);
+        });
+        webSockectConnection.on(cutConnections);
+        webSockectConnection.on(cutConnections);
+
+        /*artificial connection initiation*/
+        // tcpConnection.write(data);
+        callback();
+    });
+    tcpConnection.setNoDelay(true);
+    tcpConnection.on('error', cutConnections);
+    tcpConnection.on('close', cutConnections);
+    return tcpConnection;
+
+}
+
+/**
+ * Start a YGOSharp instance, and connect users to it.
+ * @param {Object} instance game state object
+ * @param {Object[]} sockets connections to the server.
+ * @returns {Object} augmented game state object
+ */
+function startYGOSharp(instance, sockets) {
     instance.ygopro = child_process.spawn('./ygosharp/ygosharp.exe', function(error, stdout, stderr) {
 
     });
 
     var ygopro = instance.ygopro;
 
-    function connectToCore(port, webSockectConnection, callback) {
-        var dataStream = new Framemaker(),
-            gameStateUpdater = new EventEmitter(),
-            gameBoard = manual(makeUICallback(webSockectConnection)),
-            tcpConnection;
-
-        gameStateUpdater.update = function(gameAction) {
-            gameStateUpdater.emit(gameAction.command, gameAction);
-        };
-
-        function cutConnections() {
-            if (tcpConnection) {
-                tcpConnection.end();
-            }
-            if (webSockectConnection) {
-                webSockectConnection.end();
-            }
-        }
-
-        tcpConnection = net.connect(port, '127.0.0.1', function() {
-            tcpConnection.on('data', function(data) {
-                dataStream.input(data)
-                    .map(parsePackets)
-                    .map(translateYGOProAPI)
-                    .map(gameStateUpdater.update);
-            });
-            webSockectConnection.on(cutConnections);
-            webSockectConnection.on(cutConnections);
-
-            /*artificial connection initiation*/
-            // tcpConnection.write(data);
-            callback();
-        });
-        tcpConnection.setNoDelay(true);
-        tcpConnection.on('error', cutConnections);
-        tcpConnection.on('close', cutConnections);
-        return tcpConnection;
-
-    }
-
     ygopro.stdout.on('error', function(error) {
         ygopro.kill();
-        console.log('game ended with issues', error);
+        console.log('Game ended with issues', error);
     });
     ygopro.stdout.on('data', function(core_message_raw) {
         if (core_message_raw.toString().indexOf('::::') < 0) {
@@ -122,8 +161,8 @@ module.exports = function(instance, sockets) {
         var core_message = core_message_raw.toString().split('|');
 
         if (core_message[0].trim() === '::::network-ready') {
-            ygopro.sockets[0] = connectToCore(instance.port, sockets[0], function() {
-                ygopro.sockets[1] = connectToCore(instance.port, sockets[1], function() {});
+            ygopro.sockets[0] = connectToYGOSharp(instance.port, sockets[0], function() {
+                ygopro.sockets[1] = connectToYGOSharp(instance.port, sockets[1], function() {});
             });
 
         }
@@ -132,4 +171,14 @@ module.exports = function(instance, sockets) {
     instance.relay = function(socket, message) {
         ygopro.sockets[socket].send(message);
     };
-};
+
+    instance.newConnection = function(socket) {
+        ygopro.sockets.push(connectToYGOSharp(instance.port, socket, function() {
+
+        }));
+    };
+
+    return instance;
+}
+
+module.exports = startYGOSharp;
