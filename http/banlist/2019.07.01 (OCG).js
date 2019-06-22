f8354a02afaac81ad83ea96d215e2960931e9852
Jeff Falberg <auronzeroo@aim.com>
2019-06-22 14:00:39 -0400

    Most recent official F&L Lists
    - April 2019 TCG (Advanced + Traditional)
    - July 2019 OCG

---
+++ http/banlist/2019.07.01 (OCG).js
@@ -0,0 +1,190 @@
+module.exports =
+{
+    bannedCards : {
+        // FORBIDDEN
+        // MAIN DECK MONSTERS
+        20663556: 0,  // イレカエル Substitoad
+        44910027: 0,  // ヴィクトリー・ドラゴン Victory Dragon
+        53804307: 0,  // 焔征竜－ブラスター Blaster, Dragon Ruler of Infernos
+        90411554: 0,  // 巌征竜－レドックス Redox, Dragon Ruler of Boulders
+        26400609: 0,  // 瀑征竜－タイダル Tidal, Dragon Ruler of Waterfalls
+        57421866: 0,  // レベル・スティーラー Level Eater
+        34124316: 0,  // サイバーポッド Cyber Jar
+        21593977: 0,  // 処刑人－マキュラ Makyura the Destructor
+        16923472: 0,  // ゼンマイハンター Wind-up Hunter
+        78706415: 0,  // ファイバーポッド Fiber Jar
+        93369354: 0,  // フィッシュボーグ－ガンナー Fishborg Blaster
+        34206604: 0,  // 魔導サイエンティスト Magical Scientist
+        96782886: 0,  // メンタルマスター Mind Master
+        3078576: 0,  // 八汰烏 Yata-Garasu
+        88071625: 0,  // The Tyrant Neptune
+        7563579: 0,  // Emヒグルミ Performage PlushFire
+        17330916: 0,  // EMモンキーボード Performapal Monkeyboard
+        31178212: 0,  // マジェスペクター・ユニコーン Majespecter Unicorn - Kirin
+        21377582: 0,  // Master Peace, The True Dracoslaying King
+        9929398: 0,  // BF－朧影のゴウフウ Blackwing - Gofu the Vague Shadow
+        23558733: 0,  // Phoenixian Cluster Amaryllis
+        11384280: 0,  // Cannon Soldier
+        79875176: 0,  // Toon Cannon Soldier
+        91869203: 0,  // アマゾネスの射手 Amazoness Archer
+        14702066: 0,  // メガキャノン・ソルジャー Cannon Soldier MK-2
+        67441435: 0,  // Glow-up Bulb
+        15341821: 0,  // Dandylion
+        9047460: 0,  // Blackwing - Steam the Cloak
+        88264978: 0,  // レッドアイズ·ダークネスメタルドラゴン Red-Eyes Darkness Dragon
+        71525232: 0,  // Gandora-X the Dragon of Demolition
+        // EXTRA DECK MONSTERS
+        17412721: 0,  // 旧神ノーデン Elder Entity Norden 						
+        25862681: 0,  // Ancient Fairy Dragon
+        34086406: 0,  // ラヴァルバル・チェイン Lavalval Chain
+        18326736: 0,  // 星守の騎士 プトレマイオス Tellarknight Ptolomaeus
+        54719828: 0,  // No.16 色の支配者ショック・ルーラー Number 16: Shock Master
+        48905153: 0,  // 十二獣ドランシア Zoodiac Drident
+        85115440: 0,  // Zoodiac Broadbull
+        58820923: 0,  // Number 95: Galaxy-Eyes Dark Matter Dragon
+        4423206: 0,  // M-X-Saber Invoker
+        61665245: 0,  // サモン・ソーサレス Summon Sorceress
+        39064822: 0,  // トロイメア・ゴブリン Knightmare Goblin
+        5043010: 0,  // Firewall Dragon
+        3679218: 0,  // Knightmare Mermaid
+        // SPELLS CARDS
+        44763025: 0,  // いたずら好きな双子悪魔 Delinquent Duo
+        19613556: 0,  // 大嵐 Heavy Storm
+        17375316: 0,  // 押収 Confiscation
+        74191942: 0,  // 苦渋の選択 Painful Choice
+        42829885: 0,  // 強引な番兵 The Forceful Sentry
+        55144522: 0,  // 強欲な壺 Pot of Greed
+        4031928: 0,  // 心変わり Change of Heart
+        60682203: 0,  // 大寒波 Cold Wave
+        42703248: 0,  // ハリケーン Giant Trunade
+        79571449: 0,  // 天使の施し Graceful Charity
+        23557835: 0,  // 次元融合 Dimension Fusion
+        46411259: 0,  // 突然変異 Methamorphosis
+        85602018: 0,  // 遺言状 Last Will
+        41482598: 0,  // 悪夢の蜃気楼 Mirage of Nightmare
+        57953380: 0,  // 生還の宝札 Card of the Safe Return
+        46060017: 0,  // 十二獣の会局 Zoodiac Barrage
+        45986603: 0,  // 強奪 Snatch Steal
+        69243953: 0,  // 蝶の短剣－エルマ Butterfly Dagger - Elma
+        70828912: 0,  // 早すぎた埋葬 Premature Burial
+        31423101: 0,  // Divine Sword - Phoenix Blade
+        34906152: 0,  // マスドライバー Mass Driver
+        46448938: 0,  // 魔導書の神判 Spellbook of Judgment
+        13035077: 0,  // Dragonic Diagram
+        // TRAPS CARDS
+        27174286: 0,  // 異次元からの帰還 Return from the Different Dimension
+        3280747: 0,  // 第六感 Sixth Sense
+        64697231: 0,  // ダスト·シュート Trap Dustshoot
+        35316708: 0,  // 刻の封印 Time Seal
+        17178486: 0,  // ライフチェンジャー Life Equalizer
+        28566710: 0,  // ラストバトル！ Last turn
+        93016201: 0,  // 王宮の弾圧 Royal Oppression
+        80604091: 0,  // 血の代償 Ultimate Offering
+        32723153: 0,  // マジカル·エクスプロージョン Magical Explosion
+        // LIMITED
+        // MAIN DECK MONSTERS
+        64034255: 1,  // A·ジェネクス·バードマン Genex Ally Birdman
+        50720316: 1,  // E·HERO シャドー·ミスト Elemental Hero Shadow Mist
+        40318957: 1,  // EMドクロバット・ジョーカー Performapal Skullcrobat Joker
+        78868119: 1,  // 深海のディーヴァ Deep Sea Diva
+        16226786: 1,  // 深淵の暗殺者 Night Assailant
+        28297833: 1,  // ネクロフェイス Necroface
+        33396948: 1,  // 封印されしエクゾディア Exxodia the forbidden
+        7902349: 1,  // 封印されし者の左腕 Left Arm
+        70903634: 1,  // 封印されし者の右腕 Right Arm
+        44519536: 1,  // 封印されし者の左足 Left Leg 
+        8124921: 1,  // 封印されし者の右足 Right Leg
+        10802915: 1,  // 魔界発現世行きデスガイド Tour Guide From the Underworld
+        33508719: 1,  // メタモルポット Morphing Jar
+        89463537: 1,  // ユニコールの影霊衣 Nekroz of Unicore
+        92746535: 1,  // 竜剣士ラスターP Luster Pendulum
+        78872731: 1,  // 十二獣モルモラット Zoodiac Ratpier
+        44335251: 1,  // Souleating Oviraptor
+        36042004: 1,  // Babycerasaurus
+        49684352: 1,  // Double Iris Magician
+        69610326: 1,  // Supreme King Dragon Darkwurm
+        75732622: 1,  // Grinder Golem
+        48686504: 1,  // Lonefire Blossom
+        35272499: 1,  // Predaplant Ophrys Scorpio
+        76794549: 1,  // Astrograph Sorcerer
+        73941492: 1,  // Harmonizing Magician
+        12289247: 1,  // Chronograph Sorcerer
+        41386308: 1,  // Mathematician
+        89399912: 1,  // 嵐征竜－テンペスト Tempest, Dragon Ruler of Storms
+        42790071: 1,  // オルターガイスト・マルチフェイカー Altergeist Multifaker
+        28985331: 1,  // Armageddon Knight
+        81275020: 1,  // SRベイゴマックス Speedroid Terrortop
+        26889158: 1,  // Salamangreat Gazelle
+        83107873: 1,  // Thunder Dragonhawk
+        16188701: 1,  // Lady Debug
+        // EXTRA DECK MONSTERS
+        6602300: 1,  // 重爆撃禽 ボム・フェネクス  Blaze Fenix, The Burning Bombardment Bird
+        15291624: 1,  // Thunder Dragon Colossus
+        75286621: 1,  // Invoked Mechaba
+        74586817: 1,  // PSYフレームロード・Ω PSY-Framelord Omega
+        70583986: 1,  // 氷結界の虎王ドゥローレン Dewloren, Tiger king of the Ice Barrier
+        24094258: 1,  // Heavymetalfoes Electrumite
+        50588353: 1,  // Crystron Needlefiber
+        63288573: 1,  // Sky Striker Ace - Kagari
+        // SPELLS CARDS
+        33782437: 1,  // 一時休戦 One Day of peace
+        66957584: 1,  // インフェルニティガン Infernity launcer
+        81439173: 1,  // おろかな埋葬 Foolish Burial
+        23701465: 1,  // 原初の種 Primal Seed
+        99330325: 1,  // 妨げられた壊獣の眠り Interrupted Kaiju Slumber
+        2295440: 1,  // ワン·フォー·ワン One for One
+        73628505: 1,  // Terraforming
+        83764718: 1,  // 死者蘇生 Monster Reborn
+        32807846: 1,  // 増援 Reinforcement of the Army
+        54447022: 1,  // ソウル・チャージ Soul Charge
+        58577036: 1,  // 名推理 Reasoning
+        12580477: 1,  // サンダー·ボルト Raigeki
+        18144506: 1,  // ハーピィの羽根帚 Harpie's Feather Duster
+        53208660: 1,  // ペンデュラム・コール Pendulum Call
+        14733538: 1,  // 竜呼相打つ Draco Face-Off
+        72892473: 1,  // 手札抹殺 Card Destruction
+        93600443: 1,  // マスク・チェンジ・セカンド Mask Change 2
+        45305419: 1,  // 継承の印 Symbol of Heritage
+        54631665: 1,  // SPYRAL Resort
+        15854426: 1,  // 霞の谷の神風 Divine Wind of Mist Valley
+        52340444: 1,  // Sky Striker Mecha - Hornet Drones
+        73468603: 1,  //  Set Rotation
+        75500286: 1,  // 封印の黄金櫃 Gold Sarcophagus
+        // TRAPS CARDS
+        21076084: 1,  // Trickstar Reincarnation
+        89208725: 1,  // Metaverse
+        5851097: 1,  // 虚無空間 Vanity's Emptiness
+        61740673: 1,  // 王宮の勅命 Imperial Order
+        // SEMI-LIMITED
+        14558127: 2,  // Ash Blossom & Joyous Spring
+        61283655: 2,  // Trickstar Candina
+        78080961: 2,  // SPYRAL Quik-Fix
+        65192027: 2,  // ダーク·アームド·ドラゴン Dark Armed Dragon
+        14536035: 2,  // Dark Grepher
+        82385847: 2,  // Dinowrestler Pankratops
+        69015963: 2,  // デビル・フランケン Cyber Stein
+        1561110: 2,  // ABC-Dragon Buster
+        90953320: 2,  // TG ハイパー·ライブラリアン TG Hyper Librarian
+        52687916: 2,  // 氷結界の龍 トリシューラ Trishula, Dragon of the Ice Barrier
+        73915051: 2,  // Scapegoat
+        47325505: 2,  // Fossil Dig
+        8949584: 2,  // ヒーローアライブ A Hero Lives
+        97211663: 2,  // 影霊衣の反魂術 Nekroz Cycle
+        76375976: 2,  // Mystic Mine
+        91623717: 2,  // 連鎖爆撃 Chain Strike
+        67723438: 2,  // 緊急テレポート Emergency teleporter
+        23171610: 2,  // リミッター解除 Limiter Removal
+        98338152: 2,  // 閃刀機－ウィドウアンカー Sky Striker Mecha - Widow Anchor
+        63166095: 2,  // 閃刀起動－エンゲージ Sky Striker Mobilize - Engage!
+        48130397: 2,  // 超融合 Super Polymerization
+        11110587: 2,  // 隣の芝刈り That Grass Looks Greener
+        40605147: 2,  // Solemn Strike
+        53936268: 2  // Personal Spoofing
+    },
+     bannedTypes : [],
+     exceptions : [],
+     startDate : new Date('2019-07-01'),
+     endDate : new Date('2019-10-01'),
+     masterRule: 4,
+     region: 'ocg'
+};
