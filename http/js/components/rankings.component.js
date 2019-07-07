f4934b8dc30c2f69568c226ad374035de16e16a2
Zayelion <panthrowzay@gmail.com>
2019-07-07 03:43:52 -0400

    Ranking SSS

--- http/js/components/rankings.component.js
+++ http/js/components/rankings.component.js
@@ -17,19 +17,20 @@ class RankingScreen extends React.Component {
 
 
     render() {
-        return React.createElement('table', {}, [
-            React.createElement('thead', { key: 'thead' }, [
-                React.createElement('th', { key: 'rank' }, 'Rank'),
-                React.createElement('th', { key: 'points' }, 'Points'),
-                React.createElement('th', { key: 'name' }, 'Username')
-            ]),
-            React.createElement('tbody', { key: 'tbody' }, this.state.ranks.map((user, i) => {
-                return React.createElement('tr', { className: 'rankinguser', key: `ranking-row-${i}` }, [
-                    React.createElement('td', { key: `ranking-rank-${i}` }, i + 1),
-                    React.createElement('td', { key: `ranking-points-${i}` }, `${user.points}`),
-                    React.createElement('td', { key: `ranking-name-${i}` }, `${user.username}`)
-                ]);
-            }))
-        ]);
+        return React.createElement('div', { id: '#rankholder' },
+            React.createElement('table', {}, [
+                React.createElement('thead', { key: 'thead' }, [
+                    React.createElement('th', { key: 'rank' }, 'Rank'),
+                    React.createElement('th', { key: 'points' }, 'Points'),
+                    React.createElement('th', { key: 'name' }, 'Username')
+                ]),
+                React.createElement('tbody', { key: 'tbody' }, this.state.ranks.map((user, i) => {
+                    return React.createElement('tr', { className: 'rankinguser', key: `ranking-row-${i}` }, [
+                        React.createElement('td', { key: `ranking-rank-${i}` }, i + 1),
+                        React.createElement('td', { key: `ranking-points-${i}` }, `${user.points}`),
+                        React.createElement('td', { key: `ranking-name-${i}` }, `${user.username}`)
+                    ]);
+                }))
+            ]));
     }
 }
