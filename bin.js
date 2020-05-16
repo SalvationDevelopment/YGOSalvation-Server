69d237ebd8cb07e1593c2197fcc033809cf53d68
james.gladney <panthrowzay@gmail.com>
2020-05-16 00:08:23 -0400

    DOCKER - Allow deployment in Linux

--- bin.js
+++ bin.js
@@ -22,15 +22,19 @@ function main() {
     console.log('[SERVER] YGO Salvation Server - Saving Yu-Gi-Oh!'.bold.green);
     const banlist = './http/manifest/banlist.json';
 
-    if (os.platform === 'win32' && process.env.NODIST_X64 !== '0' || os.arch() === 'x64') {
+    if (os.platform === 'win32' && process.env.NODIST_X64 !== '0') {
         console.error('Node is Running in 64bit mode, games can not start.');
         console.info('HINT --> SET NODIST_X64=0'.bold.yellow);
-        process.exit();
+        if (!process.env.DEFIANT) {
+            process.env(1);
+        }
     }
     if (!ADMIN_SERVER_URL || !ADMIN_SERVER_USERNAME || !ADMIN_SERVER_PASSWORD) {
         console.error('Administrative Server and User are not configured, no database access.');
         console.info('HINT --> README.MD'.bold.yellow);
-        process.exit();
+        if (!process.env.DEFIANT) {
+            process.env(1);
+        }
     }
 
     if (Boolean(process.env.ADMIN_SERVER_LOCAL)) {
@@ -41,7 +45,7 @@ function main() {
     if (Boolean(process.env.DATABASE_SERVER_LOCAL)) {
         console.log('[SERVER] Starting Database Server'.bold.green);
         databaseServer = child_process.fork('../ygosalvation-database/app.js', [], {
-            
+
         });
     }
 
